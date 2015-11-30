b007f89d1808ba57cdea5d16a2eddd2559c8d972
Santhosh Vasabhaktula <Santhosh@Santhoshs-MacBook-Pro.local>
2015-11-30 15:12:04 +0530

    Update data fetcher to take the json deserialization type

--- platform-driver/analytics-job-driver/src/main/scala/org/ekstep/ilimi/analytics/framework/driver/BatchJobDriver.scala
+++ platform-driver/analytics-job-driver/src/main/scala/org/ekstep/ilimi/analytics/framework/driver/BatchJobDriver.scala
@@ -7,6 +7,7 @@ import org.ekstep.ilimi.analytics.framework.JobRunner
 import org.ekstep.ilimi.analytics.framework.OutputDispatcher
 import org.ekstep.ilimi.analytics.framework.util.CommonUtil
 import org.ekstep.ilimi.analytics.framework.JobContext
+import org.ekstep.ilimi.analytics.framework.Event
 
 /**
  * @author Santhosh
@@ -17,7 +18,7 @@ object BatchJobDriver {
 
         JobContext.parallelization = CommonUtil.getParallelization(config);
         val sc = CommonUtil.getSparkContext(JobContext.parallelization, config.appName.getOrElse(config.model));
-        val rdd = DataFetcher.fetchBatchData(sc, config.search);
+        val rdd = DataFetcher.fetchBatchData[Event](sc, config.search);
         JobContext.deviceMapping = rdd.filter { x => CommonUtil.getEventId(x).equals("GE_GENIE_START") }.map { x => (x.did.get, x.edata.eks.loc.getOrElse("")) }.collect().toMap;
         val filterRdd = DataFilter.filterAndSort(rdd, config.filters, config.sort);
         val output = JobRunner.executeBatch(config.model, sc, filterRdd, config.modelParams);
