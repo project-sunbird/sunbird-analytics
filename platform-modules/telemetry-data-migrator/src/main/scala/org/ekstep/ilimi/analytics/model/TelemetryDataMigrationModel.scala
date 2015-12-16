785c9f985790a2bbb9adab9fea20c00362ba157b
Santhosh Vasabhaktula <Santhosh@Santhoshs-MacBook-Pro.local>
2015-12-16 12:08:29 +0530

    Update model to add @timestamp to the event if not found

--- platform-modules/telemetry-data-migrator/src/main/scala/org/ekstep/ilimi/analytics/model/TelemetryDataMigrationModel.scala
+++ platform-modules/telemetry-data-migrator/src/main/scala/org/ekstep/ilimi/analytics/model/TelemetryDataMigrationModel.scala
@@ -8,7 +8,17 @@ import org.ekstep.ilimi.analytics.framework.util.JSONUtils
 object TelemetryDataMigrationModel extends Serializable {
 
     def execute(rdd: RDD[SyncEvent]) : RDD[String] = {
-        rdd.filter(_ != null).map { x => x.data.getOrElse(LineData(None, None, None, None, Array())).events }.filter { x => x != null }.flatMap(f => f.map(JSONUtils.serialize(_)));
+        rdd.filter(_ != null)
+        .map { x => {
+            val ld = x.data.getOrElse(LineData(None, None, None, None, Array()));
+            val ts = ld.ts;
+            ld.events.map(f => {
+               val m = scala.collection.mutable.Map(f.toSeq: _*);
+               m += "@timestamp" -> ts.getOrElse(m.get("ts")).asInstanceOf[AnyRef];
+            }); 
+        }}
+        .filter { x => x != null }
+        .flatMap(f => f.map(JSONUtils.serialize(_)));
     }
 
 }
