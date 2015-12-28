98e0b6837d40fe1dc0a712ffd5f449b6e9f0a9f5
amit behera <amitb@ilimi.in>
2015-12-28 11:52:37 +0530

    User Screen Summary model with test case

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/SparkSpec.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/SparkSpec.scala
@@ -8,31 +8,23 @@ import org.json4s.jackson.JsonMethods
 import org.json4s.jvalue2extractable
 import org.json4s.string2JsonInput
 import org.scalatest.BeforeAndAfterAll
-
 import com.fasterxml.jackson.core.JsonParseException
+import org.ekstep.ilimi.analytics.framework.util.JSONUtils
 
 /**
  * @author Santhosh
  */
-class SparkSpec extends BaseSpec with BeforeAndAfterAll {
+class SparkSpec(val file: String =  "src/test/resources/sample_telemetry.log") extends BaseSpec with BeforeAndAfterAll {
     
     var events: RDD[Event] = null;
     var sc: SparkContext = null;
     
     override def beforeAll() {
         sc = CommonUtil.getSparkContext(1, "TestAnalyticsCore");
-        val rdd = sc.textFile("src/test/resources/sample_telemetry.log", 1).cache();
+        val rdd = sc.textFile(file, 1).cache();
         events = rdd.map { line =>
             {
-                implicit val formats = DefaultFormats;
-                try{
-                    JsonMethods.parse(line).extract[Event]   
-                } catch {
-                    case pe: JsonParseException =>
-                        null;
-                    case ex: Exception =>
-                       throw ex;
-                }
+                JSONUtils.deserialize[Event](line);
             }
         }.filter { x => x != null }.cache();
     }
