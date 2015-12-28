2d36f77dbac66d8812c9eb0e08d5b8bacf99fb3c
Santhosh Vasabhaktula <Santhosh@Santhoshs-MacBook-Pro.local>
2015-12-28 14:46:42 +0530

    Update test case

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestAserScreenSummary.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestAserScreenSummary.scala
@@ -4,19 +4,35 @@ import org.ekstep.ilimi.analytics.framework.SparkSpec
 import java.io.FileWriter
 import org.ekstep.ilimi.analytics.framework.JobContext
 import org.ekstep.ilimi.analytics.framework.util.CommonUtil
+import org.ekstep.ilimi.analytics.framework.DataFilter
+import org.ekstep.ilimi.analytics.framework.Filter
+import org.ekstep.ilimi.analytics.framework.util.JSONUtils
 
 class TestAserScreenSummary extends SparkSpec("src/test/resources/prod.telemetry.unique-2015-12-20-07-47.json") {
- 
-  "AserScreenSummary" should "produce aser page session" in {
-    
+
+    "AserScreenSummary" should "produce aser page session" in {
+
         val aserScreener = new AserScreenSummary();
-        val rdd = aserScreener.execute(sc, events, Option(Map("modelVersion" -> "1.1", "modelId" -> "AserScreenerSummary")));
-        val me = rdd.collect();
+        val rdd = DataFilter.filter(events, Filter("eventId", "IN", Option(Array("OE_START","OE_INTERACT","OE_ASSESS","OE_LEVEL_SET","OE_END"))));
+        val rdd2 = aserScreener.execute(sc, rdd, Option(Map("modelVersion" -> "1.1", "modelId" -> "AserScreenerSummary")));
+        Console.println("rdd2 count", rdd2.count);
+        val me = rdd2.collect();
         val fw = new FileWriter("aser_test_output.txt", true);
-        for(e <- me) {
-            Console.println(e);
+        for (e <- me) {
+            //Console.println(e);
             fw.write(e + "\n");
         }
         fw.close();
+    }
+
+    it should "print events to a file" in {
+        val rdd = DataFilter.filter(events, Array(Filter("uid", "EQ", Option("63a2c136-eb3e-4d0c-ae12-f552a1fbda3f")), Filter("eventId", "IN", Option(Array("OE_START","OE_INTERACT","OE_ASSESS","OE_LEVEL_SET","OE_END")))));
+        println(rdd.count);
+        val aserScreener = new AserScreenSummary();
+        val rdd2 = aserScreener.execute(sc, rdd, Option(Map("modelVersion" -> "1.1", "modelId" -> "AserScreenerSummary")));
+        val me = rdd2.collect();
+        for (e <- me) {
+            Console.println(e);
+        }
     }
 }
