fd435f257a3e96110fbbc508d19fcee38af7b8a1
amit behera <amitb@ilimi.in>
2016-02-01 12:33:06 +0530

    Pij matrix calculation for Reco Engine

---
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/RecoEngine.scala
@@ -0,0 +1,50 @@
+package org.ekstep.analytics.model
+
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.ekstep.analytics.framework.MeasuredEvent
+import com.datastax.spark.connector._
+import org.ekstep.analytics.updater.Activity
+import org.ekstep.analytics.framework.adapter.ContentAdapter
+import scala.collection.mutable.Buffer
+import org.ekstep.analytics.framework.JobContext
+import org.apache.spark.HashPartitioner
+
+case class PijMatrix(concept1: String, concept2: String, pijValue: Double);
+
+object RecoEngine {
+    def execute(sc: SparkContext, events: RDD[MeasuredEvent], jobParams: Option[Map[String, AnyRef]]): RDD[String] = {
+
+        // getting all concepts from concept model
+        val contents = ContentAdapter.getAllContent();
+        val contentConcepts = contents.map { x => (x.id, x.concepts) }.toMap;
+        val concepts = contentConcepts.values.flatten.toBuffer.distinct;
+
+        //getting timespent for each content 
+        val activitiesInDB = sc.cassandraTable[Activity]("learner_db", "learneractivity");
+        val conceptTimeSpent = activitiesInDB.map { x => (x.learner_id, (contentConcepts.get(x.content).get, x.time_spent)) }
+        .filter(f => (f._2._1.nonEmpty)).toArray.toMap.map{x=>
+            (x._1,(x._2._1.map(f=>(f,x._2._2))).toMap);
+        }
+        
+        //getting proficiency for each concepts
+        val proficiencies = sc.cassandraTable[LearnerProficiency]("learner_db", "learnerproficiency").map { x => (x.learner_id, x.proficiency) }.toArray().toMap;
+
+        var Pij = Buffer[PijMatrix]();
+        val learnerPij = events.map(event => (event.uid.get)).map { x =>
+            val proficiency = proficiencies.get(x).get;
+            val timeSpentMap = conceptTimeSpent.get(x).get
+            val totalTimeSpent = timeSpentMap.values.sum;
+            
+            concepts.foreach { a =>
+                concepts.foreach { b =>
+                    val PijValue = Math.max(proficiency.getOrElse(a,0.5d), proficiency.getOrElse(b,0.5d))+(timeSpentMap.getOrElse(b,0d)/totalTimeSpent);
+                    Pij += PijMatrix(a,b,PijValue)
+                }
+            }
+            (x,Pij)
+        };
+
+        return null;
+    }
+}
