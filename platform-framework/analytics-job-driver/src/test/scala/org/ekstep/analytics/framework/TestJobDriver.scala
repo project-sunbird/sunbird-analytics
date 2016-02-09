685e37b8b1872c22c2d53b3d756bb7e818a48ea4
Santhosh Vasabhaktula <Santhosh@Santhoshs-MacBook-Pro.local>
2016-02-09 14:03:37 +0530

    Fix test case to test for config options

--- platform-framework/analytics-job-driver/src/test/scala/org/ekstep/analytics/framework/TestJobDriver.scala
+++ platform-framework/analytics-job-driver/src/test/scala/org/ekstep/analytics/framework/TestJobDriver.scala
@@ -51,5 +51,23 @@ class TestJobDriver extends FlatSpec with Matchers {
             JobDriver.run("streaming", JSONUtils.serialize(""), new TestModel);
         }
     }
+    
+    it should "fetch the app name from job config model if the app name is not specified" in {
+        
+        val jobConfig = JobConfig(
+            Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/sample_telemetry.log"))))),
+            Option(Array[Filter](Filter("eventId", "EQ", Option("OE_START")))),
+            None,
+            "org.ekstep.analytics.framework.TestModel",
+            Option(Map()),
+            Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))),
+            Option(8),
+            None,
+            None)
+
+        noException should be thrownBy {
+            JobDriver.run[Event]("batch", JSONUtils.serialize(jobConfig), new TestModel);
+        }
+    }
 
 }
