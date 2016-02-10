afa2bf866ca0b9db05dbd055ee590a569a59880b
Santhosh Vasabhaktula <Santhosh@Santhoshs-MacBook-Pro.local>
2016-02-10 13:29:15 +0530

    Update test case to clear data in cassandra before running the test

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestRecommendationEngine.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestRecommendationEngine.scala
@@ -5,6 +5,7 @@ import org.ekstep.analytics.framework.OutputDispatcher
 import org.ekstep.analytics.framework.Dispatcher
 import com.datastax.spark.connector._
 import org.ekstep.analytics.framework.util.CommonUtil
+import com.datastax.spark.connector.cql.CassandraConnector
 
 class TestRecommendationEngine extends SparkSpec(null) {
 
@@ -14,18 +15,19 @@ class TestRecommendationEngine extends SparkSpec(null) {
         val rdd2 = RecommendationEngine.execute(sc, rdd, None);
         val result = rdd2.collect();
     }
-    
+
     it should "compute the recommendation for a learner" in {
-        
+
         val learner_id = "8f111d6c-b618-4cf4-bb4b-bbf06cf4363a";
-        val lr = LearnerConceptRelevance(learner_id, Map());
-        sc.parallelize(Array(lr), 1).saveToCassandra("learner_db", "learnerconceptrelevance");
+        CassandraConnector(sc.getConf).withSessionDo { session =>
+            session.execute("DELETE FROM learner_db.learnerconceptrelevance where learner_id = '" + learner_id + "'");
+        }
         
         val rdd = loadFile[MeasuredEvent]("src/test/resources/reco-engine/reco_test_data_1.log");
         val rdd2 = RecommendationEngine.execute(sc, rdd, None);
         val result = rdd2.collect();
         //println(result(0));
-        
+
         val lcr = sc.cassandraTable[LearnerConceptRelevance]("learner_db", "learnerconceptrelevance").where("learner_id = ?", learner_id).first();
         val r = lcr.relevance;
         r.size should be > 100;
