6ef0a54a4b3d51f4acbf6633898992e9517cc5f0
amit behera <amitb@ilimi.in>
2016-02-10 16:27:39 +0530

    changed to Application to optional.Application in Concept Similarity Job

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/ConceptSimilarityUpdaterJob.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/ConceptSimilarityUpdaterJob.scala
@@ -4,7 +4,7 @@ import org.ekstep.analytics.framework.JobDriver
 import org.ekstep.analytics.updater.ConceptSimilarityUpdater
 import org.ekstep.analytics.updater.ConceptSimilarityEntity
 
-object ConceptSimilarityUpdaterJob extends Application {
+object ConceptSimilarityUpdaterJob extends optional.Application {
 
     def main(config: String) {
         JobDriver.run[ConceptSimilarityEntity]("batch", config, ConceptSimilarityUpdater);
