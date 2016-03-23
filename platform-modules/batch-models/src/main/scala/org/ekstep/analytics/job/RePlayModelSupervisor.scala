c192c8f10c091ed8c7b844abc8d1cdcd93934f7d
amit behera <amitb@ilimi.in>
2016-03-23 18:12:34 +0530

    try catch implementation inside loop in Reply Supervisor

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/RePlayModelSupervisor.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/RePlayModelSupervisor.scala
@@ -7,28 +7,37 @@ import java.util.regex.Pattern
 import org.ekstep.analytics.framework.JobContext
 import org.ekstep.analytics.framework.util.JSONUtils
 import org.ekstep.analytics.framework.JobConfig
+import org.ekstep.analytics.framework.driver.BatchJobDriver
+import org.ekstep.analytics.framework.JobConfig
+import org.ekstep.analytics.model.LearnerProficiencySummary
+import org.ekstep.analytics.framework.JobDriver
 
 object RePlayModelSupervisor extends Application {
 
     def main(model: String, fromDate: String, toDate: String, config: String) {
-
-        val con = JSONUtils.deserialize[JobConfig](config)
         
-        val sc = CommonUtil.getSparkContext(JobContext.parallelization, con.appName.getOrElse(con.model));
-        // checking for yyyy-mm-dd formt in config 
+        val con = JSONUtils.deserialize[JobConfig](config)
+        implicit val sc = CommonUtil.getSparkContext(JobContext.parallelization, con.appName.getOrElse(con.model));
+
+        // patterns for start-date and end-date for yyyy-mm-dd formt 
         val matcher1 = Pattern.compile("(\\d{4}-\\d{2}-\\d{2})").matcher(config)
         val matcher2 = Pattern.compile("(endDate\":\"\\d{4}-\\d{2}-\\d{2})").matcher(config)
         val matcher3 = Pattern.compile("(startDate\":\"\\d{4}-\\d{2}-\\d{2})").matcher(config)
-        
+
         model match {
             case "LearnerProficiencySummary" =>
                 var jobConfig = config;
                 val dateRange = CommonUtil.getDatesBetween(fromDate, Option(toDate))
                 if (matcher1.find) {
-                    jobConfig = matcher1.replaceAll("__date__")
                     for (date <- dateRange) {
-                        jobConfig = jobConfig.replace("__date__", date);
-                        ProficiencyUpdater.main(jobConfig)(Option(sc))
+                        try {
+                            println("Running for date: ", date)
+                            jobConfig = matcher1.replaceAll(date);
+                            //ProficiencyUpdater.main(jobConfig)(Option(sc))
+                            JobDriver.run("batch", jobConfig, LearnerProficiencySummary);
+                        } catch {
+                            case e: Exception => println("Got an empty result: " + e);
+                        }
                     }
                 } else {
                     println("#### Executing Proficiency Summary from local file ####")
@@ -37,16 +46,16 @@ object RePlayModelSupervisor extends Application {
                 CommonUtil.closeSparkContext()(sc)
             case "LearnerActivitySummary" =>
                 val con = JSONUtils.deserialize[JobConfig](config)
-                val sc = CommonUtil.getSparkContext(JobContext.parallelization, con.appName.getOrElse(con.model));
                 var jobConfig = config;
                 val dateRange = CommonUtil.getDatesBetween(fromDate, Option(toDate))
                 if (matcher2.find && matcher3.find) {
+                    var end = CommonUtil.getEndDate(Option(fromDate), 6)
                     for (date <- dateRange) {
-                        val end = CommonUtil.getEndDate(Option(date), 6)
                         if (CommonUtil.dateIsAfterOrEqual(toDate, end.get)) {
                             jobConfig = matcher2.replaceAll("endDate\":\"" + end.get);
                             jobConfig = matcher3.replaceAll("startDate\":\"" + date);
                             LearnerActivitySummarizer.main(jobConfig)(Option(sc))
+                            end = CommonUtil.getEndDate(end, 1)
                         }
                     }
                 } else {
@@ -55,12 +64,13 @@ object RePlayModelSupervisor extends Application {
                 CommonUtil.closeSparkContext()(sc)
             case "LearnerContentActivitySummary" =>
                 val con = JSONUtils.deserialize[JobConfig](config)
-                val sc = CommonUtil.getSparkContext(JobContext.parallelization, con.appName.getOrElse(con.model));
                 var jobConfig = config;
                 val dateRange = CommonUtil.getDatesBetween(fromDate, Option(toDate))
                 if (matcher2.find && matcher3.find) {
+
+                    val end = CommonUtil.getEndDate(Option(fromDate), 6)
                     for (date <- dateRange) {
-                        val end = CommonUtil.getEndDate(Option(date), 6)
+
                         if (CommonUtil.dateIsAfterOrEqual(toDate, end.get)) {
                             jobConfig = matcher2.replaceAll("endDate\":\"" + end.get);
                             jobConfig = matcher3.replaceAll("startDate\":\"" + date);
@@ -73,7 +83,6 @@ object RePlayModelSupervisor extends Application {
                 CommonUtil.closeSparkContext()(sc)
             case "RecommendationEngine" =>
                 val con = JSONUtils.deserialize[JobConfig](config)
-                val sc = CommonUtil.getSparkContext(JobContext.parallelization, con.appName.getOrElse(con.model));
                 var jobConfig = config;
                 val dateRange = CommonUtil.getDatesBetween(fromDate, Option(toDate))
                 if (matcher1.find) {
@@ -88,7 +97,6 @@ object RePlayModelSupervisor extends Application {
                 CommonUtil.closeSparkContext()(sc)
             case _ =>
                 throw new Exception("Model name is not correct");
-
         }
     }
 }
--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/TestRePlayModelSupervisor.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/TestRePlayModelSupervisor.scala
@@ -10,13 +10,25 @@ import org.ekstep.analytics.model.BaseSpec
 
 class TestRePlayModelSupervisor extends BaseSpec {
 
-    ignore should " Run LP from S3 " in {
-        val config = JobConfig(Fetcher("s3", None,  Option(Array(Query(Option("ekstep-session-summary"), Option("prod.analytics.screener-"), Option("2016-02-21"), Option("2016-02-23"))))), Option(Array(Filter("eid", "EQ", Option("ME_SESSION_SUMMARY")))), None, "org.ekstep.analytics.model.ProficiencyUpdater", Option(Map("alpha" -> 1.0d.asInstanceOf[AnyRef], "beta" -> 1.0d.asInstanceOf[AnyRef])), Option(Array(Dispatcher("file", Map("file" -> "prof_output.log")))), Option(10), Option("TestModelSupervisor"), Option(false))
-        RePlayModelSupervisor.main("LearnerProficiencySummary", "2016-02-22", "2016-02-23", JSONUtils.serialize(config));
+    
+//    "TestRePlayModelSupervisor" should " Run LP from local file" in {
+//        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/learner-proficiency/"))))), None, None, "org.ekstep.analytics.model.ProficiencyUpdater", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestModelSupervisor"), Option(false))
+//        println(JSONUtils.serialize(config))
+//        RePlayModelSupervisor.main("LearnerProficiencySummary", "2016-02-22", "2016-02-23", JSONUtils.serialize(config));
+//    }
+    
+    it should " Run LP from S3 " in {
+        val config = JobConfig(Fetcher("s3", None,  Option(Array(Query(Option("prod-data-store"), Option("ss/"), Option("2016-01-10"), Option("2016-02-23"))))), Option(Array(Filter("eid", "EQ", Option("ME_SESSION_SUMMARY")))), None, "org.ekstep.analytics.model.ProficiencyUpdater", Option(Map("alpha" -> 1.0d.asInstanceOf[AnyRef], "beta" -> 1.0d.asInstanceOf[AnyRef])), Option(Array(Dispatcher("file", Map("file" -> "src/test/resources/learner-proficiency/out/prof_output.log")))), Option(10), Option("TestModelSupervisor"), Option(false))
+        RePlayModelSupervisor.main("LearnerProficiencySummary", "2016-02-18", "2016-02-20", JSONUtils.serialize(config));
     }
-    it should " Run LP from local file" in {
-        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/learner-proficiency/proficiency_update_db_test1.log"))))), None, None, "org.ekstep.analytics.model.ProficiencyUpdater", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestModelSupervisor"), Option(false))
-        println(JSONUtils.serialize(config))
-        RePlayModelSupervisor.main("LearnerProficiencySummary", "2016-02-22", "2016-02-23", JSONUtils.serialize(config));
-    }
+//    
+//    it should " Run RE from local file " in {
+//        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/reco-engine/reco_engine_test.log"))))), None, None, "org.ekstep.analytics.model.RecommendationEngine", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("Test Recommendation Engine Job"), Option(false))
+//        RePlayModelSupervisor.main("RecommendationEngine", "2016-02-22", "2016-02-23", JSONUtils.serialize(config))
+//    }
+//    
+//    it should " Run LAS from local file " in {
+//        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/learner-activity-summary/learner_activity_summary_sample1.log"))))), None, None, "org.ekstep.analytics.model.LearnerActivitySummary", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestLearnerActivitySummarizer"), Option(false))
+//        RePlayModelSupervisor.main("LearnerActivitySummarizer", "2016-02-22", "2016-02-23", JSONUtils.serialize(config))
+//    }
 }
