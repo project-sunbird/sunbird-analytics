96da88393ad2d2b4aa37ce01c0025a0c375817d7
amit behera <amitb@ilimi.in>
2016-04-07 14:19:45 +0530

    replay backup script date option changed as per Linux Env

--- platform-scripts/shell/sandbox/replay-backup.sh
+++ platform-scripts/shell/sandbox/replay-backup.sh
@@ -5,15 +5,15 @@ modelCode=$3
 bucket_name=$4
 backup_dir=$5
 
-start_date=`date -j -f %Y-%m-%d $date1 +%s`
-end_date=`date -j -f %Y-%m-%d $date2 +%s`
+start_date=$(date -d $date1 +%s)
+end_date=$(date -d $date2 +%s)
 
 while [ $start_date -le $end_date ] 
 do
-	date=`date -j -f %s $start_date +%Y-%m-%d`
- 	echo $date
- 	src="s3://$bucket_name/$modelCode/"
- 	dst="s3://$bucket_name/$backup_dir/"
-	aws s3 cp $src $dst --recursive --exclude "*" --include "$date-*"
- 	let start_date+=86400
+	date=`date -d @$start_date +%F`
+	echo $date
+    src="s3://$bucket_name/$modelCode/"
+    dst="s3://$bucket_name/$backup_dir/"
+    aws s3 mv $src $dst --recursive --exclude "*" --include "$date-*"
+    let start_date+=86400
 done
