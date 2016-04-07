80a0c6475a9ebbe64e989b441893ab7ce7903dde
sowmya <sowmyadixit7@gmail.com>
2016-04-07 13:37:21 +0530

    Implementation as per new Design

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ContentSummary.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ContentSummary.scala
@@ -14,75 +14,46 @@ class Summary(val contentId: String,val ver: String, val dtRange: DtRange,val sy
                  val interactionsMinSession: List[Double], val averageInteractionsMin: Double, 
                  val numSessionsWeek: Map[String, Int], val tsWeek: Map[String, Double]) extends Serializable {};
 
-object ContentSummary extends SessionBatchModel[Event] with Serializable {
+object ContentSummary extends IBatchModel[MeasuredEvent] with Serializable {
   
-  def execute(data: RDD[Event], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
+  def execute(data: RDD[MeasuredEvent], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
     println("### Running the model ContentSummary ###");
-    val filteredEvents = DataFilter.filter(data, Filter("eventId", "IN", Option(List("OE_ASSESS", "OE_START", "OE_END", "GE_GAME_END", "OE_INTERACT")))).sortBy { x => CommonUtil.getEventTS(x) };
-    //val sortedEvents = filteredEvents.sortBy { x => CommonUtil.getEventTS(x) }
+    val filteredEvents = DataFilter.filter(data, Filter("eid", "EQ", Option("ME_SESSION_SUMMARY")));
+    val sortedEvents = filteredEvents.sortBy { x => x.ets };
     println("### Broadcasting data to all worker nodes ###");
     val config = jobParams.getOrElse(Map[String, AnyRef]());
     val configMapping = sc.broadcast(config);
     val deviceMapping = sc.broadcast(JobContext.deviceMapping);
     
-//    val gameSessions = getGameSessions(filteredEvents).map(x => x._2)
-//    val sessions = gameSessions.map{x => x}.groupBy { x => x.head.gdata.id}.asInstanceOf[RDD[(String, Buffer[Event])]]
-//    println("checking")
-    
-    val contentMap = filteredEvents.groupBy { x => x.gdata.id }//.asInstanceOf[RDD[(String,RDD[Event])]]
+    val contentMap = sortedEvents.groupBy { x => x.dimensions.gdata.head.id }
     val contentSummary = contentMap.mapValues { events =>
-      val firstEvent = events.head//first();
-      val lastEvent = events.last//take(events.count().asInstanceOf[Int]).last;
-      val gameId = CommonUtil.getGameId(firstEvent);
-      val gameVersion = CommonUtil.getGameVersion(lastEvent);
-      val startTimestamp = Option(CommonUtil.getEventTS(firstEvent));
-      val endTimestamp = Option(CommonUtil.getEventTS(lastEvent));
-      val startDate = Option(CommonUtil.getEventDate(firstEvent));
-      val lastDate = Option(CommonUtil.getEventDate(lastEvent));
-      val loc = deviceMapping.value.getOrElse(firstEvent.did, "");
-     
-      val oeEnds = events.filter { x => "OE_END".equals(x.eid) };
-      val numSessions = oeEnds.size//count()
-      val timeSpent = oeEnds.map { x =>
-        (x.edata.eks.length.asInstanceOf[Double])
+      val firstEvent = events.head
+      val lastEvent = events.last
+      val gameId = events.head.dimensions.gdata.last.id
+      val gameVersion = events.head.ver
+      val eventStartTimestamp = firstEvent.syncts
+      val eventEndTimestamp = lastEvent.syncts
+ 
+      val numSessions = events.size
+      val timeSpent = events.map{x => 
+          (x.edata.eks.asInstanceOf[Map[String, AnyRef]].get("timeSpent").get.asInstanceOf[Double])
       }.sum
-      val averageTsSession = timeSpent/numSessions
-//    println("checking outside sessions")     
-//      val interactionsMinSession = sessions.mapValues { sess =>
-//        println("checking inside sessions")
-//        if(sess.head.gdata.id.equals(firstEvent.gdata.id))
-//        {
-//          val sessionsPerContent = sess.asInstanceOf[RDD[Event]]
-//          val noOfInteractEvents = DataFilter.filter(sessionsPerContent, Filter("edata.eks.type", "IN", Option(List("TOUCH", "DRAG", "DROP", "PINCH", "ZOOM", "SHAKE", "ROTATE", "SPEAK", "LISTEN", "WRITE", "DRAW", "START", "END", "CHOOSE", "ACTIVATE")))).count();
-//          var tmpLastEvent: Event = null;
-//          val eventsWithTs = sessionsPerContent.map { x =>
-//               if (tmpLastEvent == null) tmpLastEvent = x;
-//               val ts = CommonUtil.getTimeDiff(tmpLastEvent, x).get;
-//               tmpLastEvent = x;
-//               (x, ts)
-//           }
-//          val tS = eventsWithTs.map(f => f._2).sum;
-//          val interactEventsPerMin: Double = if (noOfInteractEvents == 0 || tS == 0) 0d else BigDecimal(noOfInteractEvents / (tS / 60)).toDouble;
-//          interactEventsPerMin
-//        }
-//          
-//      }.asInstanceOf[RDD[Double]]
-//      val averageInteractionsMin = (interactionsMinSession.map{ x => x}.sum)/interactionsMinSession.count()
-      val interactionsMinSession = null
-      val averageInteractionsMin = 0d
-      val numSessionsWeek = Map(1+":"+startDate.get+"-"+lastDate.get -> numSessions )
-      //println(numSessionsWeek)
-      val tsWeek = Map(1+":"+startDate.get+"-"+lastDate.get -> timeSpent )
-      //println(tsWeek)
-      new Summary(gameId,gameVersion,DtRange(startTimestamp.getOrElse(0l),endTimestamp.getOrElse(0l)), CommonUtil.getEventSyncTS(lastEvent), 
-               Option(loc), timeSpent, numSessions, averageTsSession, interactionsMinSession.asInstanceOf[List[Double]], 
+      val averageTsSession = timeSpent/numSessions  
+      val interactionsMinSession = events.map{ x => 
+          (x.edata.eks.asInstanceOf[Map[String, AnyRef]].get("interactEventsPerMin").get.asInstanceOf[Double])
+      }.asInstanceOf[List[Double]]
+      val averageInteractionsMin = ((interactionsMinSession.map(x => x).sum)/interactionsMinSession.size)
+      val numSessionsWeek = null
+      val tsWeek = null
+      new Summary(gameId,gameVersion,DtRange(eventStartTimestamp,eventEndTimestamp), lastEvent.syncts, 
+               None, timeSpent, numSessions, averageTsSession, interactionsMinSession.asInstanceOf[List[Double]], 
                averageInteractionsMin, numSessionsWeek, tsWeek )
     }
     contentSummary.map(f => {
             getMeasuredEvent(f, configMapping.value);
         }).map { x => JSONUtils.serialize(x) };
   }
-  
+
   private def getMeasuredEvent(userMap: (String, Summary), config: Map[String, AnyRef]): MeasuredEvent = {
         val game = userMap._2;
         val mid = CommonUtil.getMessageId("ME_CONTENT_SUMMARY", userMap._1, "CONTENT", game.dtRange, game.contentId);
--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentSummary.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentSummary.scala
@@ -8,11 +8,11 @@ import org.ekstep.analytics.framework.MeasuredEvent
 class TestContentSummary extends SparkSpec(null){
   "ContentSummary" should "generate summary and pass all positive test cases" in {
 
-        val rdd = loadFile[Event]("src/test/resources/prod.telemetry.unique-2016-04-03-10-21.json");
+        val rdd = loadFile[MeasuredEvent]("src/test/resources/learner-proficiency/test.log");
         val rdd2 = ContentSummary.execute(rdd, Option(Map("modelVersion" -> "1.4", "modelId" -> "GenericContentSummary")));
         val me = rdd2.collect();
         
-        me.length should be(28);
+        me.length should be(18);
 
         val event1 = JSONUtils.deserialize[MeasuredEvent](me(1));
         event1.eid should be("ME_CONTENT_SUMMARY");
@@ -23,11 +23,12 @@ class TestContentSummary extends SparkSpec(null){
         event1.context.date_range should not be null;
         
         val eks = event1.edata.eks.asInstanceOf[Map[String,AnyRef]]
-        eks.get("averageTsSession").get should be (20.5)
+        eks.get("averageTsSession").get should be (15.5)
         eks.get("numSessions").get should be (2)
-        eks.get("timeSpent").get should be (41.0)
-        eks.get("numSessionsWeek").get should be (Map("1:Wed Mar 30 00:00:00 IST 2016-Wed Mar 30 00:00:00 IST 2016" -> 2))
-        eks.get("tsWeek").get should be (Map("1:Wed Mar 30 00:00:00 IST 2016-Wed Mar 30 00:00:00 IST 2016" -> 41.0))
+        eks.get("timeSpent").get should be (31.0)
+        eks.get("averageInteractionsMin").get should be (8.31)
+//        eks.get("numSessionsWeek").get should be ()
+//        eks.get("tsWeek").get should be ()
         
   }
 }
