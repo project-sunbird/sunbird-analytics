d10af26fcd21fa7cb42ad27de98a9eca8ebcb6cd
sowmya <sowmyadixit7@gmail.com>
2016-04-14 14:17:42 +0530

    Updated test case

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentSummary.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentSummary.scala
@@ -9,7 +9,7 @@ class TestContentSummary extends SparkSpec(null){
   "ContentSummary" should "generate contentsummary and pass all positive test cases" in {
 
         val rdd = loadFile[MeasuredEvent]("src/test/resources/2016-02-15-20160316.json.gz");
-        val rdd2 = ContentSummary.execute(rdd, Option(Map("modelVersion" -> "1.4", "modelId" -> "GenericContentSummary")));
+        val rdd2 = ContentSummary.execute(rdd, None);
         val me = rdd2.collect();
         
         me.length should be(23);
@@ -18,8 +18,8 @@ class TestContentSummary extends SparkSpec(null){
         println(JSONUtils.serialize(event1))
         event1.eid should be("ME_CONTENT_SUMMARY");
         event1.mid should be("8224701A2A442F3CD81995791700435C");
-        event1.context.pdata.model should be("GenericContentSummary");
-        event1.context.pdata.ver should be("1.4");
+        event1.context.pdata.model should be("ContentSummary");
+        event1.context.pdata.ver should be("1.0");
         event1.context.granularity should be("CONTENT");
         event1.context.date_range should not be null;
         
