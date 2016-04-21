f3eb6cf20033a911a82c8548073ebc4bb8238d34
sowmya <sowmyadixit7@gmail.com>
2016-04-21 17:27:21 +0530

    Implement DeviceSpecification, Update DeviceSpecification table in learner_db and test case to test DeviceSpecification.

---
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/DeviceSpecification.scala
@@ -0,0 +1,49 @@
+package org.ekstep.analytics.model
+
+import org.ekstep.analytics.framework.util.CommonUtil
+import org.ekstep.analytics.framework.IBatchModel
+import org.ekstep.analytics.framework._
+import org.apache.spark.SparkContext
+import org.apache.spark.rdd.RDD
+import org.ekstep.analytics.util.Constants
+import com.datastax.spark.connector._
+import org.ekstep.analytics.framework.util.JSONUtils
+
+case class DeviceSpec(device_id: String, device_name: String, device_local_name: String, os: String, make: String,
+                      memory: Double, internal_disk: Double, external_disk: Double, screen_size: Double, 
+		                  primary_secondary_camera:String, cpu:String,num_sims:Double,capabilities:List[String])
+
+object DeviceSpecification extends IBatchModel[TelemetryEventV2] with Serializable{
+  
+  def execute(data: RDD[TelemetryEventV2], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
+     
+      val filteredEvents = DataFilter.filter(data, Filter("eid", "EQ", Option("GE_GENIE_START")));
+      val config = jobParams.getOrElse(Map[String, AnyRef]());
+      val configMapping = sc.broadcast(config);
+    
+      val deviceSummary = filteredEvents.map { event =>
+        
+            val deviceSpec = event.edata.eks.dspec.asInstanceOf[Map[String,AnyRef]]
+            val deviceId = event.did
+            val deviceName = deviceSpec.get("dname").get.asInstanceOf[String]
+            val deviceLocalName = deviceSpec.get("dlocname").get.asInstanceOf[String]
+            val os = deviceSpec.get("os").get.asInstanceOf[String]
+            val make = deviceSpec.get("make").get.asInstanceOf[String]
+            val memory = deviceSpec.get("mem").get.asInstanceOf[Double]
+            val internalDisk = deviceSpec.get("idisk").get.asInstanceOf[Double]
+            val externalDisk = deviceSpec.get("edisk").get.asInstanceOf[Double]
+            val screenSize = deviceSpec.get("scrn").get.asInstanceOf[Double]
+            val primarySecondaryCamera = deviceSpec.get("camera").get.asInstanceOf[String]
+            val cpu = deviceSpec.get("cpu").get.asInstanceOf[String]
+            val numSims = deviceSpec.get("sims").get.asInstanceOf[Double]
+            val capabilities = deviceSpec.get("cap").get.asInstanceOf[List[String]]
+
+            DeviceSpec(deviceId,deviceName,deviceLocalName,os,make,memory,internalDisk,externalDisk,screenSize,primarySecondaryCamera,cpu,numSims,capabilities)
+   
+      }
+      
+      deviceSummary.saveToCassandra(Constants.KEY_SPACE_NAME, Constants.DEVICE_SPECIFICATION_TABLE);
+      deviceSummary.map { x => JSONUtils.serialize(x) };
+  }
+  
+}
---
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestDeviceSpecification.scala
@@ -0,0 +1,14 @@
+package org.ekstep.analytics.model
+
+import org.ekstep.analytics.framework.Event
+import org.ekstep.analytics.framework.util.JSONUtils
+import org.ekstep.analytics.framework._
+
+class TestDeviceSpecification extends SparkSpec(null){
+  
+  "DeviceSpecification" should "generate devicespec and pass all positive test cases" in {
+
+        val rdd = loadFile[TelemetryEventV2]("src/test/resources/device-specification/raw.telemetry.test1.json");
+        DeviceSpecification.execute(rdd, None);
+  }
+}
