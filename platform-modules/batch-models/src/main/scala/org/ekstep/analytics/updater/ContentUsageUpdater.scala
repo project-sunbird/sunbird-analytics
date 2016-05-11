02a707be1fecd225111a410093ab9dd024c013af
amit behera <amitb@ilimi.in>
2016-05-12 00:49:44 +0530

    Content Usage Updater implemented

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/ContentUsageUpdater.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/ContentUsageUpdater.scala
@@ -14,8 +14,10 @@ import org.ekstep.analytics.framework.Filter
 import org.ekstep.analytics.framework.Period._
 import com.datastax.spark.connector._
 import org.ekstep.analytics.util.Constants
+import org.joda.time.LocalDate
+import org.ekstep.analytics.framework.util.JSONUtils
 
-case class ContentDetail(content_id: String, ts: Long, partner_id: String, group_user: Boolean, content_type: String, mime_type: String, publish_date: DateTime, total_ts: Double, total_sessions: Int, avg_ts_session: Double, total_interactions: Long, avg_interactions_min: Double)
+case class ContentDetail(content_id: String, ts: Long, partner_id: String, group_user: Boolean, content_type: String, mime_type: String, publish_date: DateTime, total_ts: Double, total_sessions: Int, avg_ts_session: Double, total_interactions: Long, avg_interactions_min: Double, end_date: Long)
 case class ContentUsageSummary(d_content_id: String, d_period: Int, d_partner_id: String, d_group_user: Boolean, d_content_type: String, d_mime_type: String, m_publish_date: DateTime, m_total_ts: Double, m_total_sessions: Long, m_avg_ts_session: Double, m_total_interactions: Long, m_avg_interactions_min: Double, m_avg_sessions_week: Double, m_avg_ts_week: Double)
 case class PeriodCode(period: Int)
 
@@ -26,6 +28,7 @@ object ContentUsageUpdater extends IBatchModel[MeasuredEvent] with Serializable 
         val contentSummary = events.map { x =>
             val ts = x.syncts
             val startDate = x.context.date_range.from
+            val endDate = x.context.date_range.to
 
             val content_id = x.content_id.get
             val partner_id = x.dimensions.partner_id.get
@@ -40,9 +43,13 @@ object ContentUsageUpdater extends IBatchModel[MeasuredEvent] with Serializable 
             val avg_ts_session = eksMap.get("avg_ts_session").asInstanceOf[Double]
             val total_interactions = eksMap.get("total_interactions").get.asInstanceOf[Long]
             val avg_interactions_min = eksMap.get("avg_interactions_min").get.asInstanceOf[Double]
-            ContentDetail(content_id, ts, partner_id, group_user, content_type, mime_type, publish_date, total_ts, total_sessions, avg_ts_session, total_interactions, avg_interactions_min);
+            ContentDetail(content_id, ts, partner_id, group_user, content_type, mime_type, publish_date, total_ts, total_sessions, avg_ts_session, total_interactions, avg_interactions_min, endDate);
         }
-        null;
+        updateDbWithPeriod(contentSummary, DAY)
+        updateDbWithPeriod(contentSummary, WEEK)
+        updateDbWithPeriod(contentSummary, MONTH)
+        updateDbWithPeriod(contentSummary, CUMULATIVE)
+        contentSummary.map { x => JSONUtils.serialize(x) };
     }
 
     private def updateDbWithPeriod(data: RDD[ContentDetail], period: Period) {
@@ -104,9 +111,41 @@ object ContentUsageUpdater extends IBatchModel[MeasuredEvent] with Serializable 
                 updatedSummary.saveToCassandra(Constants.CONTENT_KEY_SPACE_NAME, Constants.CONTENT_USAGE_SUMMARY_FACT)
 
             case CUMULATIVE =>
-                
-            case _          =>
+                val currentData = data.map { x =>
+                    val periodCode = CommonUtil.getPeriod(x.ts, CUMULATIVE)
+                    (periodCode, (ContentUsageSummary(x.content_id, periodCode, x.partner_id, x.group_user, x.content_type, x.mime_type, x.publish_date, x.total_ts, x.total_sessions, x.avg_ts_session, x.total_interactions, x.avg_interactions_min, 0d, 0d), x.end_date));
+                }
+                val prvData = data.map { x => PeriodCode(CommonUtil.getPeriod(x.ts, CUMULATIVE)) }.joinWithCassandraTable[ContentUsageSummary](Constants.CONTENT_KEY_SPACE_NAME, Constants.CONTENT_USAGE_SUMMARY_FACT).map(f => (f._1.period, f._2))
+                val joinedData = currentData.leftOuterJoin(prvData)
+                val updatedSummary = joinedData.map { x =>
+                    val code = x._1
+                    val prvSumm = x._2._2.getOrElse(null)
+                    val newSumm = x._2._1._1
+                    val end_date = x._2._1._2
+                    if (null != prvSumm) {
+                        val total_ts = newSumm.m_total_ts + prvSumm.m_total_ts
+                        val total_sessions = newSumm.m_total_sessions + prvSumm.m_total_sessions
+                        val avg_ts_session = (total_ts) / (total_sessions)
+                        val total_interactions = newSumm.m_total_interactions + prvSumm.m_total_interactions
+                        val avg_interactions_min = if (total_interactions == 0 || total_ts == 0) 0d else CommonUtil.roundDouble(BigDecimal(total_interactions / (total_ts / 60)).toDouble, 2);
+                        val publis_date = if (newSumm.m_publish_date.isBefore(prvSumm.m_publish_date)) newSumm.m_publish_date else prvSumm.m_publish_date;
+                        val numWeeks = getWeeksBetween(publis_date.getMillis, end_date)
+                        val avg_sessions_week = (total_sessions) / numWeeks
+                        val avg_ts_week = (total_ts) / numWeeks
+                        ContentUsageSummary(prvSumm.d_content_id, code, prvSumm.d_partner_id, prvSumm.d_group_user, prvSumm.d_content_type, prvSumm.d_mime_type, prvSumm.m_publish_date, total_ts, total_sessions, avg_ts_session, total_interactions, avg_interactions_min, avg_sessions_week, avg_ts_week);
+                    } else {
+                        newSumm;
+                    }
+                }
+                updatedSummary.saveToCassandra(Constants.CONTENT_KEY_SPACE_NAME, Constants.CONTENT_USAGE_SUMMARY_FACT)
+            case _ =>
 
         }
+    }
+    private def getWeeksBetween(fromDate: Long, toDate: Long): Int = {
+        val from = new LocalDate(fromDate)
+        val to = new LocalDate(toDate)
+        val dates = CommonUtil.datesBetween(from, to)
+        dates.size / 7
     }
 }
