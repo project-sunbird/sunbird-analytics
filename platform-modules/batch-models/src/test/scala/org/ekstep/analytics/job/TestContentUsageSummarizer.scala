793b60f9aed2ccdae921a6a39f52df67baeb7f28
amit behera <amitb@ilimi.in>
2016-05-25 15:30:32 +0530

    test cases for content usage summary

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/TestContentUsageSummarizer.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/TestContentUsageSummarizer.scala
@@ -10,7 +10,7 @@ import org.ekstep.analytics.framework.Dispatcher
 class TestContentUsageSummarizer extends SparkSpec(null) {
     
     "ContentUsageSummarizer" should "execute the job and shouldn't throw any exception" in {
-        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/content-usage-summary/*"))))), None, None, "org.ekstep.analytics.model.ContentUsageSummary", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestContentUsageSummarizer"), Option(false))
+        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/content-usage-summary/multiple-dates/*"))))), None, None, "org.ekstep.analytics.model.ContentUsageSummary", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestContentUsageSummarizer"), Option(false))
         ContentUsageSummarizer.main(JSONUtils.serialize(config))(Option(sc));
     }
   
