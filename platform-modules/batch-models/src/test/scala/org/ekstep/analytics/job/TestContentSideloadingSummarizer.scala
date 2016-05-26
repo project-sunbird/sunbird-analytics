b10bc35b4b96280c1fbb95940d2e0ce5b42e80c3
sowmya-dixit <sowmyadixit7@gmail.com>
2016-05-26 17:55:38 +0530

    Added content sideloading summarizer job and its test cases.

---
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/TestContentSideloadingSummarizer.scala
@@ -0,0 +1,24 @@
+package org.ekstep.analytics.job
+
+import org.ekstep.analytics.model.SparkSpec
+import org.ekstep.analytics.framework.JobConfig
+import org.ekstep.analytics.framework.Fetcher
+import org.ekstep.analytics.framework.Query
+import org.ekstep.analytics.framework.Filter
+import org.ekstep.analytics.framework.Dispatcher
+import org.ekstep.analytics.framework.util.JSONUtils
+
+class TestContentSideloadingSummarizer extends SparkSpec(null) {
+  
+    "ContentSideloadingSummarizer" should "execute ContentSideloadingSummary job and won't throw any Exception" in {
+
+        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/content-sideloading-summary/test_data_1.log"))))), None, None, "org.ekstep.analytics.model.ContentSideloadingSummary", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("ContentSideloadingSummarizer"), Option(false))
+        ContentSideloadingSummarizer.main(JSONUtils.serialize(config))(Option(sc));
+    }
+    
+    ignore should "execute the job from s3 data" in {
+        
+        val config = JobConfig(Fetcher("s3", None, Option(Array(Query(Option("sandbox-data-store"), Option("raw/"), Option("2016-01-01"), Option("2016-04-14"))))), Option(Array(Filter("eventId","EQ",Option("GE_TRANSFER")))), None, "org.ekstep.analytics.model.ContentSideloadingSummary", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("ContentSideloadingSummarizer"), Option(false))
+        DeviceSpecificationUpdater.main(JSONUtils.serialize(config))(Option(sc));
+    }
+}
