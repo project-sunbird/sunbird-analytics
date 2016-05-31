e46f62d1a21e3e80f1aa634ee751d4213be86874
Soma S Dhavala <soma.dhavala@gmail.com>
2016-05-31 15:49:18 +0530

    take-off ladakh data

--- platform-scripts/Takeoff/runTakeOff.scala
+++ platform-scripts/Takeoff/runTakeOff.scala
@@ -4,17 +4,60 @@ import org.ekstep.analytics.updater._
 import org.ekstep.analytics.model._
 import org.apache.spark.SparkContext
 
+
 val outputPath = "/Users/soma/github/ekStep/Learning-Platform-Analytics/platform-scripts/Takeoff";
 //val inputFile = "/Users/adarsa/ilimi/github/Learning-Platform-Analytics/platform-scripts/RecoEngine/Data/inputRE.txt";
+
 println("### Setting S3 Keys ###")
 CommonUtil.setS3Conf(sc);
 implicit val sparkContext: SparkContext = sc
 println("### Fetching Data with filter ###")
 
-//val queries = Option(Array(Query(Option("prod-data-store"), Option("raw/"), Option("2015-08-01"), Option("2015-12-30"))));
+
+// write raw data to local file
 val queries = Option(Array(Query(Option("prod-data-store"), Option("raw/"), Option("2016-01-01"), Option("2016-04-30"))));
 val rdd = DataFetcher.fetchBatchData[Event](Fetcher("S3", None, queries));
 val takeOffrdd = rdd.filter(e => "org.ekstep.delta".equals(e.gdata.id))
+ 
+
+// session-summary data to local file
+val queries = Option(Array(Query(Option("prod-data-store"), Option("ss/"), Option("2016-01-01"), Option("2016-04-30"))));
+val rdd = DataFetcher.fetchBatchData[MeasuredEvent](Fetcher("S3", None, queries));
+val sessSummaries = DataFilter.filter(rdd, Filter("eid","EQ",Option("ME_SESSION_SUMMARY")));
+val takeoffrdd = sessSummaries.filter(e => "org.ekstep.delta".equals(e.dimensions.gdata.get.id)).cache();
+
+
+// write to file
 val rddAsString = takeOffrdd.map(JSONUtils.serialize(_));
-OutputDispatcher.dispatch(Dispatcher("file", Map("file" -> (outputPath+"/DeltaDump2016tillApril.txt"))), rddAsString);
+OutputDispatcher.dispatch(Dispatcher("file", Map("file" -> (outputPath+"/SSDeltaDump2016tillApril.txt"))), rddAsString);
+
+
+
+// write raw data to local file
+val queries = Option(Array(Query(Option("sandbox-data-store"), Option("raw/"), Option("2016-04-27"), Option("2016-05-07"))));
+val rdd = DataFetcher.fetchBatchData[Event](Fetcher("S3", None, queries));
+val takeOffrdd = rdd.filter(e => "org.ekstep.delta".equals(e.gdata.id))
+val rddAsString = takeOffrdd.map(JSONUtils.serialize(_));
+OutputDispatcher.dispatch(Dispatcher("file", Map("file" -> (outputPath+"/DeltaDumpLadakh.txt"))), rddAsString);
+
+ // session-summary data to local file
+val queries = Option(Array(Query(Option("sandbox-data-store"), Option("ss/"), Option("2016-04-27"), Option("2016-05-07"))));
+val rdd = DataFetcher.fetchBatchData[MeasuredEvent](Fetcher("S3", None, queries));
+val sessSummaries = DataFilter.filter(rdd, Filter("eid","EQ",Option("ME_SESSION_SUMMARY")));
+val takeoffrdd = sessSummaries.filter(e => "org.ekstep.delta".equals(e.dimensions.gdata.get.id)).cache();
+// write to file
+val rddAsString = takeOffrdd.map(JSONUtils.serialize(_));
+OutputDispatcher.dispatch(Dispatcher("file", Map("file" -> (outputPath+"/SSDeltaDumpLadakh.txt"))), rddAsString);
+
+
 System.exit(0)
+
+
+
+System.exit(0)
+
+
+
+// take-off in Lakdakh
+
+
