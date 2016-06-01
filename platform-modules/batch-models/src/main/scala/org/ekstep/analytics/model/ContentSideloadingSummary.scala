f22dfbf755411183791507297721dce3d89004f1
Santhosh Vasabhaktula <santhosh@canopusconsulting.com>
2016-06-01 16:26:12 +0530

    Fix origin map issue

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ContentSideloadingSummary.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ContentSideloadingSummary.scala
@@ -11,63 +11,57 @@ import org.apache.spark.HashPartitioner
 import org.ekstep.analytics.util.Constants
 import com.datastax.spark.connector._
 
-case class ContentSideloading(content_id: String, num_downloads: Long, total_count: Long, num_sideloads: Long, origin_map: Map[String,Double], avg_depth: Double)
-case class ReducedContentDetails(content_id: String,transfer_count: Double, did: String, origin: String, ts: Long)
+case class ContentSideloading(content_id: String, num_downloads: Long, total_count: Long, num_sideloads: Long, origin_map: Map[String, Double], avg_depth: Double)
+case class ReducedContentDetails(content_id: String, transfer_count: Double, did: String, origin: String, ts: Long)
 
 object ContentSideloadingSummary extends IBatchModel[Event] with Serializable {
-  
+
     def execute(data: RDD[Event], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
-      
+
         val events = DataFilter.filter(data, Filter("eid", "EQ", Option("GE_TRANSFER")));
         val filteredEvents = DataFilter.filter(DataFilter.filter(events, Filter("edata.eks.direction", "EQ", Option("IMPORT"))), Filter("edata.eks.datatype", "EQ", Option("CONTENT")));
         val config = jobParams.getOrElse(Map[String, AnyRef]());
         val configMapping = sc.broadcast(config);
-        
-        val reducedData = filteredEvents.map{ event => 
+
+        val reducedData = filteredEvents.map { event =>
             val contents = event.edata.eks.contents
-            contents.map{ f => 
-                ReducedContentDetails(f.get("identifier").get.asInstanceOf[String],f.get("transferCount").get.asInstanceOf[Double],event.did,f.get("origin").get.asInstanceOf[String],CommonUtil.getEventTS(event))
+            contents.map { f =>
+                ReducedContentDetails(f.get("identifier").get.asInstanceOf[String], f.get("transferCount").get.asInstanceOf[Double], event.did, f.get("origin").get.asInstanceOf[String], CommonUtil.getEventTS(event))
             }
-        }.flatMap(f=>f)
-        val contentMap = reducedData.groupBy{x => x.content_id}.partitionBy(new HashPartitioner(JobContext.parallelization))
+        }.flatMap(f => f)
+        val contentMap = reducedData.groupBy { x => x.content_id }.partitionBy(new HashPartitioner(JobContext.parallelization))
         val prevSideloadingSummary = contentMap.map(f => ContentId(f._1)).joinWithCassandraTable[ContentSideloading](Constants.CONTENT_KEY_SPACE_NAME, Constants.CONTENT_SIDELOADING_SUMMARY).map(f => (f._1.content_id, f._2))
         val joinedContentData = contentMap.leftOuterJoin(prevSideloadingSummary)
-        
-        val contentSideloadingSummary = joinedContentData.mapValues{ x => 
-            
+
+        val contentSideloadingSummary = joinedContentData.mapValues { x =>
+
             val newContentMap = x._1
             val contentID = newContentMap.map(y => y.content_id).head
-            val prevContentSummary = x._2.getOrElse(ContentSideloading(contentID,0,0,0,Map(" " -> 0.0),0.0))
+            val prevContentSummary = x._2.getOrElse(ContentSideloading(contentID, 0, 0, 0, Map[String, Double](), 0.0))
             val sortedTsList = newContentMap.map(y => y.ts).toList.sortBy { x => x }
-            val dtRange = DtRange(sortedTsList.head,sortedTsList.last)
-            val num_downloads = newContentMap.map{x => x.transfer_count}.toList.count(_==0.0) + prevContentSummary.num_downloads
-            val total_count = newContentMap.map{y => y.did}.toList.size + prevContentSummary.total_count
+            val dtRange = DtRange(sortedTsList.head, sortedTsList.last)
+            val num_downloads = newContentMap.map { x => x.transfer_count }.toList.count(_ == 0.0) + prevContentSummary.num_downloads
+            val total_count = newContentMap.map { y => y.did }.toList.size + prevContentSummary.total_count
             val num_sideloads = total_count - num_downloads
             //creating Map(origin,max(transfer_count))
-            val currentOriginMap = x._1.groupBy{y => y.origin}.mapValues{z => z.map{x => x.transfer_count}.max}
-            val prevOriginMap = prevContentSummary.origin_map
+            val currentOriginMap = x._1.groupBy { y => y.origin }.mapValues { z => z.map { x => x.transfer_count }.max }.map(f => (f._1, if(f._2 > prevContentSummary.origin_map.getOrElse(f._1, 0.0)) f._2 else prevContentSummary.origin_map.getOrElse(f._1, 0.0)) )
             //Creating new origin map comparing current and previous maps
-            val newOriginMap = currentOriginMap.map{ x =>
-                if(prevOriginMap.get(x._1) != None) 
-                    if(prevOriginMap.get(x._1).get > x._2) (x._1,prevOriginMap.get(x._1).get) 
-                    else (x._1,x._2)
-                else (x._1,x._2)
-            }
-            val avgDepth = CommonUtil.roundDouble((newOriginMap.map{x => x._2}.sum / newOriginMap.size),2)
-            
-            (ContentSideloading(contentID,num_downloads,total_count,num_sideloads,newOriginMap,avgDepth),dtRange)
+            val newOriginMap = prevContentSummary.origin_map ++ currentOriginMap
+            val avgDepth = CommonUtil.roundDouble((newOriginMap.map { x => x._2 }.sum / newOriginMap.size), 2)
+
+            (ContentSideloading(contentID, num_downloads, total_count, num_sideloads, newOriginMap, avgDepth), dtRange)
         }.cache();
-      
+
         contentSideloadingSummary.map(x => x._2._1).saveToCassandra(Constants.CONTENT_KEY_SPACE_NAME, Constants.CONTENT_SIDELOADING_SUMMARY)
-        
+
         contentSideloadingSummary.map { f =>
             getMeasuredEvent(f._2._1, configMapping.value, f._2._2);
         }.map { x => JSONUtils.serialize(x) };
-   }
-    
-   private def getMeasuredEvent(sideloadingSummary:ContentSideloading , config: Map[String, AnyRef], dtRange: DtRange): MeasuredEvent = {
+    }
 
-        val mid = CommonUtil.getMessageId("ME_CONTENT_SIDELOADING_SUMMARY", sideloadingSummary.content_id, null, DtRange(0l,0l));
+    private def getMeasuredEvent(sideloadingSummary: ContentSideloading, config: Map[String, AnyRef], dtRange: DtRange): MeasuredEvent = {
+
+        val mid = CommonUtil.getMessageId("ME_CONTENT_SIDELOADING_SUMMARY", sideloadingSummary.content_id, null, DtRange(0l, 0l));
         val measures = Map(
             "num_downloads" -> sideloadingSummary.num_downloads,
             "num_sideloads" -> sideloadingSummary.num_sideloads,
