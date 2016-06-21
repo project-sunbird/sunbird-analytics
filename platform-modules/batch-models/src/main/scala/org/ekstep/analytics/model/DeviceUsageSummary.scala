5ae70a7e087360d6018d232839d2aade6bb5476d
sowmya-dixit <sowmyadixit7@gmail.com>
2016-06-21 12:56:11 +0530

    Updated DeviceUsageSummary and its test cases with new code structure.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/DeviceUsageSummary.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/DeviceUsageSummary.scala
@@ -14,68 +14,62 @@ import org.ekstep.analytics.util.Constants
 import com.datastax.spark.connector._
 import org.ekstep.analytics.framework.util.JobLogger
 
-case class UsageSummary(device_id: String, start_time: Long, end_time: Long, num_days: Long, total_launches: Long, total_timespent: Double, avg_num_launches: Double, avg_time: Double)
-
+case class UsageSummary(device_id: String, start_time: Long, end_time: Long, num_days: Long, total_launches: Long, total_timespent: Double, avg_num_launches: Double, avg_time: Double) extends AlgoOutput
+case class DeviceUsageInput(device_id: String, currentData: Buffer[DerivedEvent], previousData: Option[UsageSummary]) extends AlgoInput
 case class DeviceId(device_id: String)
 
-object DeviceUsageSummary extends IBatchModel[MeasuredEvent, String] with Serializable {
+object DeviceUsageSummary extends IBatchModelTemplate[DerivedEvent, DeviceUsageInput, UsageSummary, MeasuredEvent] with Serializable {
 
     val className = "org.ekstep.analytics.model.DeviceUsageSummary"
-    def execute(data: RDD[MeasuredEvent], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
-  
-        JobLogger.debug("### Execute method started ###", className);
+
+    override def preProcess(data: RDD[DerivedEvent], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[DeviceUsageInput] = {
         val filteredEvents = DataFilter.filter(data, Filter("eid", "EQ", Option("ME_GENIE_LAUNCH_SUMMARY")));
-        val config = jobParams.getOrElse(Map[String, AnyRef]());
-        val configMapping = sc.broadcast(config);
-  
         val newGroupedEvents = filteredEvents.map(event => (event.dimensions.did.get, Buffer(event)))
             .partitionBy(new HashPartitioner(JobContext.parallelization))
             .reduceByKey((a, b) => a ++ b);
         val prevDeviceSummary = newGroupedEvents.map(f => DeviceId(f._1)).joinWithCassandraTable[UsageSummary](Constants.KEY_SPACE_NAME, Constants.DEVICE_USAGE_SUMMARY_TABLE).map(f => (f._1.device_id, f._2))
         val deviceData = newGroupedEvents.leftOuterJoin(prevDeviceSummary);
+        deviceData.map { x => DeviceUsageInput(x._1, x._2._1, x._2._2) }
+    }
 
-        val deviceUsageSummary = deviceData.mapValues { events =>
-  
-            val eventsSortedByTS = events._1.sortBy { x => x.context.date_range.to };
-            val eventsSortedByDateRange = events._1.sortBy { x => x.context.date_range.from };
-            val prevUsageSummary = events._2.getOrElse(UsageSummary(events._1.head.dimensions.did.get, 0L, 0L, 0L, 0L, 0.0, 0.0, 0.0));
+    override def algorithm(data: RDD[DeviceUsageInput], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[UsageSummary] = {
+
+        val configMapping = sc.broadcast(config);
+        data.map { events =>
+            val eventsSortedByTS = events.currentData.sortBy { x => x.context.date_range.to };
+            val eventsSortedByDateRange = events.currentData.sortBy { x => x.context.date_range.from };
+            val prevUsageSummary = events.previousData.getOrElse(UsageSummary(events.device_id, 0L, 0L, 0L, 0L, 0.0, 0.0, 0.0));
             val tsInString = configMapping.value.getOrElse("startTime", "2015-03-01").asInstanceOf[String]
             val ts = CommonUtil.getTimestamp(tsInString, CommonUtil.dateFormat, "yyyy-MM-dd");
-            val eventStartTime = if(eventsSortedByDateRange.head.context.date_range.from < ts) ts else eventsSortedByDateRange.head.context.date_range.from
+            val eventStartTime = if (eventsSortedByDateRange.head.context.date_range.from < ts) ts else eventsSortedByDateRange.head.context.date_range.from
             val start_time = if (prevUsageSummary.start_time == 0) eventStartTime else if (eventStartTime > prevUsageSummary.start_time) prevUsageSummary.start_time else eventStartTime;
             val end_time = eventsSortedByTS.last.context.date_range.to
             val num_days: Long = CommonUtil.daysBetween(new LocalDate(start_time), new LocalDate(end_time))
-            val num_launches = events._1.size + prevUsageSummary.total_launches
+            val num_launches = events.currentData.size + prevUsageSummary.total_launches
             val avg_num_launches = if (num_days == 0) num_launches else CommonUtil.roundDouble((num_launches / (num_days.asInstanceOf[Double])), 2)
-            val totalTimeSpent = events._1.map { x =>
+            val totalTimeSpent = events.currentData.map { x =>
                 (x.edata.eks.asInstanceOf[Map[String, AnyRef]].get("timeSpent").get.asInstanceOf[Double])
             }.sum + prevUsageSummary.total_timespent
             val avg_time = if (num_days == 0) totalTimeSpent else CommonUtil.roundDouble(totalTimeSpent / num_days, 2)
-  
-            (UsageSummary(events._1.head.dimensions.did.get, start_time, end_time, num_days, num_launches, totalTimeSpent, avg_num_launches, avg_time))
+
+            (UsageSummary(events.device_id, start_time, end_time, num_days, num_launches, totalTimeSpent, avg_num_launches, avg_time))
         }.cache();
-  
-        deviceUsageSummary.map(f => f._2).saveToCassandra(Constants.KEY_SPACE_NAME, Constants.DEVICE_USAGE_SUMMARY_TABLE);
-  
-        JobLogger.debug("### Execute method ended ###", className);
-        deviceUsageSummary.map(f => {
-            getMeasuredEvent(f._2, configMapping.value);
-        }).map { x => JSONUtils.serialize(x) };
-  
     }
 
-    private def getMeasuredEvent(usageSummary: UsageSummary, config: Map[String, AnyRef]): MeasuredEvent = {
-  
-        val mid = CommonUtil.getMessageId("ME_DEVICE_USAGE_SUMMARY", usageSummary.device_id, null, DtRange(0l,0l));
-        val measures = Map(
-            "start_time" -> usageSummary.start_time,
-            "end_time" -> usageSummary.end_time,
-            "num_days" -> usageSummary.num_days,
-            "avg_num_launches" -> usageSummary.avg_num_launches,
-            "avg_time" -> usageSummary.avg_time);
-        MeasuredEvent("ME_DEVICE_USAGE_SUMMARY", System.currentTimeMillis(), usageSummary.end_time, "1.0", mid, "", None, None,
-            Context(PData(config.getOrElse("producerId", "AnalyticsDataPipeline").asInstanceOf[String], config.getOrElse("modelId", "DeviceUsageSummarizer").asInstanceOf[String], config.getOrElse("modelVersion", "1.0").asInstanceOf[String]), None, config.getOrElse("granularity", "DAY").asInstanceOf[String], DtRange(usageSummary.start_time, usageSummary.end_time)),
-            Dimensions(None, Option(usageSummary.device_id), None, None, None, None, None),
-            MEEdata(measures));
+    override def postProcess(data: RDD[UsageSummary], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[MeasuredEvent] = {
+        data.saveToCassandra(Constants.KEY_SPACE_NAME, Constants.DEVICE_USAGE_SUMMARY_TABLE);
+        data.map { usageSummary =>
+            val mid = CommonUtil.getMessageId("ME_DEVICE_USAGE_SUMMARY", usageSummary.device_id, null, DtRange(0l, 0l));
+            val measures = Map(
+                "start_time" -> usageSummary.start_time,
+                "end_time" -> usageSummary.end_time,
+                "num_days" -> usageSummary.num_days,
+                "avg_num_launches" -> usageSummary.avg_num_launches,
+                "avg_time" -> usageSummary.avg_time);
+            MeasuredEvent("ME_DEVICE_USAGE_SUMMARY", System.currentTimeMillis(), usageSummary.end_time, "1.0", mid, "", None, None,
+                Context(PData(config.getOrElse("producerId", "AnalyticsDataPipeline").asInstanceOf[String], config.getOrElse("modelId", "DeviceUsageSummarizer").asInstanceOf[String], config.getOrElse("modelVersion", "1.0").asInstanceOf[String]), None, config.getOrElse("granularity", "DAY").asInstanceOf[String], DtRange(usageSummary.start_time, usageSummary.end_time)),
+                Dimensions(None, Option(usageSummary.device_id), None, None, None, None, None),
+                MEEdata(measures));
+        }
     }
 }
--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestDeviceUsageSummary.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestDeviceUsageSummary.scala
@@ -1,6 +1,7 @@
 package org.ekstep.analytics.model
 
 import org.ekstep.analytics.framework.MeasuredEvent
+import org.ekstep.analytics.framework.DerivedEvent
 import org.ekstep.analytics.framework.util.JSONUtils
 import com.datastax.spark.connector.cql.CassandraConnector
 
@@ -11,33 +12,33 @@ class TestDeviceUsageSummary extends SparkSpec(null) {
         CassandraConnector(sc.getConf).withSessionDo { session =>
             session.execute("TRUNCATE learner_db.device_usage_summary;");
         }
-        
-        val rdd1 = loadFile[MeasuredEvent]("src/test/resources/device-usage-summary/test_data_1.log");
+
+        val rdd1 = loadFile[DerivedEvent]("src/test/resources/device-usage-summary/test_data_1.log");
         val rdd2 = DeviceUsageSummary.execute(rdd1, Option(Map("modelId" -> "DeviceUsageSummarizer", "granularity" -> "DAY")));
         val me = rdd2.collect()
         me.length should be(1)
-        val event1 = JSONUtils.deserialize[MeasuredEvent](me(0));
+        val event1 = me(0);
 
         event1.eid should be("ME_DEVICE_USAGE_SUMMARY");
         event1.context.pdata.model should be("DeviceUsageSummarizer");
         event1.context.pdata.ver should be("1.0");
         event1.context.granularity should be("DAY");
         event1.context.date_range should not be null;
-  
+
         val eks = event1.edata.eks.asInstanceOf[Map[String, AnyRef]]
         eks.get("num_days").get should be(12)
         eks.get("start_time").get should be(1460627512768L)
         eks.get("avg_time").get should be(2.5)
         eks.get("avg_num_launches").get should be(0.25)
         eks.get("end_time").get should be(1461669647260L)
-  
-        val rdd3 = loadFile[MeasuredEvent]("src/test/resources/device-usage-summary/test_data_2.log");
+
+        val rdd3 = loadFile[DerivedEvent]("src/test/resources/device-usage-summary/test_data_2.log");
         val rdd4 = DeviceUsageSummary.execute(rdd3, Option(Map("modelVersion" -> "1.0", "producerId" -> "AnalyticsDataPipeline")));
         val me2 = rdd4.collect()
         me2.length should be(2)
-  
-        val event2 = JSONUtils.deserialize[MeasuredEvent](me2(1));
-  
+
+        val event2 = me2(1);
+
         event2.eid should be("ME_DEVICE_USAGE_SUMMARY");
         event2.context.pdata.model should be("DeviceUsageSummarizer");
         event2.context.pdata.ver should be("1.0");
@@ -50,15 +51,15 @@ class TestDeviceUsageSummary extends SparkSpec(null) {
         eks2.get("avg_time").get should be(1.2)
         eks2.get("avg_num_launches").get should be(0.09)
         eks2.get("end_time").get should be(1462869647260L)
-  
-        val event3 = JSONUtils.deserialize[MeasuredEvent](me2(0));
-  
+
+        val event3 = me2(0);
+
         event3.eid should be("ME_DEVICE_USAGE_SUMMARY");
         event3.context.pdata.model should be("DeviceUsageSummarizer");
         event3.context.pdata.ver should be("1.0");
         event3.context.granularity should be("DAY");
         event3.context.date_range should not be null;
-  
+
         val eks3 = event3.edata.eks.asInstanceOf[Map[String, AnyRef]]
         eks3.get("num_days").get should be(0)
         eks3.get("start_time").get should be(1460627674628L)
@@ -66,25 +67,25 @@ class TestDeviceUsageSummary extends SparkSpec(null) {
         eks3.get("avg_num_launches").get should be(5.0)
         eks3.get("end_time").get should be(1460627674628L)
     }
-    
+
     it should "generate DeviceUsageSummary event with start_time = March 1st 2015 " in {
 
         CassandraConnector(sc.getConf).withSessionDo { session =>
             session.execute("TRUNCATE learner_db.device_usage_summary;");
         }
-        
-        val rdd1 = loadFile[MeasuredEvent]("src/test/resources/device-usage-summary/test_data_4.log");
+
+        val rdd1 = loadFile[DerivedEvent]("src/test/resources/device-usage-summary/test_data_4.log");
         val rdd2 = DeviceUsageSummary.execute(rdd1, Option(Map("modelId" -> "DeviceUsageSummarizer", "granularity" -> "DAY")));
         val me = rdd2.collect()
         me.length should be(1)
-        val event1 = JSONUtils.deserialize[MeasuredEvent](me(0));
-        
+        val event1 = me(0);
+
         event1.eid should be("ME_DEVICE_USAGE_SUMMARY");
         event1.context.pdata.model should be("DeviceUsageSummarizer");
         event1.context.pdata.ver should be("1.0");
         event1.context.granularity should be("DAY");
         event1.context.date_range should not be null;
-  
+
         val eks = event1.edata.eks.asInstanceOf[Map[String, AnyRef]]
         eks.get("num_days").get should be(410)
         eks.get("start_time").get should be(1425168000000L)
