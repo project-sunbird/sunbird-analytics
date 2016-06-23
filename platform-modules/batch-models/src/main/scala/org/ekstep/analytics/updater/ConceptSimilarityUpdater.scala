cad0b1453d910c730a4f8daa15bb5bab3cfe2bfa
sowmya-dixit <sowmyadixit7@gmail.com>
2016-06-23 12:40:13 +0530

    Updated ConceptSimilarityUpdater with new code structure and its test case.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/ConceptSimilarityUpdater.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/ConceptSimilarityUpdater.scala
@@ -9,33 +9,28 @@ import org.ekstep.analytics.framework.util.JSONUtils
 import org.ekstep.analytics.util.Constants
 import org.ekstep.analytics.framework.util.JobLogger
 
-case class ConceptSimilarity(concept1: String, concept2: String, relation_type: String, sim: Double)
-case class ConceptSimilarityEntity(startNodeId: String, endNodeId: String, similarity: List[Map[String, AnyRef]])
+case class ConceptSimilarity(concept1: String, concept2: String, relation_type: String, sim: Double) extends AlgoOutput with Output
+case class ConceptSimilarityEntity(startNodeId: String, endNodeId: String, similarity: List[Map[String, AnyRef]]) extends AlgoInput with Input
 
-object ConceptSimilarityUpdater extends IBatchModel[ConceptSimilarityEntity, String] with Serializable {
+object ConceptSimilarityUpdater extends IBatchModelTemplate[ConceptSimilarityEntity, ConceptSimilarityEntity, ConceptSimilarity, ConceptSimilarity] with Serializable {
 
     val className = "org.ekstep.analytics.updater.ConceptSimilarityUpdater"
-    
-    def execute(jsonLines: RDD[ConceptSimilarityEntity], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
 
-        JobLogger.debug("Execute method started", className)
-        val similarity = jsonLines.map { x =>
-           x.similarity.map(f => {
-               ConceptSimilarity(x.startNodeId, x.endNodeId, f.get("relationType").get.asInstanceOf[String], f.get("sim").get.asInstanceOf[Double])
-           });
-       }.flatMap { x => x.map { x => x } }
-       
+    override def preProcess(data: RDD[ConceptSimilarityEntity], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[ConceptSimilarityEntity] = {
+        data
+    }
+
+    override def algorithm(data: RDD[ConceptSimilarityEntity], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[ConceptSimilarity] = {
+        data.map { x =>
+            x.similarity.map(f => {
+                ConceptSimilarity(x.startNodeId, x.endNodeId, f.get("relationType").get.asInstanceOf[String], f.get("sim").get.asInstanceOf[Double])
+            });
+        }.flatMap { x => x.map { x => x } }
+    }
+
+    override def postProcess(data: RDD[ConceptSimilarity], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[ConceptSimilarity] = {
         JobLogger.debug("Saving concept & similarity value to DB ", className)
-        similarity.saveToCassandra(Constants.KEY_SPACE_NAME, Constants.CONCEPT_SIMILARITY_TABLE);
-        
-        JobLogger.debug("Execute method ended", className)
-        similarity.map { x =>
-            val similarityMap = Map(
-                "concept1" -> x.concept1,
-                "concept2" -> x.concept2,
-                "relation_type" -> x.relation_type,
-                "similarity_value" -> x.sim);
-            JSONUtils.serialize(similarityMap);
-        };
+        data.saveToCassandra(Constants.KEY_SPACE_NAME, Constants.CONCEPT_SIMILARITY_TABLE);
+        data
     }
 }
