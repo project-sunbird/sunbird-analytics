dd3ff9d566b5dba91731b8186a32610b0671380a
sowmya-dixit <sowmyadixit7@gmail.com>
2016-06-23 13:47:34 +0530

    Updated UpdateContentPopularity with new code structure and its test case.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdateContentPopularity.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdateContentPopularity.scala
@@ -18,24 +18,32 @@ import org.ekstep.analytics.framework.util.RestUtil
 import org.ekstep.analytics.framework.Response
 import org.ekstep.analytics.framework.ContentId
 
+case class PopularityUpdaterInput(contentId: String, contentSummary: ContentSummary) extends AlgoInput
+case class PopularityUpdaterOutut(contentId: String, popularity: Long, reponseCode: String, errorMsg: Option[String]) extends AlgoOutput with Output
+
 /**
  * @author Santhosh
  */
-object UpdateContentPopularity extends IBatchModel[MeasuredEvent, String] with Serializable {
+object UpdateContentPopularity extends IBatchModelTemplate[DerivedEvent, PopularityUpdaterInput, PopularityUpdaterOutut, PopularityUpdaterOutut] with Serializable {
 
     val className = "org.ekstep.analytics.updater.UpdateContentPopularity"
-    
-    def execute(data: RDD[MeasuredEvent], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
 
-        JobLogger.debug("Execute method started", className)
+    override def preProcess(data: RDD[DerivedEvent], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[PopularityUpdaterInput] = {
         val contents = DataFilter.filter(data, Filter("eid", "EQ", Option("ME_CONTENT_SUMMARY"))).map { x => ContentId(x.dimensions.gdata.get.id) }.distinct();
         val summaries = contents.joinWithCassandraTable[ContentSummary](Constants.CONTENT_KEY_SPACE_NAME, Constants.CONTENT_CUMULATIVE_SUMMARY_TABLE);
-        JobLogger.debug("Execute method ended", className)
-        summaries.map { x =>  
-            val url = org.ekstep.analytics.framework.util.Constants.getContentUpdateAPIUrl(x._1.content_id);
-            val request = Map("request" -> Map("content" -> Map("popularity" -> x._2.total_num_sessions)));
+        summaries.map { x => PopularityUpdaterInput(x._1.content_id, x._2) }
+    }
+
+    override def algorithm(data: RDD[PopularityUpdaterInput], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[PopularityUpdaterOutut] = {
+        data.map { x =>
+            val url = org.ekstep.analytics.framework.util.Constants.getContentUpdateAPIUrl(x.contentId);
+            val request = Map("request" -> Map("content" -> Map("popularity" -> x.contentSummary.total_num_sessions)));
             val r = RestUtil.patch[Response](url, JSONUtils.serialize(request));
-            JSONUtils.serialize(Map("contentId" -> x._1.content_id, "popularity" -> x._2.total_num_sessions, "responseCode" -> r.responseCode, "errMsg" -> r.params.errmsg));
+            PopularityUpdaterOutut(x.contentId, x.contentSummary.total_num_sessions, r.responseCode, r.params.errmsg)
         };
+    }
+
+    override def postProcess(data: RDD[PopularityUpdaterOutut], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[PopularityUpdaterOutut] = {
+        data
     }
 }
--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/updater/TestUpdateContentPopularity.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/updater/TestUpdateContentPopularity.scala
@@ -20,7 +20,7 @@ class TestUpdateContentPopularity extends SparkSpec(null) {
         val testContent = ContentSummary("org.ekstep.story.hi.nature",DateTime.now,5,0d,0d,100,0d,0d,0d,"Story","application/vnd.ekstep.ecml-archive")
         val testRdd = sc.parallelize(Array(testContent));
         testRdd.saveToCassandra(Constants.CONTENT_KEY_SPACE_NAME, Constants.CONTENT_CUMULATIVE_SUMMARY_TABLE)
-        val rdd = DataFetcher.fetchBatchData[MeasuredEvent](Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/content-popularity/test-data.json"))))));
+        val rdd = DataFetcher.fetchBatchData[DerivedEvent](Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/content-popularity/test-data.json"))))));
         val rdd2 = UpdateContentPopularity.execute(rdd, Option(Map()));
         var out = rdd2.collect();
         out.length should be(1);
