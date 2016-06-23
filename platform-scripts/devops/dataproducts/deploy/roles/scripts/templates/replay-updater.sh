ee2ccabb4fe5b540c5eb75b094ead6488beccbf1
Manojvv <manojv@ilimi.in>
2016-06-23 17:28:13 +0530

    Added scripts to ansible folder

---
+++ platform-scripts/devops/dataproducts/deploy/roles/scripts/templates/replay-updater.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+export SPARK_HOME=/home/ec2-user/spark-1.5.2-bin-hadoop2.3
+export MODELS_HOME=/mnt/data/analytics/models
+cd /mnt/data/analytics/scripts
+source model-config.sh
+source replay-utils.sh
+
+job_config=$(config $1 '__endDate__')
+start_date=$2
+end_date=$3
+
+echo "Running the $1 updater replay..." >> "logs/$end_date-$1-replay.log"
+$SPARK_HOME/bin/spark-submit --master local[*] --jars $MODELS_HOME/analytics-framework-1.0.jar --class org.ekstep.analytics.job.ReplaySupervisor $MODELS_HOME/batch-models-1.0.jar --model "$1" --fromDate "$start_date" --toDate "$end_date" --config "$job_config" >> "logs/$end_date-$1-replay.log"
+
+if [ $? == 0 ] 
+	then
+  	echo "$1 updater replay executed successfully..." >> "logs/$end_date-$1-replay.log"
+else
+ 	echo "$1 updater replay failed" >> "logs/$end_date-$1-replay.log"
+ 	exit 1
+fi
