7373eb097a36878ce7122980360bf0c99306234f
Manojvv <manojv@ilimi.in>
2016-06-24 19:00:38 +0530

    Add  env specific scripts

--- platform-scripts/devops/secor/deploy/roles/start-secor-raw/files/secor-script.sh
+++ platform-scripts/devops/secor/deploy/roles/start-secor-raw/files/secor-script.sh
@@ -10,7 +10,7 @@
 
  case $1 in
     start)
-       echo $$ > /var/run/secor-script.pid;
+       echo $$ > /var/run/secor-script.pid
        cd /mnt/secor-raw
        exec 2>&1 nohup java -Xms256M -Xmx512M -ea -Dsecor_group=me -Dlog4j.configuration=log4j.{{ env }}.properties -Dconfig=secor.{{ env }}.partition.properties - cp secor-me/secor-{{ secor.version }}-SNAPSHOT.jar:lib/* com.pinterest.secor.main.ConsumerMain & 1>/tmp/secor-script.out 
        ;;
