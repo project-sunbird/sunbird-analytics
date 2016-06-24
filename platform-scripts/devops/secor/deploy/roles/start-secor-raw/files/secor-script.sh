ebfaaafb9a1241de90bb47fe395078fb1d2633f6
Manojvv <manojv@ilimi.in>
2016-06-24 14:15:09 +0530

    Added Start and Stop scripts for decor

---
+++ platform-scripts/devops/secor/deploy/roles/start-secor-raw/files/secor-script.sh
@@ -0,0 +1,31 @@
+
+
+ #check process secor-script with pidfile /var/run/secor-script.pid
+ #      start = "/bin/secor-script start"
+ #      stop = "/bin/secor-script stop"
+
+#The wrapper script:
+ #!/bin/bash
+ CLASSPATH=secor-0.2-SNAPSHOT.jar
+
+ case $1 in
+    start)
+       echo $$ > /var/run/secor-script.pid;
+       cd /mnt/secor-raw
+       exec 2>&1 nohup java -Xms256M -Xmx512M -ea -Dsecor_group=me -Dlog4j.configuration=log4j.{{ env }}.properties -Dconfig=secor.{{ env }}.partition.properties - cp secor-me/secor-{{ secor.version }}-SNAPSHOT.jar:lib/* com.pinterest.secor.main.ConsumerMain & 1>/tmp/xyz.out 
+       ;;
+     
+     stop)  
+       kill `cat /var/run/secor-script.pid` ;;
+     
+     check)
+       if [ -f /var/run/secor-script.pid ] then;
+          echo "SECOR PROCESS IS RUNNING"
+        else
+          echo "SECOR PROCESS IS NOT RUNNING"
+
+     
+     *)  
+       echo "usage: secor-script {start|stop|check}" ;;
+ esac
+ exit 0
