d25118fda733bcadc4c1706698af8ffd29d58577
Santhosh <santhosh@Santhoshs-MacBook-Pro-2.local>
2016-06-26 02:23:07 +0530

    Updated secor monitoring script

--- platform-scripts/shell/monitoring/monitor-secor.sh
+++ platform-scripts/shell/monitoring/monitor-secor.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 log_file=$1
+mType=$2
 
 warn=`grep " WARN " $log_file`
 errors=`grep " ERROR " $log_file`
 upload=`grep " INFO  uploading file " $log_file`
 
-warn_count=`echo "$warn" | wc -l`
-upload_count=`echo "$upload" | wc -l`
-errors_count="0"
+warn_count=`echo "$warn" | wc -l | bc`
+upload_count=`echo "$upload" | wc -l | bc`
+errors_count=0
 if [ "$errors" != "" ]; then
-	errors_count=`echo "$errors" | wc -l`
+	errors_count=`echo "$errors" | wc -l | bc`
 fi
+
+echo "Warn Count: $warn_count"
+echo "Upload Count: $upload_count"
+echo "Errors Count: $errors_count"
 
 today=$(date "+%Y-%m-%d")
 
@@ -19,32 +24,30 @@ file_names=""
 while read -r line
 do
 	f=`sed 's/.*s3n:\(.*\).*/\1/' <<< "$line"`
-	file_names="$file_names s3n:$f\n"
+	file_names+="s3:$f\n"
 done <<< "$upload"
 
+echo "## Logging errors "
+file_content="Type,Message\n"
 ## Errors
-error_msg=""
 while read -r line
 do
 	msg=`sed 's/.*ERROR \(.*\).*/\1/' <<< "$line"`
-	error_msg="$error_msg$msg\n"
+	file_content+="ERROR,$msg\n"
 done <<< "$errors"
+echo "## errors logged "
 
+echo "## Logging warning "
 ##Warnings
-warn_msg=""
 while read -r line
 do
 	msg=`sed 's/.*WARN \(.*\).*/\1/' <<< "$line"`
-	warn_msg="$warn_msg$msg\n"
+	file_content+="WARN,$msg\n"
 done <<< "$warn"
+echo "## warnings logged "
 
-echo "Warn Count: $warn_count"
-echo "Upload Count: $upload_count"
-echo "Errors Count: $errors_count"
-echo -e $file_names
+echo -e $file_content > secor-monitor-$today.csv
 
-msg="$warn_msg$error_msg"
-echo -e $msg >> secor-monitor-$today.txt
-
-data='{"channel": "#test_webhooks", "username": "Monitoring-Secor", "text":"*-:Secor Monitoring Status Report:-*\n *Number of Uploaded Files: * `'$upload_count'` \n *Number of Warnings: * `'$warn_count'` \n *Number of Errors: * `'$errors_count'` \n\n File Names:\n```'$file_names'```", "icon_emoji": ":sunglasses:"}'
+data='{"channel": "#analytics_monitoring", "username": "secor-monitor", "text":"*Secor | Monitoring Report | '$today' | '$mType'*\nFiles Uploaded: `'$upload_count'` \n Warnings: `'$warn_count'` \n Errors: `'$errors_count'` \n\nUploaded Files:\n\n```'$file_names'```", "icon_emoji": ":ghost:"}'
+#echo $data
 curl -X POST -H 'Content-Type: application/json' --data "$data" https://hooks.slack.com/services/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
