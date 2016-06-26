c1c6982770c48432dc6fa193d7d297777a95daa0
Santhosh <santhosh@Santhoshs-MacBook-Pro-2.local>
2016-06-26 22:02:45 +0530

    Secor setup - ansible changes

--- platform-scripts/devops/secor/deploy/roles/start-secor-raw/files/secor-script.sh
+++ platform-scripts/devops/secor/deploy/roles/start-secor-raw/files/secor-script.sh
@@ -1,23 +1,22 @@
 
 
- #check process secor-script with pidfile /var/run/secor-script.pid
- #      start = "/bin/secor-script start"
- #      stop = "/bin/secor-script stop"
+# check process secor-script with pidfile /var/run/secor-script.pid
+# start = "/bin/secor-script start"
+# stop = "/bin/secor-script stop"
 
 #The wrapper script:
- #!/bin/bash
- CLASSPATH=secor-0.2-SNAPSHOT.jar
+#!/bin/bash
 
- case $1 in
+case $1 in
     start)
        echo $$ > /var/run/secor-script.pid
-       cd /mnt/secor-raw
-       exec 2>&1 nohup java -Xms256M -Xmx512M -ea -Dsecor_group=me -Dlog4j.configuration=log4j.{{ env }}.properties -Dconfig=secor.{{ env }}.partition.properties - cp secor-me/secor-{{ secor.version }}-SNAPSHOT.jar:lib/* com.pinterest.secor.main.ConsumerMain & 1>/tmp/secor-script.out 
+       cd /home/ec2-user/secor-raw
+       exec 2>&1 nohup java -Xms256M -Xmx512M -ea -Dsecor_group=raw -Dlog4j.configuration=log4j.{{ env }}.properties -Dconfig=secor.{{ env }}.partition.properties - cp secor-{{ secor.version }}-SNAPSHOT.jar:lib/* com.pinterest.secor.main.ConsumerMain & 1>nohup.out 
        ;;
      
      stop)  
        kill `cat /var/run/secor-script.pid` ;;
      *)  
        echo "usage: secor-script {start|stop|check}" ;;
- esac
- exit 0
+esac
+exit 0
