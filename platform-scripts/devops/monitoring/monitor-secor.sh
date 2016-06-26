ea01bb5140ea3d700ba51a068c8176dc420204f4
Santhosh <santhosh@Santhoshs-MacBook-Pro-2.local>
2016-06-26 13:53:07 +0530

    update data products setup scripts

--- platform-scripts/devops/monitoring/monitor-secor.sh
+++ platform-scripts/devops/monitoring/monitor-secor.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 log_file=$1
 mType=$2
+today=$(date "+%Y-%m-%d")
+output_dir="/mnt/secor/reports"
+#output_dir="/tmp"
+
+if [ "$mType" == "me" ]; then
+	title="Secor | Monitoring Report | '$today' | Derived Telemetry"
+	output_fname="$output_dir/secor-me-monitor-$today.csv"
+else
+	title="Secor | Monitoring Report | '$today' | Raw Telemetry"
+	output_fname="$output_dir/secor-raw-monitor-$today.csv"
+fi
 
 warn=`grep " WARN " $log_file`
 errors=`grep " ERROR " $log_file`
@@ -16,8 +27,6 @@ fi
 echo "Warn Count: $warn_count"
 echo "Upload Count: $upload_count"
 echo "Errors Count: $errors_count"
-
-today=$(date "+%Y-%m-%d")
 
 ## Upload Info
 file_names=""
@@ -46,8 +55,8 @@ do
 done <<< "$warn"
 echo "## warnings logged "
 
-echo -e $file_content > secor-monitor-$today.csv
+echo -e $file_content > $output_fname
 
-data='{"channel": "#analytics_monitoring", "username": "secor-monitor", "text":"*Secor | Monitoring Report | '$today' | '$mType'*\nFiles Uploaded: `'$upload_count'` \n Warnings: `'$warn_count'` \n Errors: `'$errors_count'` \n\nUploaded Files:\n\n```'$file_names'```", "icon_emoji": ":ghost:"}'
+data='{"channel": "#analytics_monitoring", "username": "secor-monitor", "text":"*'$title'*\nFiles Uploaded: `'$upload_count'` \n Warnings: `'$warn_count'` \n Errors: `'$errors_count'` \n\nUploaded Files:\n\n```'$file_names'```", "icon_emoji": ":ghost:"}'
 #echo $data
 curl -X POST -H 'Content-Type: application/json' --data "$data" https://hooks.slack.com/services/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
