d86ced240342ba8cbc103c1062b2f4192a679b95
Santhosh <santhosh@Santhoshs-MacBook-Pro-2.local>
2016-07-05 00:45:03 +0530

    1. Remove unused methods in Constants file 2. Ignore AppDBUtils while checking code coverage

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/util/Constants.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/util/Constants.scala
@@ -21,14 +21,6 @@ object Constants {
     val CONTENT_SIDELOADING_SUMMARY = "content_sideloading_summary";
     val LP_URL = AppConf.getConfig("lp.url");
 
-    def getContentAPIUrl(contentId: String): String = {
-        s"$LP_URL/learning-service/v2/content/$contentId";
-    }
-
-    def getGameList(): String = {
-        s"$LP_URL/learning-service/v1/game/list";
-    }
-    
     def getContentList(): String = {
         s"$LP_URL/learning-service/v2/analytics/content/list";
     }
@@ -43,22 +35,6 @@ object Constants {
 
     def getItemConcept(version: String, contentId: String, itemId: String): String = {
         s"$LP_URL/learning-service/$version/analytics/item/$contentId/$itemId";
-    }
-    
-    def getQuestionnaireAPIUrl(questionnaireId: String, subject: String): String = {
-        s"$LP_URL/learning-service/v1/questionnaire/$questionnaireId?taxonomyId=$subject";
-    }
-
-    def getItemAPIUrl(itemId: String, subject: String): String = {
-        s"$LP_URL/learning-service/v1/assessmentitem/$itemId?taxonomyId=$subject";
-    }
-
-    def getSearchItemAPIUrl(subject: String): String = {
-        s"$LP_URL/learning-service/v1/assessmentitem/search?taxonomyId=$subject";
-    }
-
-    def getItemSetAPIUrl(itemSetId: String, subject: String): String = {
-        s"$LP_URL/learning-service/v1/assessmentitemset/$itemSetId?taxonomyId=$subject";
     }
     
     def getContentUpdateAPIUrl(contentId: String): String = {
