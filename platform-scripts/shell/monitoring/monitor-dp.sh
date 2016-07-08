478b52b384913f74bda1967dbea26eb9ab8c1f6f
amit behera <amitb@ilimi.in>
2016-07-08 12:33:26 +0530

    updated monito-dp

--- platform-scripts/shell/monitoring/monitor-dp.sh
+++ platform-scripts/shell/monitoring/monitor-dp.sh
@@ -15,7 +15,7 @@ failed_num=0
 total_events=0
 total_time=0
 
-file_content="Model,Job Status,Events Count,Time Taken(in Seconds),Date,Message\n"
+file_content="Model,Job Status,Input Events Count, Output Events Count,Time Taken(in Seconds),Date\n"
 job_status="COMPLETED"
 
 if [ "$comp_jobs" != "" ]; then
@@ -23,16 +23,16 @@ if [ "$comp_jobs" != "" ]; then
 	while read -r line
 	do	
 	    model=`sed 's/.*model":"\(.*\)","ver.*/\1/' <<< "$line" | sed -e "s/org.ekstep.analytics.model.//g" | sed -e "s/org.ekstep.analytics.updater.//g"`
-		message=`sed 's/.*message":"\(.*\)","class.*/\1/' <<< "$line"`
-		
-		events=`sed 's/.*","events":\(.*\),"timeTaken".*/\1/' <<< "$line"`
-		
+		#message=`sed 's/.*message":"\(.*\)","class.*/\1/' <<< "$line"`
+
+		eventsIn=`sed 's/.*","inputEvents":\(.*\),"outputEvents":.*/\1/' <<< "$line"`		
+		events=`sed 's/.*"outputEvents":\(.*\),"timeTaken".*/\1/' <<< "$line"`
 		total_events=$((total_events + events))
 		
 		time_taken=`sed 's/.*,"timeTaken":\(.*\)},".*/\1/' <<< "$line"`
 		total_time=`echo $total_time + $time_taken | bc`		
-		event_date=`sed 's/.*{"date":"\(.*\)","events":.*/\1/' <<< "$line"`
-		file_content+="$model,$job_status,$events,$time_taken,$event_date,$message\n"
+		event_date=`sed 's/.*{"date":"\(.*\)","inputEvents":.*/\1/' <<< "$line"`
+		file_content+="$model,$job_status,$eventsIn,$events,$time_taken,$event_date\n"
 	done <<< "$comp_jobs"
 fi
 
@@ -46,9 +46,10 @@ if [ "$failed_jobs" != "" ]; then
 	while read -r line
 	do
 		model=`sed 's/.*model":"\(.*\)","ver.*/\1/' <<< "$line" | sed -e "s/org.ekstep.analytics.model.//g" | sed -e "s/org.ekstep.analytics.updater.//g"`
-		message=`grep -Po '(?<="localizedMessage":")[^"]*' <<< "$line"`
-		#event_date=`sed 's/.*"data":{"date":"\(.*\)"},"message":.*/\1/' <<< "$line"`
-		file_content+="$model,$job_status,$events,$time_taken,$event_date,$message\n"
+		#message=`grep -Po '(?<="localizedMessage":")[^"]*' <<< "$line"`
+		eventsIn=`sed 's/.*","inputEvents":\(.*\)},".*/\1/' <<< "$line"`
+		event_date=`sed 's/.*"data":{"date":"\(.*\)","inputEvents".*/\1/' <<< "$line"`
+		file_content+="$model,$job_status,$eventsIn,$events,$time_taken,$event_date\n"
 	done <<< "$failed_jobs"
 fi
 
@@ -61,5 +62,5 @@ echo "Total time taken: $total_time"
 echo "Total events generated: $total_events"
 
 data='{"channel": "#test_webhooks", "username": "dp-monitor", "text":"*Jobs | Monitoring Report | '$today'*\nNumber of Completed Jobs: `'$comp_num'` \nNumber of failed Jobs: `'$failed_num'` \nTotal time taken: `'$total_time'`\nTotal events generated: `'$total_events'`\n\nDetailed Report:\n```'$file_content'```", "icon_emoji": ":ghost:"}'
-
+echo "$data"
 curl -X POST -H 'Content-Type: application/json' --data "$data" https://hooks.slack.com/services/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
