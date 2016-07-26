5e0d986c0c4ac172489ae2c2381e685dad581870
Santhosh <santhosh@Santhoshs-MacBook-Pro-2.local>
2016-07-27 00:30:51 +0530

    update api service to restart spark context due to stale errors

--- platform-api/analytics-api/app/filter/RequestInterceptor.scala
+++ platform-api/analytics-api/app/filter/RequestInterceptor.scala
@@ -1,10 +1,12 @@
 package filter
 import play.api.mvc._
 import scala.concurrent.Future
+import context.Context
 
 object RequestInterceptor extends Filter {
     
     def apply(next: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
+        Context.checkSparkContext();
         val result = next(request)
         result
     }
