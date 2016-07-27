c61c36bc17af80165c8153eeb32e8790f8bc7b0c
Ajit Barik <ajitb@ilimi.in>
2016-07-27 23:23:26 +0530

    changes for dev concept map link

--- platform-scripts/python/main/vidyavaani/content/get_concepts.py
+++ platform-scripts/python/main/vidyavaani/content/get_concepts.py
@@ -20,12 +20,11 @@ def getConcepts(URL="https://api.ekstep.in/learning/v2/domains/numeracy/concepts
 		resp=requests.get(URL).json()
 		conceptList=[]
 		for i in resp['result']['concepts']:
-			for k in i.keys():
-				try:
-					conceptList.index(i['identifier'])
-				except:
-					conceptList.append(i['identifier'])
-	except:
+			try:
+				conceptList.index(i['identifier'])
+			except:
+				conceptList.append(i['identifier'])
+	except:	
 		print("Bad internet")
 	with codecs.open(conceptListFile,"a",encoding="utf-8") as f:
 		f.write(",".join(conceptList))
