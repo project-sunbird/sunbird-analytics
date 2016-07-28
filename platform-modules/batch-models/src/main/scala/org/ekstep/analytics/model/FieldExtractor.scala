f30fa9e65f74ece734a9af799e34eae06fefbeb2
Santhosh <santhosh@Santhoshs-MacBook-Pro-2.local>
2016-07-28 20:18:28 +0530

    escape json and other string for csv output

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/FieldExtractor.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/FieldExtractor.scala
@@ -7,11 +7,12 @@ import org.apache.spark.SparkContext
 import org.ekstep.analytics.framework.util.JSONUtils
 import org.apache.commons.beanutils.PropertyUtils
 import org.ekstep.analytics.framework.Event
+import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.StringEscapeUtils
 
-object EventFieldExtractor extends IBatchModel[Event, String] with Serializable {
+trait FieldExtractor extends Serializable {
 
-    def execute(events: RDD[Event], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
-
+    def serializeToCSV[T](events: RDD[T], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
         val config = jobParams.getOrElse(Map());
 
         val keys = config.getOrElse("fields", "eid").asInstanceOf[String];
@@ -20,28 +21,37 @@ object EventFieldExtractor extends IBatchModel[Event, String] with Serializable 
 
         val headerRDD = sc.parallelize(Array(headers), 1)
         val valuesRDD = events.map { x =>
-            for (f <- fields) yield PropertyUtils.getProperty(x, f);
-        }.map(_.mkString(","));
-        headerRDD.union(valuesRDD);
+            for (f <- fields) yield getValue(x, f);
+        };
+        valuesRDD.collect();
+        
+        headerRDD.union(valuesRDD.map(_.mkString(",")));
     }
-
+    
+    def getValue[T](event: T, field: String) : String = {
+        val propValue = PropertyUtils.getProperty(event, field);
+        StringEscapeUtils.escapeCsv(JSONUtils.serialize(propValue));
+    }
 }
 
-object DerivedEventFieldExtractor extends IBatchModel[DerivedEvent, String] with Serializable {
+object EventFieldExtractor extends IBatchModel[Event, String] with FieldExtractor {
+
+    def execute(events: RDD[Event], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
+        serializeToCSV[Event](events, jobParams);
+    }
+    
+    def main(args: Array[String]): Unit = {
+        val str = """[{"survey_codes":"aser007"},{"activation_keys":"ptm007"},{"partnerid":"org.ekstep.partner.pratham"}]""";
+        println(JSONUtils.serialize(StringEscapeUtils.escapeCsv(str)));
+        println(StringEscapeUtils.escapeJson(str));
+    }
+    
+}
+
+object DerivedEventFieldExtractor extends IBatchModel[DerivedEvent, String] with FieldExtractor {
 
     def execute(events: RDD[DerivedEvent], jobParams: Option[Map[String, AnyRef]])(implicit sc: SparkContext): RDD[String] = {
-
-        val config = jobParams.getOrElse(Map());
-
-        val keys = config.getOrElse("fields", "eid").asInstanceOf[String];
-        val headers = config.getOrElse("headers", keys).asInstanceOf[String];
-        val fields = keys.split(",");
-
-        val headerRDD = sc.parallelize(Array(headers), 1)
-        val valuesRDD = events.map { x =>
-            for (f <- fields) yield JSONUtils.serialize(PropertyUtils.getProperty(x, f));
-        }.map(_.mkString(","));
-        headerRDD.union(valuesRDD);
+        serializeToCSV[DerivedEvent](events, jobParams);
     }
 
 }
