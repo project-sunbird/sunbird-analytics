45f5428fb62efcc7be71f10c368fb1716af34213
Mahesh <mahesh@canopusconsulting.com>
2016-08-01 19:13:21 +0530

    #340: Recommendations API: Context filtering and test cases

--- platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/RecommendationAPIService.scala
+++ platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/RecommendationAPIService.scala
@@ -11,6 +11,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.broadcast.Broadcast
 import org.joda.time.DateTime
 import org.joda.time.DateTimeZone
+import scala.util.control.Breaks
 
 /**
  * @author mahesh
@@ -47,32 +48,58 @@ object RecommendationAPIService {
 
     def recommendations(requestBody: String)(implicit sc: SparkContext, config: Map[String, String]): String = {
         
-        validateCache();
-        val reqBody = JSONUtils.deserialize[RequestBody](requestBody);
-        val context = reqBody.request.context;
-        val did = context.get("did").asInstanceOf[String];
-        val dlang = context.get("dlang").asInstanceOf[String];
-        val limit = reqBody.request.limit.getOrElse(10);
-        
-        if (StringUtils.isBlank(did) || StringUtils.isBlank(dlang)) {
-            throw new Exception("did or dlang is missing.");
-        }
-
-        val deviceRecos = sc.cassandraTable[(List[(String, Double)])](Constants.DEVICE_DB, Constants.DEVICE_RECOS_TABLE).select("scores").where("device_id = ?", did);
-        val recoContent = if (deviceRecos.count() > 0) {
-            /** The below code joins the data with content RDD but the order is removed from it - so the results require re-ordering of data */
-            /** 
-            val deviceRDD = sc.parallelize(deviceRecos.first.take(limit), 4);
-            val contentRecos = deviceRDD.join(contentRDD).map(f => f._2._2 ++ Map("reco_score" -> f._2._1));
-            val rec = contentRecos.collect();
-            */
-            
-            deviceRecos.first.take(limit).map(f => contentBroadcastMap.value.getOrElse(f._1, Map()) ++ Map("reco_score" -> f._2)).filter(p => p.get("identifier").isDefined);
-        } else {
-            Array();
-        }
+        validateCache()(sc, config);
+	    val reqBody = JSONUtils.deserialize[RequestBody](requestBody);
+	    val context = reqBody.request.context.get;
+	    val did = context.get("did").get.asInstanceOf[String];
+	    val dlang = context.getOrElse("dlang", "").asInstanceOf[String];
+	    val reqFilters = reqBody.request.filters.getOrElse(Map());
+	    val filters: Array[(String, String, String)] 
+	    	= Array(("language", reqFilters.getOrElse("language", "").asInstanceOf[String], "LIST"), 
+	    			("domain", reqFilters.getOrElse("subject", "").asInstanceOf[String], "LIST"), 
+	    			("contentType", reqFilters.getOrElse("contentType", "").asInstanceOf[String], "STRING"), 
+	    			("gradeLevel", reqFilters.getOrElse("gradeLevel", "").asInstanceOf[String], "LIST"), 
+	    			("ageGroup", reqFilters.getOrElse("ageGroup", "").asInstanceOf[String], "LIST"))
+	    			.filter(p => !StringUtils.isBlank(p._2));
+	
+	    val limit = reqBody.request.limit.getOrElse(10);
+	
+	    if (StringUtils.isBlank(did) || StringUtils.isBlank(dlang)) {
+	      throw new Exception("did or dlang is missing.");
+	    }
+	
+	    val deviceRecos = sc.cassandraTable[(List[(String, Double)])](Constants.DEVICE_DB, Constants.DEVICE_RECOS_TABLE).select("scores").where("device_id = ?", did);
+	    val recoContent = if (deviceRecos.count() > 0) {
+	      /** The below code joins the data with content RDD but the order is removed from it - so the results require re-ordering of data */
+	      /**
+	       * val deviceRDD = sc.parallelize(deviceRecos.first.take(limit), 4);
+	       * val contentRecos = deviceRDD.join(contentRDD).map(f => f._2._2 ++ Map("reco_score" -> f._2._1));
+	       * val rec = contentRecos.collect();
+	       */
+	      deviceRecos.first.map(f => contentBroadcastMap.value.getOrElse(f._1, Map()) ++ Map("reco_score" -> f._2))
+	      	.filter(p => p.get("identifier").isDefined)
+	      	.filter(p => {
+	      		var valid = true;
+	      		Breaks.breakable {
+	      			filters.foreach { filter =>
+                        valid = recoFilter(p, filter);
+                        if (!valid) Breaks.break;
+                    }
+	      		}
+	      		valid;
+	      	}).take(limit);
+	    } else {
+	      Array();
+	    }
 
         val result = Map[String, AnyRef]("content" -> recoContent);
         JSONUtils.serialize(CommonUtil.OK("ekstep.analytics.recommendations", result));
+    }
+    
+    private def recoFilter(map: Map[String, Any], filter: (String, String, String)) : Boolean = {
+    	if ("LIST".equals(filter._3))
+    			map.getOrElse(filter._1, List()).asInstanceOf[List[String]].contains(filter._2.trim());
+    		else 
+    			map.getOrElse(filter._1, "").asInstanceOf[String].equals(filter._2.trim());
     }
 }
--- platform-api/analytics-api/test/ApplicationSpec.scala
+++ platform-api/analytics-api/test/ApplicationSpec.scala
@@ -6,6 +6,7 @@ import play.api.libs.json._
 import play.api.http._
 import play.api.test._
 import play.api.test.Helpers._
+import org.ekstep.analytics.api.util.JSONUtils
 
 /**
  * Add your spec here.
@@ -44,9 +45,41 @@ class ApplicationSpec extends Specification {
        }
        
        "return the recommendations - successful response" in new WithApplication {
-           val req = Json.toJson(Json.parse(""" {"id":"ekstep.analytics.recommendations","ver":"1.0","ts":"YYYY-MM-DDThh:mm:ssZ+/-nn.nn","request":{"context":{"did":"5edf49c4-313c-4f57-fd52-9bfe35e3c8c2","dlang":"hi","uid":"","contentId":""},"query":"elephant","filters":{"contentType":"Story"},"limit":10}} """))
-           val home = route(FakeRequest(POST, "/recommendations", FakeHeaders(Seq(("content-type", "application/json"))), req)).get
-           status(home) must equalTo(OK)
+			val req = Json.toJson(Json.parse(""" {"id":"ekstep.analytics.recommendations","ver":"1.0","ts":"YYYY-MM-DDThh:mm:ssZ+/-nn.nn","request":{"context":{"did":"5edf49c4-313c-4f57-fd52-9bfe35e3b7d6","dlang":"English"}}} """))
+			val home = route(FakeRequest(POST, "/recommendations", FakeHeaders(Seq(("content-type", "application/json"))), req)).get
+			val content = JSONUtils.deserialize[Map[String, AnyRef]](contentAsString(home)).getOrElse("result", Map("content" -> List())).asInstanceOf[Map[String, AnyRef]].get("content").get.asInstanceOf[List[AnyRef]];
+			status(home) must equalTo(OK)
+			contentType(home) must beSome.which(_ == "application/json")
+			content should not be empty
+       }
+       
+       "retun only 2 contents - successful response" in new WithApplication {
+			val req = Json.toJson(Json.parse(""" {"id":"ekstep.analytics.recommendations","ver":"1.0","ts":"2016-05-19T09:23:44.212+00:00","request":{"context":{"did":"5edf49c4-313c-4f57-fd52-9bfe35e3b7d6","dlang":"English"},"limit":3}} """));
+			val home = route(FakeRequest(POST, "/recommendations", FakeHeaders(Seq(("content-type", "application/json"))), req)).get
+			val content = JSONUtils.deserialize[Map[String, AnyRef]](contentAsString(home)).getOrElse("result", Map("content" -> List())).asInstanceOf[Map[String, AnyRef]].get("content").get.asInstanceOf[List[AnyRef]];
+			status(home) must equalTo(OK)
+			contentType(home) must beSome.which(_ == "application/json")
+			content should have size(3)
+       }
+       
+       "return only 'Hindi' content - successful response" in new WithApplication {
+    	   val req = Json.toJson(Json.parse(""" {"id":"ekstep.analytics.recommendations","ver":"1.0","ts":"2016-05-19T09:23:44.212+00:00","request":{"context":{"did":"5edf49c4-313c-4f57-fd52-9bfe35e3b7d6","dlang":"Hindi"},"filters":{"language":"Hindi"},"limit":100}} """));
+			val home = route(FakeRequest(POST, "/recommendations", FakeHeaders(Seq(("content-type", "application/json"))), req)).get
+			val content = JSONUtils.deserialize[Map[String, AnyRef]](contentAsString(home)).getOrElse("result", Map("content" -> List())).asInstanceOf[Map[String, AnyRef]].get("content").get.asInstanceOf[List[AnyRef]];
+			status(home) must equalTo(OK)
+			contentType(home) must beSome.which(_ == "application/json")
+			content.filter { x => x.asInstanceOf[Map[String, AnyRef]].getOrElse("language", List()).asInstanceOf[List[String]].contains("Hindi") } should have size(content.length)
+       }
+       
+       "return only 'Hindi Stories' content - successful response" in new WithApplication {
+    	   val req = Json.toJson(Json.parse(""" {"id":"ekstep.analytics.recommendations","ver":"1.0","ts":"2016-05-19T09:23:44.212+00:00","request":{"context":{"did":"5edf49c4-313c-4f57-fd52-9bfe35e3b7d6","dlang":"Hindi"},"filters":{"language":"Hindi", "contentType": "Story"},"limit":100}} """));
+			val home = route(FakeRequest(POST, "/recommendations", FakeHeaders(Seq(("content-type", "application/json"))), req)).get
+			val content = JSONUtils.deserialize[Map[String, AnyRef]](contentAsString(home)).getOrElse("result", Map("content" -> List())).asInstanceOf[Map[String, AnyRef]].get("content").get.asInstanceOf[List[AnyRef]];
+			status(home) must equalTo(OK)
+			contentType(home) must beSome.which(_ == "application/json")
+			content
+				.filter({ x => x.asInstanceOf[Map[String, AnyRef]].getOrElse("language", List()).asInstanceOf[List[String]].contains("Hindi") })
+				.filter({ x => x.asInstanceOf[Map[String, AnyRef]].getOrElse("contentType", "").asInstanceOf[String].contains("Story") }) should have size(content.length)
        }
        
     }
