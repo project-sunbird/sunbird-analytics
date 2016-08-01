ce3f26ae0f6957802552593c5228f7b9758e9d02
amit behera <amitb@ilimi.in>
2016-08-01 18:53:59 +0530

    debugging

--- platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/ContentAPIService.scala
+++ platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/ContentAPIService.scala
@@ -53,14 +53,14 @@ object ContentAPIService {
         
         println("Calling _doContentEnrichment......")
         val enrichedContentRDD = _doContentEnrichment(contentRDD, scriptLoc, pythonExec).cache();
-
+        printRDD("enrichedContentRDD", enrichedContentRDD);
         println("Calling _doContentToCorpus......")
         val corpusRDD = _doContentToCorpus(enrichedContentRDD, scriptLoc, pythonExec);
 
         println("Calling _doTrainContent2VecModel......")
         _doTrainContent2VecModel(scriptLoc, pythonExec);
         println("Calling _doUpdateContentVectors......")
-        printRDD(corpusRDD);
+        printRDD("corpusRDD",corpusRDD);
         val vectors = _doUpdateContentVectors(corpusRDD, scriptLoc, pythonExec, contentId);
 
         vectors.first();
