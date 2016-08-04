ec85ee5a17380d5166349a98d18768a5756eef6a
amit behera <amitb@ilimi.in>
2016-08-04 10:20:14 +0530

    content to vec test case issue fixed

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentToVec.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentToVec.scala
@@ -5,9 +5,11 @@ import org.ekstep.analytics.framework.util.JSONUtils
 class TestContentToVec extends SparkSpec(null) {
     
     "ContentToVec" should "update content_to_vec tabel and generates enriched json" in {
-            val jsonRdd = ContentToVec.execute(null, Option(Map("content_limit" -> Int.box(10))));
+        val req = """{"request":{"filters":{"objectType":["Content"],"contentType":["Story","Worksheet","Collection","Game"],"status":["Live"]},"limit":5}}"""
+        val content_search_request = JSONUtils.deserialize[Map[String, AnyRef]](req)
+        val jsonRdd = ContentToVec.execute(null, Option(Map("content_search_request" -> content_search_request)));
             val jsons = jsonRdd.collect
-            jsons.size should be (10)
+            jsons.size should be (5)
             println(JSONUtils.serialize(jsons(0)))
     }
 }
