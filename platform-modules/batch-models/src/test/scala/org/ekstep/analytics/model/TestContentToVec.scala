9681722c0abfb6176d6e6539e594458d1fe5fb51
amit behera <amitb@ilimi.in>
2016-08-16 15:14:11 +0530

    replace https with http

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentToVec.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentToVec.scala
@@ -1,22 +1,25 @@
 package org.ekstep.analytics.model
 
 import org.ekstep.analytics.framework.util.JSONUtils
+import org.ekstep.analytics.framework.util.CommonUtil
 
 class TestContentToVec extends SparkSpec(null) {
 
-    ignore should "update content_to_vec tabel and generates enriched json" in {
+    ignore should "update content_to_vec tabel and generates enriched json for 10 contents" in {
 
         val jobParams = Map(
             "content2vec.s3_bucket" -> "sandbox-data-store",
             "content2vec.s3_key_prefix" -> "model/",
-            "content2vec.model_path" -> "content2vec/",
+            "content2vec.model_path" -> "/tmp/content2vec/model",
             "content2vec.kafka_topic" -> "sandbox.learning.graph.events",
             "content2vec.kafka_broker_list" -> "localhost:9092",
-            "content2vec.corpus_path" -> "content_corpus",
+            "content2vec.corpus_path" -> "/tmp/content2vec/content_corpus",
             "python.home" -> "/usr/local/bin/",
-            "content2vec.download_path" -> "/Users/santhosh/ekStep/content2vec/tmp/",
-            "content2vec.search_request" -> Map("request" -> Map("filters" -> Map("objectType" -> List("Content"), "contentType" -> List("Story", "Worksheet", "Collection", "Game"), "status" -> List("Live")), "limit" -> 1000)))
+            "content2vec.download_path" -> "/tmp/content2vec/download",
+            "content2vec.search_request" -> Map("request" -> Map("filters" -> Map("objectType" -> List("Content"), "contentType" -> List("Story", "Worksheet", "Collection", "Game"), "status" -> List("Live")), "limit" -> 5)))
         val jsonRdd = ContentVectorsModel.execute(null, Option(jobParams));
-        println("Total vectors produced", jsonRdd.count);
+        CommonUtil.deleteDirectory("/tmp/content2vec")
+        jsonRdd.collect.length should be(5)
+        //println("Total vectors produced", jsonRdd.count);
     }
 }
