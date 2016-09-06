c6490154fb3e4de5997453e888b71d8dfac2f87c
Mahesh <mahesh@canopusconsulting.com>
2016-09-06 18:02:12 +0530

    creating access log file.

--- platform-api/analytics-api/app/filter/RequestInterceptor.scala
+++ platform-api/analytics-api/app/filter/RequestInterceptor.scala
@@ -2,13 +2,19 @@ package filter
 import play.api.mvc._
 import scala.concurrent.Future
 import context.Context
+import java.util.UUID
 
 object RequestInterceptor extends Filter {
-    
+	
     def apply(next: (RequestHeader) => Future[Result])(request: RequestHeader): Future[Result] = {
         Context.checkSparkContext();
+        val msgid = UUID.randomUUID().toString();
+        request.headers.add(("msgid", msgid));
+        val msg = s"${msgid} | Method: ${request.method} | Path: ${request.uri} | Remote Address: ${request.remoteAddress} " +
+	      s"| Domain=${request.domain} | Params: ${request.rawQueryString} " +
+	      s"| User-Agent: [${request.headers.get("user-agent").getOrElse("N/A")}]"
+	    play.Logger.of("accesslog").info(msg);
         val result = next(request)
         result
     }
-
 }
