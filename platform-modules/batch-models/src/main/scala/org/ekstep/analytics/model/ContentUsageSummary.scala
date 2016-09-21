28c4b863ccdd610f5769dbc07eeef96ea7a0b90f
amit behera <amitb@ilimi.in>
2016-09-21 18:49:04 +0530

    Revert back the content usage summary code

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ContentUsageSummary.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ContentUsageSummary.scala
@@ -18,112 +18,64 @@ import org.ekstep.analytics.framework.Context
 import org.ekstep.analytics.framework.Filter
 import org.ekstep.analytics.framework.DataFilter
 import org.ekstep.analytics.framework.util.JobLogger
-import com.datastax.spark.connector._
-import org.ekstep.analytics.util.Constants
-import org.joda.time.DateTime
 
-case class ContentUsageMetrics(content_id: Option[String] = None, content_ver: Option[String] = None, tag: Option[String] = None, total_ts: Double, total_sessions: Long, avg_ts_session: Double, total_interactions: Long, avg_interactions_min: Double, dt_range: DtRange, syncts: Long, period: Int) extends AlgoOutput;
-case class RegisteredTag(tag_id: String)
-case class InputEvents(day: Int, events: Buffer[DerivedEvent]) extends Input with AlgoInput
+case class ContentUsageMetrics(content_id: String, content_ver: String, is_group_user: Boolean, total_ts: Double, total_sessions: Int, avg_ts_session: Double, total_interactions: Long, avg_interactions_min: Double, content_type: String, mime_type: String, dt_range: DtRange) extends AlgoOutput;
+case class ContentUsageSummaryInput(contentId: String, isGroupUser: Boolean, data: Buffer[DerivedEvent]) extends AlgoInput;
 
-object ContentUsageSummary extends IBatchModelTemplate[DerivedEvent, InputEvents, ContentUsageMetrics, MeasuredEvent] with Serializable {
+object ContentUsageSummary extends IBatchModelTemplate[DerivedEvent, ContentUsageSummaryInput, ContentUsageMetrics, MeasuredEvent] with Serializable {
 
     val className = "org.ekstep.analytics.model.ContentUsageSummary"
     override def name: String = "ContentUsageSummarizer"
-
-    private def _getTagList(event: DerivedEvent, tags: Array[String]): List[String] = {
-        val tagList = event.tags.getOrElse(List()).asInstanceOf[List[Map[String, List[String]]]]
-        val genieTagFilter = if (tagList.nonEmpty) tagList.filter(f => f.contains("genie")) else List()
-        val tempList = if (genieTagFilter.nonEmpty) genieTagFilter.filter(f => f.contains("genie")).last.get("genie").get; else List();
-        tempList.filter { x => tags.contains(x) };
-    }
-
-    private def _getregisteredTags(config: Map[String, AnyRef])(implicit sc: SparkContext): Array[String] = {
-        val tags = sc.cassandraTable[RegisteredTag](Constants.CONTENT_KEY_SPACE_NAME, Constants.REGISTERED_TAGS).select("tag_id").where("active = ?", true).map { x => x.tag_id }.collect
-        if (tags.nonEmpty) tags; else config.getOrElse("tagList", Array()).asInstanceOf[Array[String]];
-    }
-
-    private def _computeMetrics(events: Buffer[DerivedEvent], period: Int, content_id: Option[String] = None, tag: Option[String] = None): ContentUsageMetrics = {
-
-        val firstEvent = events.sortBy { x => x.context.date_range.from }.head;
-        val lastEvent = events.sortBy { x => x.context.date_range.to }.last;
-        val ver = if (content_id != None) Option(firstEvent.dimensions.gdata.get.ver); else None
-        val date_range = DtRange(firstEvent.context.date_range.from, lastEvent.context.date_range.to);
-        val total_ts = CommonUtil.roundDouble(events.map { x => (x.edata.eks.asInstanceOf[Map[String, AnyRef]].get("timeSpent").get.asInstanceOf[Double]) }.sum, 2);
-        val total_sessions = events.size
-        val avg_ts_session = CommonUtil.roundDouble((total_ts / total_sessions), 2)
-        val total_interactions = events.map { x => x.edata.eks.asInstanceOf[Map[String, AnyRef]].get("noOfInteractEvents").get.asInstanceOf[Int] }.sum.toLong
-        val avg_interactions_min = if (total_interactions == 0 || total_ts == 0) 0d else CommonUtil.roundDouble(BigDecimal(total_interactions / (total_ts / 60)).toDouble, 2);
-        ContentUsageMetrics(content_id, ver, tag, total_ts, total_sessions, avg_ts_session, total_interactions, avg_interactions_min, date_range, lastEvent.syncts, period);
-    }
-
-    override def preProcess(data: RDD[DerivedEvent], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[InputEvents] = {
+    
+    override def preProcess(data: RDD[DerivedEvent], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[ContentUsageSummaryInput] = {
         val configMapping = sc.broadcast(config);
 
         val filteredEvents = DataFilter.filter(data, Filter("eid", "EQ", Option("ME_SESSION_SUMMARY")));
-        filteredEvents.map { x => (CommonUtil.getPeriod(x.context.date_range.to, Period.DAY), Buffer(x)) }
-            .partitionBy(new HashPartitioner(JobContext.parallelization))
-            .reduceByKey((a, b) => a ++ b).map { x => InputEvents(x._1, x._2) };
+        val contentSessions = filteredEvents.map { event =>
+            val eksMap = event.edata.eks.asInstanceOf[Map[String, AnyRef]]
+            val content_id = event.dimensions.gdata.get.id
+            val group_user = event.dimensions.group_user.get
+            ((content_id, group_user), Buffer(event));
+        }.partitionBy(new HashPartitioner(JobContext.parallelization)).reduceByKey((a, b) => a ++ b);
+        contentSessions.map(f => ContentUsageSummaryInput(f._1._1, f._1._2, f._2));
     }
 
-    override def algorithm(data: RDD[InputEvents], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[ContentUsageMetrics] = {
+    override def algorithm(data: RDD[ContentUsageSummaryInput], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[ContentUsageMetrics] = {
 
-        val inputs = data.cache()
-
-        // all summary
-        val allSummary = inputs.map { x => _computeMetrics(x.events, x.day); }
-
-        // per content summary
-        val contentSummary = inputs.map { x =>
-            x.events.groupBy(f => f.dimensions.gdata.get.id).map { f => _computeMetrics(f._2, x.day, Option(f._1)); };
-        }.flatMap { x => x }
-
-        val registeredTags = _getregisteredTags(config)(sc)
-
-        // (per tag) & (per tag,per content) summary
-        val tagSummary_tagContentSummary = inputs.map { x =>
-
-            val period = x.day
-            val tagEvent = x.events.map { x => (_getTagList(x, registeredTags), Buffer(x)); }
-                .map { x => x._1.map { f => (f, x._2) }; }.flatMap(f => f).groupBy(f => f._1)
-
-            // per tag Summary
-            val tagSum = tagEvent.map { f =>
-                f._2.map { x => x._2 }.map { x => _computeMetrics(x, period, None, Option(f._1)); }
-            }.flatMap { x => x }
-
-            // per tag per content Summary
-            val tagContentSum = tagEvent.map { tagEvents =>
-                val tag = tagEvents._1
-                tagEvents._2.map { x => x._2 }.map { x =>
-                    x.groupBy(f => f.dimensions.gdata.get.id).map { f => _computeMetrics(f._2, period, Option(f._1), Option(tagEvents._1)); }
-                }.flatMap { x => x };
-            }.flatMap { x => x };
-
-            (tagSum ++ tagContentSum);
-        }.flatMap { x => x }
-
-        inputs.unpersist(true)
-        val rdd1 = contentSummary.union(allSummary)
-        rdd1.union(tagSummary_tagContentSummary);
+        data.map { cusEvent =>
+            val firstEvent = cusEvent.data.sortBy { x => x.context.date_range.from }.head;
+            val lastEvent = cusEvent.data.sortBy { x => x.context.date_range.to }.last;
+            val date_range = DtRange(firstEvent.syncts, lastEvent.syncts);
+            val content_type = firstEvent.edata.eks.asInstanceOf[Map[String, AnyRef]].get("contentType").get.asInstanceOf[String]
+            val mime_type = firstEvent.edata.eks.asInstanceOf[Map[String, AnyRef]].get("mimeType").get.asInstanceOf[String]
+            val content_ver = firstEvent.dimensions.gdata.get.ver
+            
+            val total_ts = CommonUtil.roundDouble(cusEvent.data.map { x => (x.edata.eks.asInstanceOf[Map[String, AnyRef]].get("timeSpent").get.asInstanceOf[Double]) }.sum, 2);
+            val total_sessions = cusEvent.data.size
+            val avg_ts_session = CommonUtil.roundDouble((total_ts / total_sessions), 2)
+            val total_interactions = cusEvent.data.map { x => x.edata.eks.asInstanceOf[Map[String, AnyRef]].get("noOfInteractEvents").get.asInstanceOf[Int] }.sum
+            val avg_interactions_min = if (total_interactions == 0 || total_ts == 0) 0d else CommonUtil.roundDouble(BigDecimal(total_interactions / (total_ts / 60)).toDouble, 2);
+            ContentUsageMetrics(cusEvent.contentId, content_ver, cusEvent.isGroupUser, total_ts, total_sessions, avg_ts_session, total_interactions, avg_interactions_min, content_type, mime_type, date_range)
+        }
     }
 
     override def postProcess(data: RDD[ContentUsageMetrics], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[MeasuredEvent] = {
         data.map { cuMetrics =>
-            val mid = CommonUtil.getMessageId("ME_CONTENT_USAGE_SUMMARY", "", config.getOrElse("granularity", "DAY").asInstanceOf[String], cuMetrics.dt_range, cuMetrics.content_id.getOrElse(""), cuMetrics.tag.getOrElse(""), cuMetrics.period);
+            val mid = CommonUtil.getMessageId("ME_CONTENT_USAGE_SUMMARY", null, config.getOrElse("granularity", "DAY").asInstanceOf[String], cuMetrics.dt_range, cuMetrics.content_id + cuMetrics.is_group_user);
             val measures = Map(
                 "total_ts" -> cuMetrics.total_ts,
                 "total_sessions" -> cuMetrics.total_sessions,
-                "avg_ts" -> cuMetrics.avg_ts_session,
+                "avg_ts_session" -> cuMetrics.avg_ts_session,
                 "total_interactions" -> cuMetrics.total_interactions,
-                "avg_interactions_min" -> cuMetrics.avg_interactions_min)
-
-            val gdata = if (cuMetrics.content_id != None) Option(new GData(cuMetrics.content_id.get, cuMetrics.content_ver.get)) else None
-
-            MeasuredEvent("ME_CONTENT_USAGE_SUMMARY", System.currentTimeMillis(), cuMetrics.syncts, "1.0", mid, "", None, None,
+                "avg_interactions_min" -> cuMetrics.avg_interactions_min,
+                "content_type" -> cuMetrics.content_type,
+                "mime_type" -> cuMetrics.mime_type);
+            MeasuredEvent("ME_CONTENT_USAGE_SUMMARY", System.currentTimeMillis(), cuMetrics.dt_range.to, "1.0", mid, "", Option(cuMetrics.content_id), None,
                 Context(PData(config.getOrElse("producerId", "AnalyticsDataPipeline").asInstanceOf[String], config.getOrElse("modelId", "ContentUsageSummary").asInstanceOf[String], config.getOrElse("modelVersion", "1.0").asInstanceOf[String]), None, config.getOrElse("granularity", "DAY").asInstanceOf[String], cuMetrics.dt_range),
-                Dimensions(None, None, gdata, None, None, None, None, None, None, cuMetrics.tag),
+                Dimensions(None, None, Option(new GData(cuMetrics.content_id, cuMetrics.content_ver)), None, None, None, None, Option(cuMetrics.is_group_user)),
                 MEEdata(measures));
         }
+
     }
+
 }
