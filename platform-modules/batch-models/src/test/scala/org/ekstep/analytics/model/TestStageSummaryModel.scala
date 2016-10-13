4f31fcfb3ee96f79733fd46293c9060a3968df92
sowmya-dixit <sowmyadixit7@gmail.com>
2016-10-13 17:58:02 +0530

    Modified Stage Summary test file and test data.

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestStageSummaryModel.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestStageSummaryModel.scala
@@ -8,7 +8,7 @@ class TestStageSummaryModel extends SparkSpec(null) {
 
     "StageSummaryModel" should "generate stage summaries" in {
 
-        val rdd = loadFile[org.ekstep.analytics.util.DerivedEvent]("src/test/resources/item-usage-summary/test-data.log");
+        val rdd = loadFile[org.ekstep.analytics.util.DerivedEvent]("src/test/resources/stage-summary/test-data.log");
 
         val rdd2 = StageSummaryModel.execute(rdd, None);
         val me = rdd2.map { x => JSONUtils.serialize(x) }.collect();
