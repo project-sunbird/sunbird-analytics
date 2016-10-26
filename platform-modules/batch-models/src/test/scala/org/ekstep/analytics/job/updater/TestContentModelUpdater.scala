dd7322e07eac3d85688e7486dbee26af571ae180
amit behera <amitb@ilimi.in>
2016-10-26 15:07:24 +0530

    test class name updated

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/updater/TestContentModelUpdater.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/updater/TestContentModelUpdater.scala
@@ -7,7 +7,7 @@ import org.ekstep.analytics.framework.Query
 import org.ekstep.analytics.framework.Dispatcher
 import org.ekstep.analytics.framework.util.JSONUtils
 
-class TestContentPopularityUpdater extends SparkSpec(null) {
+class TestContentModelUpdater extends SparkSpec(null) {
     
     "ContentModelUpdater" should "execute the job and shouldn't throw any exception" in {
         val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/content-popularity/test-data.json"))))), None, None, "org.ekstep.analytics.updater.UpdateContentPopularity", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("Content Popularity Updater Test"), Option(false))
