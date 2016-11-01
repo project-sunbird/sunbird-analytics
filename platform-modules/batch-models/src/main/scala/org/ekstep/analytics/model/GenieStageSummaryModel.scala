f09ca54f1a51742e48f8061d589adad1cd28ccb6
amit behera <amitb@ilimi.in>
2016-11-01 10:59:28 +0530

    added did and tags in Genie Stage Summary

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/GenieStageSummaryModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/GenieStageSummaryModel.scala
@@ -7,7 +7,7 @@ import org.ekstep.analytics.util.DerivedEvent
 import org.ekstep.analytics.framework.util.JSONUtils
 import org.ekstep.analytics.framework.util.CommonUtil
 
-case class GenieStageAlgoOut(stageId: String, sid: String, timeSpent: Double, visitCount: Int, interactEventsCount: Int, interactEvents: List[Map[String, String]], dt_range: DtRange, syncts: Long) extends AlgoOutput
+case class GenieStageAlgoOut(stageId: String, sid: String, timeSpent: Double, visitCount: Int, interactEventsCount: Int, interactEvents: List[Map[String, String]], dt_range: DtRange, did: String, tags: Option[AnyRef], syncts: Long) extends AlgoOutput
 
 object GenieStageSummaryModel extends SessionBatchModel[DerivedEvent, MeasuredEvent] with IBatchModelTemplate[DerivedEvent, DerivedEvent, GenieStageAlgoOut, MeasuredEvent] with Serializable {
   
@@ -21,10 +21,14 @@ object GenieStageSummaryModel extends SessionBatchModel[DerivedEvent, MeasuredEv
     override def algorithm(data: RDD[DerivedEvent], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[GenieStageAlgoOut] = {
         data.map { event =>
             val screenSummaries = event.edata.eks.screenSummary;
+            
+            val did = event.dimensions.did
+            val tags = event.tags
+            
             if (null != screenSummaries && screenSummaries.size > 0) {
                 screenSummaries.map { x =>
                     val ss = JSONUtils.deserialize[GenieStageSummary](JSONUtils.serialize(x));
-                    GenieStageAlgoOut(ss.stageId, ss.sid, ss.timeSpent, ss.visitCount, ss.interactEventsCount, ss.interactEvents, event.context.date_range, event.syncts)
+                    GenieStageAlgoOut(ss.stageId, ss.sid, ss.timeSpent, ss.visitCount, ss.interactEventsCount, ss.interactEvents, event.context.date_range, did, Option(tags), event.syncts)
                 }
             } else {
                 Array[GenieStageAlgoOut]();
@@ -44,7 +48,7 @@ object GenieStageSummaryModel extends SessionBatchModel[DerivedEvent, MeasuredEv
             val pdata = PData(config.getOrElse("producerId", "AnalyticsDataPipeline").asInstanceOf[String], config.getOrElse("modelId", "GenieStageSummary").asInstanceOf[String], config.getOrElse("modelVersion", "1.0").asInstanceOf[String]);
             MeasuredEvent("ME_GENIE_STAGE_SUMMARY", System.currentTimeMillis(), summary.syncts, "1.0", mid, null, None, None,
                 Context(pdata, None, "GENIE SESSION", summary.dt_range),
-                Dimensions(None, None, None, None, None, None, None, None, None, None, None, None, None, None, Option(summary.sid), Option(summary.stageId)), MEEdata(measures), None);
+                Dimensions(None, Option(summary.did), None, None, None, None, None, None, None, None, None, None, None, None, Option(summary.sid), Option(summary.stageId)), MEEdata(measures), summary.tags);
         };
     }
 }
