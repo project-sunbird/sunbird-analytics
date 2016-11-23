955829d1f88f325e351ff0cf18ae563e93e363c5
Mahesh <mahesh@canopusconsulting.com>
2016-11-23 11:51:13 +0530

    feat: top5 incorrect responses with count in item usage metrics API.
    contributes: #524

--- platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/IMetricsModel.scala
+++ platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/IMetricsModel.scala
@@ -53,12 +53,6 @@ trait IMetricsModel[T <: Metrics, R <: Metrics] {
     }
 
     def getMetrics(records: RDD[T], period: String, fields: Array[String] = Array())(implicit sc: SparkContext, config: Config): RDD[R]
-
-    def postProcess(metrics: RDD[R], summary: R): Map[String, AnyRef] = {
-    	Map[String, AnyRef](
-                "metrics" -> metrics.collect(),
-                "summary" -> summary);
-    };
     
     def getSummary(metrics: RDD[R],  fields: Array[String] = Array()): R = {
         metrics.reduce((a, b) => reduce(a, b, fields));
@@ -69,7 +63,9 @@ trait IMetricsModel[T <: Metrics, R <: Metrics] {
     private def getResult(records: RDD[T], period: String, fields: Array[String] = Array())(implicit sc: SparkContext, config: Config) : Map[String, AnyRef] = {
     	val metrics = getMetrics(records, period, fields);
             val summary = getSummary(metrics, fields);
-            postProcess(metrics, summary);
+            Map[String, AnyRef](
+                "metrics" -> metrics.collect(),
+                "summary" -> summary);
     }
 
     private def getData[T](contentId: String, tag: String, period: String)(implicit mf: Manifest[T], sc: SparkContext, config: Config): RDD[T] = {
