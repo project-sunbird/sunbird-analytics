6b98a8c0f587f60984192f4ac013c7536f1fcf54
amit behera <amitb@ilimi.in>
2016-11-25 17:20:11 +0530

    refactor: refactored Genie Funnel Aggregator .... contribute: #507

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/GenieFunnelAggregatorModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/GenieFunnelAggregatorModel.scala
@@ -20,71 +20,49 @@ import org.ekstep.analytics.framework.MEEdata
 import scala.collection.mutable.HashMap
 import org.ekstep.analytics.framework.DtRange
 import org.ekstep.analytics.framework.Period
+import org.ekstep.analytics.framework.Stage
+import org.ekstep.analytics.framework.OnboardStage
+import org.ekstep.analytics.framework.OtherStage
 
 case class DeviceFunnelSummary(did: String, funnel: String, events: Buffer[DerivedEvent]) extends AlgoInput
-case class FunnelSummary(funnel: String, did: String, period: Int, dspec: Map[String, AnyRef], genieVer: String, summary: HashMap[String, StageAggSumm], totalTimeSpent: Double, completionPercentage: Double, syncts: Long, dateRange: DtRange, tags: Option[AnyRef]) extends AlgoOutput
+case class FunnelSummary(funnel: String, did: String, period: Int, dspec: Map[String, AnyRef], genieVer: String, summary: Map[String, StageAggSumm], totalTimeSpent: Double, completionPercentage: Double, syncts: Long, dateRange: DtRange, tags: Option[AnyRef]) extends AlgoOutput
 case class StageAggSumm(timeSpent: Double, totalCount: Int, dropoffPercentage: Double)
 
 object GenieFunnelAggregatorModel extends IBatchModelTemplate[DerivedEvent, DeviceFunnelSummary, FunnelSummary, MeasuredEvent] with Serializable {
 
     def _funnelAggregator(data: DeviceFunnelSummary): FunnelSummary = {
-        val did = data.did
         val funnel = data.funnel
+        if ("GenieOnboarding".equals(funnel)) {
+            _getFunnelSummary(data, OnboardStage)
+        } else {
+            _getFunnelSummary(data, OtherStage)
+        }
+    }
+
+    private def _getFunnelSummary(data: DeviceFunnelSummary, stage: Stage): FunnelSummary = {
+
         val events = data.events.sortBy { x => x.ets }
         val firstEvent = events.head
         val lastEvent = events.last
         val dimensions = firstEvent.dimensions
-        val syncts = firstEvent.syncts
-        val period = CommonUtil.getPeriod(syncts, Period.DAY)
 
-        val eksMap = data.events.map { x => x.edata.eks.asInstanceOf[Map[String, AnyRef]] }
-        if ("GenieOnboarding".equals(funnel)) {
-            val totalTimeSpent = CommonUtil.roundDouble(eksMap.map { x => (x.get("timeSpent").get.asInstanceOf[Double]) }.sum, 2)
+        val eksMaps = data.events.map { x => x.edata.eks.asInstanceOf[Map[String, AnyRef]] }
+        val totalTimeSpent = CommonUtil.roundDouble(eksMaps.map { x => (x.get("timeSpent").get.asInstanceOf[Double]) }.sum, 2)
+        val period = CommonUtil.getPeriod(firstEvent.syncts, Period.DAY)
 
-            val loadOnboardPage = eksMap.map { x => (x.get("loadOnboardPage").get.asInstanceOf[Map[String, AnyRef]]) }
-            val welcomeContentSkipped = eksMap.map { x => (x.get("welcomeContentSkipped").get.asInstanceOf[Map[String, AnyRef]]) }
-            val addChildSkipped = eksMap.map { x => (x.get("addChildSkipped").get.asInstanceOf[Map[String, AnyRef]]) }
-            val firstLessonSkipped = eksMap.map { x => (x.get("firstLessonSkipped").get.asInstanceOf[Map[String, AnyRef]]) }
-            val gotoLibrarySkipped = eksMap.map { x => (x.get("gotoLibrarySkipped").get.asInstanceOf[Map[String, AnyRef]]) }
-            val searchLessonSkipped = eksMap.map { x => (x.get("searchLessonSkipped").get.asInstanceOf[Map[String, AnyRef]]) }
-            val contentPlayed = eksMap.map { x => (x.get("contentPlayed").get.asInstanceOf[Map[String, AnyRef]]) }
-
-            val contentPlayedSumm = _getStageAggSumm(contentPlayed)
-            val completionPercentage = CommonUtil.roundDouble((100 - contentPlayedSumm.dropoffPercentage), 2)
-
-            val stageSumm = HashMap(
-                "loadOnboardPage" -> _getStageAggSumm(loadOnboardPage),
-                "welcomeContentSkipped" -> _getStageAggSumm(welcomeContentSkipped),
-                "addChildSkipped" -> _getStageAggSumm(addChildSkipped),
-                "firstLessonSkipped" -> _getStageAggSumm(firstLessonSkipped),
-                "gotoLibrarySkipped" -> _getStageAggSumm(gotoLibrarySkipped),
-                "searchLessonSkipped" -> _getStageAggSumm(searchLessonSkipped),
-                "contentPlayed" -> contentPlayedSumm)
-
-            FunnelSummary(funnel, did, period, dimensions.dspec.getOrElse(Map()), dimensions.genieVer.get, stageSumm, totalTimeSpent, completionPercentage, syncts, DtRange(firstEvent.context.date_range.from, lastEvent.context.date_range.to), firstEvent.tags)
-        } else {
-            val totalTimeSpent = CommonUtil.roundDouble(eksMap.map { x => (x.get("timeSpent").get.asInstanceOf[Double]) }.sum, 2)
-
-            val listContent = eksMap.map { x => (x.get("listContent").get.asInstanceOf[Map[String, AnyRef]]) }
-            val selectContent = eksMap.map { x => (x.get("selectContent").get.asInstanceOf[Map[String, AnyRef]]) }
-            val downloadInitiated = eksMap.map { x => (x.get("downloadInitiated").get.asInstanceOf[Map[String, AnyRef]]) }
-            val downloadComplete = eksMap.map { x => (x.get("downloadComplete").get.asInstanceOf[Map[String, AnyRef]]) }
-            val contentPlayed = eksMap.map { x => (x.get("contentPlayed").get.asInstanceOf[Map[String, AnyRef]]) }
-
-            val contentPlayedSumm = _getStageAggSumm(contentPlayed)
-            val completionPercentage = CommonUtil.roundDouble((100 - contentPlayedSumm.dropoffPercentage), 2)
-
-            val stageSumm = HashMap(
-                "listContent" -> _getStageAggSumm(listContent),
-                "selectContent" -> _getStageAggSumm(selectContent),
-                "downloadInitiated" -> _getStageAggSumm(downloadInitiated),
-                "downloadComplete" -> _getStageAggSumm(downloadComplete),
-                "contentPlayed" -> contentPlayedSumm)
-
-            FunnelSummary(funnel, did, period, dimensions.dspec.getOrElse(Map()), dimensions.genieVer.get, stageSumm, totalTimeSpent, completionPercentage, syncts, DtRange(firstEvent.context.date_range.from, lastEvent.context.date_range.to), firstEvent.tags)
-        }
+        val stageSumm = _getStageSummMap(eksMaps, stage)
+        val contentPlayedSumm = stageSumm.get("contentPlayed").get
+        val completionPercentage = CommonUtil.roundDouble((100 - contentPlayedSumm.dropoffPercentage), 2)
+        FunnelSummary(data.funnel, data.did, period, dimensions.dspec.getOrElse(Map()), dimensions.genieVer.get, stageSumm, totalTimeSpent, completionPercentage, firstEvent.syncts, DtRange(firstEvent.context.date_range.from, lastEvent.context.date_range.to), firstEvent.tags)
     }
 
+    private def _getStageSummMap(eksMaps: Buffer[Map[String, AnyRef]], stage: Stage): Map[String, StageAggSumm] = {
+        stage.values.map { x =>
+            val stageId = x.toString()
+            (stageId, eksMaps.map { x => (x.get(stageId).get.asInstanceOf[Map[String, AnyRef]]) })
+        }.toMap.map { x => (x._1, _getStageAggSumm(x._2)) }
+
+    }
     private def _getStageAggSumm(data: Buffer[Map[String, AnyRef]]): StageAggSumm = {
         val stageSumm = data.map { x => FunnelStageSummary(Option(x.get("timeSpent").get.asInstanceOf[Double]), Option(x.get("count").get.asInstanceOf[Int]), Option(x.get("stageInvoked").get.asInstanceOf[Int])) }
         val timeSpent = CommonUtil.roundDouble(stageSumm.map { x => x.timeSpent.get }.sum, 2)
