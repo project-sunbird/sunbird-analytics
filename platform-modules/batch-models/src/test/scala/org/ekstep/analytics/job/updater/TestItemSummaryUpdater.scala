fb9459ccf62effcd6e3341e4ad77897fcd0d2a7a
amit behera <amitb@ilimi.in>
2016-12-27 16:02:20 +0530

    correct test file path updated in TestItemSummaryUpdater

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/updater/TestItemSummaryUpdater.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/updater/TestItemSummaryUpdater.scala
@@ -10,7 +10,7 @@ import org.ekstep.analytics.framework.Dispatcher
 class TestItemSummaryUpdater extends SparkSpec(null) {
     
     "ItemSummaryUpdater" should "execute the job and shouldn't throw any exception" in {
-        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/item-summary-updater/ius_1.log"))))), None, None, "org.ekstep.analytics.updater.UpdateItemSummaryDB", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestItemSummaryUpdater"), Option(false))
+        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/item-summary-updater/ius_2.log"))))), None, None, "org.ekstep.analytics.updater.UpdateItemSummaryDB", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestItemSummaryUpdater"), Option(false))
         ItemSummaryUpdater.main(JSONUtils.serialize(config))(Option(sc));
     }
   
