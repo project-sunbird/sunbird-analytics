70589ef1550733ef5589d00c2c22daaf463d9d2b
Mahesh <mahesh@canopusconsulting.com>
2016-12-28 15:52:32 +0530

    fixing testcases for api url changes.

--- platform-api/analytics-api/test/ApplicationSpec.scala
+++ platform-api/analytics-api/test/ApplicationSpec.scala
@@ -29,60 +29,60 @@ class ApplicationSpec extends BaseSpec {
        
        "return error response on invalid request(recommendations) - error response" in new WithApplication {
     		val request = """ {"id":"ekstep.analytics.recommendations","ver":"1.0","ts":"YYYY-MM-DDThh:mm:ssZ+/-nn.nn","request":{"context":{}}} """
-			val response = post("/recommendations", request);
+			val response = post("/content/recommend", request);
     		hasClientError(response);
        }
        
        "return the recommendations - successful response" in new WithApplication {
 			val request = """ {"id":"ekstep.analytics.recommendations","ver":"1.0","ts":"YYYY-MM-DDThh:mm:ssZ+/-nn.nn","request":{"context":{"did":"5edf49c4-313c-4f57-fd52-9bfe35e3b7d6","dlang":"English"}, "filters": {"contentType": "Story"}}} """
-			val response = post("/recommendations", request);
+			val response = post("/content/recommend", request);
 			isOK(response);
        }
        
        "invoke content to vec on a content" in new WithApplication {
 			val request = """ {"id":"ekstep.analytics.content-to-vec","ver":"1.0","ts":"YYYY-MM-DDThh:mm:ssZ+/-nn.nn","request":{}} """
-			val response = post("/content-to-vec/domain_63844", request);
+			val response = post("/content/vectors/domain_63844", request);
     	   	isOK(response);			
        }
        
        "invoke content to vec train model" in new WithApplication {
 			val request = """ {} """
-			val response = post("/content-to-vec/train/model", request);
+			val response = post("/train/content-to-vec", request);
 			isOK(response);
        }
        
        "invoke recommendations model training" in new WithApplication {
 			val request = """ {} """
-			val response = post("/recommendations/train/model", request);
+			val response = post("/train/recommendations", request);
 			isOK(response);
        }
        
        "invoke run job" in new WithApplication {
 			val request = """ {} """
-			val response = post("/dataproduct/run/ss", request);
+			val response = post("/run/ss", request);
 			isOK(response);
        }
        
        "invoke replay-job" in new WithApplication {
 			val request = """ {} """
-			val response = post("/dataproduct/replay/ss/2016-04-01/2016-04-03", request);
+			val response = post("/replay/ss/2016-04-01/2016-04-03", request);
 			isOK(response);
        }
        
        "register tag" in new WithApplication {
     	   	val request = """ {} """
-			val response = post("/tag/4f04da601e244d31aa7b1daf91c46341",request);
+			val response = post("/metrics/tag/4f04da601e244d31aa7b1daf91c46341",request);
 			isOK(response);
        }
        
        "un-register tag" in new WithApplication {
-    	   	val response = route(FakeRequest(DELETE, "/tag/4f04da601e244d31aa7b1daf91c46341")).get;
+    	   	val response = route(FakeRequest(DELETE, "/metrics/tag/delete/4f04da601e244d31aa7b1daf91c46341")).get;
 			isOK(response);
        }
        
        "recommendations - should return empty response when recommendations disabled" in new WithApplication {
 			val request = """ {"id":"ekstep.analytics.recommendations","ver":"1.0","ts":"YYYY-MM-DDThh:mm:ssZ+/-nn.nn","request":{"context":{"did":"5edf49c4-313c-4f57-fd52-9bfe35e3b7d6","dlang":"English"}, "filters": {"contentType": "Story"}}} """
-			val response = post("/recommendations", request);
+			val response = post("/content/recommend", request);
 			isOK(response);
 			contentAsString(response) must contain(""""count":0""")
        }
