8842fea60a173f6f78d44b19667da0494ae53f81
Mahesh <mahesh@canopusconsulting.com>
2016-12-29 22:48:59 +0530

    fix: test cases changes for url update.

--- platform-api/analytics-api/test/JobsAPISpec.scala
+++ platform-api/analytics-api/test/JobsAPISpec.scala
@@ -18,7 +18,9 @@ import scala.concurrent.Await
 
 @RunWith(classOf[JUnitRunner])
 class JobsAPISpec extends BaseSpec with Serializable {
-
+	
+	val url = "/dataset/request/submit";
+	
     "Job Request API" should {
 
         CassandraConnector(Context.sc.getConf).withSessionDo { session =>
@@ -27,26 +29,26 @@ class JobsAPISpec extends BaseSpec with Serializable {
 
         "return error response on invalid request - error response" in new WithApplication {
             val request = """ {"id": "ekstep.analytics.data.out","ver": "1.0", "ts": "2016-12-07T12:40:40+05:30","params": { "msgid": "4f04da60-1e24-4d31-aa7b-1daf91c46341", "client_key": "dev-portal" },"request": {"filter": {"start_date": "2016-11-20","end_date": "2016-11-10","tags": ["6da8fa317798fd23e6d30cdb3b7aef10c7e7bef5"]  }}}"""
-            val response = post("/dataset/request", request);
+            val response = post(url, request);
             hasClientError(response);
         }
 
         "return the result - successful response" in new WithApplication {
             val request = """ {"id": "ekstep.analytics.data.out","ver": "1.0", "ts": "2016-12-07T12:40:40+05:30","params": { "msgid": "4f04da60-1e24-4d31-aa7b-1daf91c46341", "client_key": "dev-portal" },"request": {"filter": {"start_date": "2016-11-03","end_date": "2016-11-10","tags": ["6da8fa317798fd23e6d30cdb3b7aef10c7e7bef5"]  }}}"""
-            val response = post("/dataset/request", request);
+            val response = post(url, request);
             isOK(response);
         }
 
         "return error response on empty request - error response" in new WithApplication {
             val request = """ {}"""
-            val response = post("/dataset/request", request);
+            val response = post(url, request);
             println(response)
             //hasClientError(response);
         }
 
         "return status as processed when submitted job is processed" in new WithApplication {
             val request = """ {"id": "ekstep.analytics.data.out","ver": "1.0", "ts": "2016-12-07T12:40:40+05:30","params": { "msgid": "4f04da60-1e24-4d31-aa7b-1daf91c46341", "client_key": "dev-portal1" },"request": {"filter": {"start_date": "2016-11-04","end_date": "2016-11-10","tags": ["6da8fa317798fd23e6d30cdb3b7aef10c7e7bef7"]  }}}"""
-            val response = post("/dataset/request", request);
+            val response = post(url, request);
             val rowRDD = Context.sc.cassandraTable[JobRequest]("platform_db", "job_request");
             val filterRDD = rowRDD.filter { x => (x.client_key.get == "dev-portal1") }
             val rddJobRequest = filterRDD.map { x => JobRequest(x.client_key, x.request_id, x.job_id, Some("PROCESSING"), x.request_data, x.iteration, x.dt_job_submitted, x.location, x.dt_file_created, x.dt_first_event, x.dt_last_event, x.dt_expiration, x.dt_job_processing, x.dt_job_completed, x.input_events, x.output_events, x.file_size, x.latency, x.execution_time, x.err_message, x.stage, x.stage_status) }
@@ -61,7 +63,7 @@ class JobsAPISpec extends BaseSpec with Serializable {
 
          "return status as completed when submitted job is completed" in new WithApplication {
             val request = """ {"id": "ekstep.analytics.data.out","ver": "1.0", "ts": "2016-12-07T12:40:40+05:30","params": { "msgid": "4f04da60-1e24-4d31-aa7b-1daf91c46341", "client_key": "dev-portal2" },"request": {"filter": {"start_date": "2016-11-05","end_date": "2016-11-10","tags": ["6da8fa317798fd23e6d30cdb3b7aef10c7e7bef8"]  }}}"""
-            val response = post("/dataset/request", request);
+            val response = post(url, request);
             val rowRDD = Context.sc.cassandraTable[JobRequest]("platform_db", "job_request");
             val filterRDD = rowRDD.filter { x => (x.client_key.get == "dev-portal2") }
             val rddJobRequest = filterRDD.map { x => JobRequest(x.client_key, x.request_id, x.job_id, Some("COMPLETE"), x.request_data, x.iteration, x.dt_job_submitted, Some("http://"), Some(new org.joda.time.DateTime("2016-12-28")), Some(new org.joda.time.DateTime("2016-12-28")), Some(new org.joda.time.DateTime("2016-12-28")), Some(new org.joda.time.DateTime("2016-12-28")), Some(new org.joda.time.DateTime("2016-12-28")), Some(new org.joda.time.DateTime("2016-12-28")), Some(1000), Some(2000), Some(12333), Some(22345), Some(12345l), x.err_message, x.stage, x.stage_status) }
@@ -70,7 +72,7 @@ class JobsAPISpec extends BaseSpec with Serializable {
             rddJobRequest.saveToCassandra("platform_db", "job_request")
 
             val request1 = """ {"id": "ekstep.analytics.data.out","ver": "1.0", "ts": "2016-12-07T12:40:40+05:30","params": { "msgid": "4f04da60-1e24-4d31-aa7b-1daf91c46341", "client_key": "dev-portal2" },"request": {"filter": {"start_date": "2016-11-05","end_date": "2016-11-10","tags": ["6da8fa317798fd23e6d30cdb3b7aef10c7e7bef8"]  }}}"""
-            val response1 = post("/dataset/request", request1);
+            val response1 = post(url, request1);
 
 
             //contentAsString(response1) must contain(""""status":"COMPLETE"""")
@@ -81,7 +83,7 @@ class JobsAPISpec extends BaseSpec with Serializable {
 
 
             val request = """ {"id": "ekstep.analytics.data.out","ver": "1.0", "ts": "2016-12-07T12:40:40+05:30","params": { "msgid": "4f04da60-1e24-4d31-aa7b-1daf91c46341", "client_key": "dev-portal3" },"request": {"filter": {"start_date": "2016-11-06","end_date": "2016-11-10","tags": ["6da8fa317798fd23e6d30cdb3b7aef10c7e7bef9"]  }}}"""
-            val response = post("/dataset/request", request);
+            val response = post(url, request);
             val rowRDD = Context.sc.cassandraTable[JobRequest]("platform_db", "job_request");
             val filterRDD = rowRDD.filter { x => (x.client_key.get == "dev-portal3") }
             val rddJobRequest = filterRDD.map { x => JobRequest(x.client_key, x.request_id, x.job_id, Some("FAILED"), x.request_data, x.iteration, x.dt_job_submitted, x.location, x.dt_file_created, x.dt_first_event, x.dt_last_event, x.dt_expiration, x.dt_job_processing, x.dt_job_completed, x.input_events, x.output_events, x.file_size, x.latency, x.execution_time, x.err_message, x.stage, x.stage_status) }
@@ -89,7 +91,7 @@ class JobsAPISpec extends BaseSpec with Serializable {
             rddJobRequest.saveToCassandra("platform_db", "job_request")
 
             val request1 = """ {"id": "ekstep.analytics.data.out","ver": "1.0", "ts": "2016-12-07T12:40:40+05:30","params": { "msgid": "4f04da60-1e24-4d31-aa7b-1daf91c46341", "client_key": "dev-portal3" },"request": {"filter": {"start_date": "2016-11-06","end_date": "2016-11-10","tags": ["6da8fa317798fd23e6d30cdb3b7aef10c7e7bef9"]  }}}"""
-            val response1 = post("/dataset/request", request1);
+            val response1 = post(url, request1);
             import scala.concurrent.Await
             import scala.concurrent.duration.DurationInt
 
@@ -107,7 +109,7 @@ class JobsAPISpec extends BaseSpec with Serializable {
         /*"return status as FAILED when job got FAILED" in new WithApplication {
 
             val request = """ {"id": "ekstep.analytics.data.out","ver": "1.0", "ts": "2016-12-07T12:40:40+05:30","params": { "msgid": "4f04da60-1e24-4d31-aa7b-1daf91c46341", "client_key": "dev-portal4" },"request": {"filter": {"start_date": "2016-11-04","end_date": "2016-11-10","tags": ["6da8fa317798fd23e6d30cdb3b7aef10c7e7bef10"]  }}}"""
-            val response = post("/dataset/request", request);
+            val response = post(url, request);
             val rowRDD = Context.sc.cassandraTable[JobRequest]("platform_db", "job_request");
             val filterRDD = rowRDD.filter { x => (x.client_key.get == "dev-portal4") }
             val rddJobRequest = filterRDD.map { x => JobRequest(x.client_key, x.request_id, x.job_id, Some("RETRY"), x.request_data, x.iteration, x.dt_job_submitted, x.location, x.dt_file_created, x.dt_first_event, x.dt_last_event, x.dt_expiration, x.dt_job_processing, x.dt_job_completed, x.input_events, x.output_events, x.file_size, x.latency, x.execution_time, x.err_message) }
@@ -115,7 +117,7 @@ class JobsAPISpec extends BaseSpec with Serializable {
             rddJobRequest.saveToCassandra("platform_db", "job_request")
 
             val request1 = """ {"id": "ekstep.analytics.data.out","ver": "1.0", "ts": "2016-12-07T12:40:40+05:30","params": { "msgid": "4f04da60-1e24-4d31-aa7b-1daf91c46341", "client_key": "dev-portal4" },"request": {"filter": {"start_date": "2016-11-04","end_date": "2016-11-10","tags": ["6da8fa317798fd23e6d30cdb3b7aef10c7e7bef10"]  }}}"""
-            val response1 = post("/dataset/request", request1);
+            val response1 = post(url, request1);
 
             contentAsString(response1) must contain(""""status":"RETRY"""")
 
