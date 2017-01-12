3c3d3ed0551830d0e75546b735be4e5dc829741c
yuva <yuva.phani@gmail.com>
2017-01-12 18:39:03 +0530

    Enhancement : Keep track of Precomputed Jobs using monitor-dp Script

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/JobFactory.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/JobFactory.scala
@@ -1,9 +1,28 @@
 package org.ekstep.analytics.job
 
+import org.ekstep.analytics.framework.IJob
 import org.ekstep.analytics.framework.exception.JobNotFoundException
-import org.ekstep.analytics.framework._
+import org.ekstep.analytics.job.consolidated.RawTelemetryJobs
+import org.ekstep.analytics.job.consolidated.RawTelemetryUpdaters
+import org.ekstep.analytics.job.consolidated.SessionSummaryJobs
+import org.ekstep.analytics.job.summarizer.AserScreenSummarizer
+import org.ekstep.analytics.job.summarizer.CSVDumpJob
+import org.ekstep.analytics.job.summarizer.ContentPopularitySummarizer
+import org.ekstep.analytics.job.summarizer.ContentSideloadingSummarizer
+import org.ekstep.analytics.job.summarizer.ContentToVecJob
 import org.ekstep.analytics.job.summarizer.ContentUsageSummarizer
+import org.ekstep.analytics.job.summarizer.DeviceContentUsageSummarizer
+import org.ekstep.analytics.job.summarizer.DeviceRecommendationScoringJob
+import org.ekstep.analytics.job.summarizer.DeviceRecommendationTrainingJob
 import org.ekstep.analytics.job.summarizer.DeviceUsageSummarizer
+import org.ekstep.analytics.job.summarizer.GenieFunnelAggregatorJob
+import org.ekstep.analytics.job.summarizer.GenieFunnelSummarizer
+import org.ekstep.analytics.job.summarizer.GenieLaunchSummarizer
+import org.ekstep.analytics.job.summarizer.GenieStageSummarizer
+import org.ekstep.analytics.job.summarizer.GenieUsageSessionSummarizer
+import org.ekstep.analytics.job.summarizer.GenieUsageSummarizer
+import org.ekstep.analytics.job.summarizer.ItemSummarizer
+import org.ekstep.analytics.job.summarizer.ItemUsageSummarizer
 import org.ekstep.analytics.job.summarizer.LearnerActivitySummarizer
 import org.ekstep.analytics.job.summarizer.AserScreenSummarizer
 import org.ekstep.analytics.job.summarizer.ContentSideloadingSummarizer
@@ -12,9 +31,9 @@ import org.ekstep.analytics.job.summarizer.DeviceRecommendationTrainingJob
 import org.ekstep.analytics.job.summarizer.GenieUsageSessionSummarizer
 import org.ekstep.analytics.job.summarizer.GenieLaunchSummarizer
 import org.ekstep.analytics.job.summarizer.RecommendationEngineJob
-import org.ekstep.analytics.job.summarizer.ContentPopularitySummarizer
-import org.ekstep.analytics.job.summarizer.DeviceContentUsageSummarizer
-import org.ekstep.analytics.job.updater.LearnerContentActivityUpdater
+import org.ekstep.analytics.job.summarizer.StageSummarizer
+import org.ekstep.analytics.job.updater.ContentModelUpdater
+import org.ekstep.analytics.job.updater.ContentPopularityUpdater
 import org.ekstep.analytics.job.updater.LearnerProfileUpdater
 import org.ekstep.analytics.job.updater.ContentUsageUpdater
 import org.ekstep.analytics.job.updater.ProficiencyUpdater
@@ -32,11 +51,11 @@ import org.ekstep.analytics.job.summarizer.GenieUsageSummarizer
 import org.ekstep.analytics.job.updater.GenieUsageUpdater
 import org.ekstep.analytics.job.summarizer.ItemSummarizer
 import org.ekstep.analytics.job.updater.ItemSummaryUpdater
-import org.ekstep.analytics.job.updater.ContentPopularityUpdater
-import org.ekstep.analytics.job.updater.ContentModelUpdater
-import org.ekstep.analytics.job.summarizer.GenieStageSummarizer
-import org.ekstep.analytics.job.summarizer.GenieFunnelSummarizer
-import org.ekstep.analytics.job.summarizer.GenieFunnelAggregatorJob
+import org.ekstep.analytics.job.updater.LearnerContentActivityUpdater
+import org.ekstep.analytics.job.updater.LearnerProfileUpdater
+import org.ekstep.analytics.job.updater.LearnerSnapshotUpdater
+import org.ekstep.analytics.job.updater.ProficiencyUpdater
+import org.ekstep.analytics.views.PrecomputedViewsJob
 
 object JobFactory {
     @throws(classOf[JobNotFoundException])
@@ -114,6 +133,8 @@ object JobFactory {
                 GenieFunnelAggregatorJob
             case "data-exhaust" =>
                 DataExhaustJob
+            case "precomputed" =>
+                PrecomputedViewsJob
             case _ =>
                 throw new JobNotFoundException("Unknown job type found");
         }
