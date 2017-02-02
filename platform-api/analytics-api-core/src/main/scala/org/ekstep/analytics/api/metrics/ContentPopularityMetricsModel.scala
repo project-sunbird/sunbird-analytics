0d24df2343ec46e813e6f7f7a36d2d737dbefcbc
amit behera <amitb@ilimi.in>
2017-02-02 18:59:03 +0530

    Issue #669 fix: Change the format of API output response for Content Popularity Metrics

--- platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/metrics/ContentPopularityMetricsModel.scala
+++ platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/metrics/ContentPopularityMetricsModel.scala
@@ -6,11 +6,13 @@ import org.apache.spark.SparkContext
 import com.typesafe.config.Config
 import org.apache.spark.rdd.RDD
 import org.ekstep.analytics.api.util.CommonUtil
+import org.ekstep.analytics.api.ContentPopularityViews
+import org.ekstep.analytics.api.Rating
 
-object ContentPopularityMetricsModel extends IMetricsModel[ContentPopularityMetrics, ContentPopularityMetrics]  with Serializable {
+object ContentPopularityMetricsModel extends IMetricsModel[ContentPopularityViews, ContentPopularityMetrics]  with Serializable {
 	override def metric : String = "cps";
 	
-	override def getMetrics(records: RDD[ContentPopularityMetrics], period: String, fields: Array[String] = Array())(implicit sc: SparkContext, config: Config): RDD[ContentPopularityMetrics] = {
+	override def getMetrics(records: RDD[ContentPopularityViews], period: String, fields: Array[String] = Array())(implicit sc: SparkContext, config: Config): RDD[ContentPopularityMetrics] = {
 	    val periodEnum = periodMap.get(period).get._1;
 		val periods = _getPeriods(period);
 		val addComments = returnComments(fields);
@@ -26,11 +28,13 @@ object ContentPopularityMetricsModel extends IMetricsModel[ContentPopularityMetr
 		};
 	}
 
-	private def _merge(obj: ContentPopularityMetrics, dummy: ContentPopularityMetrics, addComments: Boolean): ContentPopularityMetrics = {
-        if (addComments)
-        	ContentPopularityMetrics(dummy.d_period, dummy.label, obj.m_comments, obj.m_downloads, obj.m_side_loads, obj.m_ratings, obj.m_avg_rating)
+	private def _merge(obj: ContentPopularityViews, dummy: ContentPopularityMetrics, addComments: Boolean): ContentPopularityMetrics = {
+        
+	    val m_ratings = obj.m_ratings.getOrElse(List()).map{x=> Rating(x._1, x._2)}
+	    if (addComments)
+        	ContentPopularityMetrics(dummy.d_period, dummy.label, obj.m_comments, obj.m_downloads, obj.m_side_loads, Option(m_ratings), obj.m_avg_rating)
         else
-        	ContentPopularityMetrics(dummy.d_period, dummy.label, None, obj.m_downloads, obj.m_side_loads, obj.m_ratings, obj.m_avg_rating)
+        	ContentPopularityMetrics(dummy.d_period, dummy.label, None, obj.m_downloads, obj.m_side_loads, Option(m_ratings), obj.m_avg_rating)
     }
 	
 	private def returnComments(fields: Array[String] = Array()) : Boolean = {
@@ -42,7 +46,7 @@ object ContentPopularityMetricsModel extends IMetricsModel[ContentPopularityMetr
 		val m_side_loads = fact2.m_side_loads.getOrElse(0l).asInstanceOf[Number].longValue() + fact1.m_side_loads.getOrElse(0l).asInstanceOf[Number].longValue();
 		val m_ratings = (fact2.m_ratings.getOrElse(List()) ++ fact1.m_ratings.getOrElse(List())).distinct;
 		val m_avg_rating = if (m_ratings.length > 0) {
-			val total_rating = m_ratings.map(_._1).sum;
+			val total_rating = m_ratings.map(_.rating).sum;
 			if (total_rating > 0) CommonUtil.roundDouble(total_rating/m_ratings.length, 2) else 0.0;
 		} else 0.0;
 		if(returnComments(fields)) {
