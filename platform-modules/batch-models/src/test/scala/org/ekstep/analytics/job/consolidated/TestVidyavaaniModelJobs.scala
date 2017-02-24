fb51479183b3666d45bdd2d927d72b44f6dca4e7
amit behera <amitb@ilimi.in>
2017-02-24 20:46:25 +0530

    Issue #689 feat: new data product to add Content-Asset relationship

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/consolidated/TestVidyavaaniModelJobs.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/consolidated/TestVidyavaaniModelJobs.scala
@@ -2,9 +2,9 @@ package org.ekstep.analytics.job.consolidated
 
 import org.ekstep.analytics.model.SparkSpec
 
-class TestVidyavaaniNeo4jModelJobs extends SparkSpec(null) {
+class TestVidyavaaniModelJobs extends SparkSpec(null) {
     
     it should "run Vidyavaani Neo4j Enhancement Jobs and does not throw any exception" in {
-        VidyavaaniNeo4jModelJobs.main("{}")(Option(sc));    
+        VidyavaaniModelJobs.main("{}")(Option(sc));    
     }
 }
