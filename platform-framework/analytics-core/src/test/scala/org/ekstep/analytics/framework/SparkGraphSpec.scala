032c7781c17fdbf55428daa59d459652a557bb53
Mahesh <mahesh@canopusconsulting.com>
2017-03-02 12:14:05 +0530

    Issue #712 feat: Use embedded neo4j to run test cases (with test scope)

--- platform-framework/analytics-core/src/test/scala/org/ekstep/analytics/framework/SparkGraphSpec.scala
+++ platform-framework/analytics-core/src/test/scala/org/ekstep/analytics/framework/SparkGraphSpec.scala
@@ -9,30 +9,40 @@ import org.neo4j.graphdb.schema.IndexDefinition
 import org.neo4j.graphdb.Label
 import java.util.concurrent.TimeUnit
 import org.ekstep.analytics.framework.conf.AppConf
+import org.apache.commons.lang3.StringUtils
 
 class SparkGraphSpec extends SparkSpec {
 	
-	var graphDb: GraphDatabaseService = null 
+	var graphDb: GraphDatabaseService = null
 	
 	override def beforeAll() {
         super.beforeAll();
-        println("Start Embedded Neo4j...");
-        val bolt = GraphDatabaseSettings.boltConnector("0");
-		graphDb = new GraphDatabaseFactory()
-			.newEmbeddedDatabaseBuilder(new File(AppConf.getConfig("graph.service.embedded.dbpath")))
-			.setConfig(bolt.`type`, "BOLT")
-			.setConfig(bolt.enabled, "true")
-			.setConfig(bolt.address, "localhost:7687")
-			.newGraphDatabase();
-		sys.addShutdownHook {
-			graphDb.shutdown();
+		if (embeddedMode) {
+	        println("Starting Embedded Neo4j...");
+	        val bolt = GraphDatabaseSettings.boltConnector("0");
+			graphDb = new GraphDatabaseFactory()
+				.newEmbeddedDatabaseBuilder(new File(AppConf.getConfig("graph.service.embedded.dbpath")))
+				.setConfig(bolt.`type`, "BOLT")
+				.setConfig(bolt.enabled, "true")
+				.setConfig(bolt.address, "localhost:7687")
+				.newGraphDatabase();
+			sys.addShutdownHook {
+				graphDb.shutdown();
+			}
 		}
     }
 	
     override def afterAll() {
         super.afterAll();
-        println("Stop Embedded Neo4j...");
-        if (null != graphDb) graphDb.shutdown();
+        if (embeddedMode) {
+        	println("Stopping Embedded Neo4j...");
+        	if (null != graphDb) graphDb.shutdown();	
+        }
+    }
+    
+    private def embeddedMode() : Boolean = {
+    	val isEmbedded = AppConf.getConfig("graph.service.embedded.enable");
+    	StringUtils.isNotBlank(isEmbedded) && StringUtils.equalsIgnoreCase("true", isEmbedded);
     }
   
 }
--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/SparkGraphSpec.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/SparkGraphSpec.scala
@@ -5,6 +5,7 @@ import org.neo4j.graphdb.factory.GraphDatabaseSettings
 import org.neo4j.graphdb.factory.GraphDatabaseFactory
 import java.io.File
 import org.ekstep.analytics.framework.conf.AppConf
+import org.apache.commons.lang3.StringUtils
 
 class SparkGraphSpec(override val file: String =  "src/test/resources/sample_telemetry.log") extends SparkSpec(file) {
 	
@@ -12,23 +13,32 @@ class SparkGraphSpec(override val file: String =  "src/test/resources/sample_tel
 	
 	override def beforeAll() {
         super.beforeAll();
-        println("Start Embedded Neo4j...");
-        val bolt = GraphDatabaseSettings.boltConnector("0");
-		graphDb = new GraphDatabaseFactory()
-			.newEmbeddedDatabaseBuilder(new File(AppConf.getConfig("graph.service.embedded.dbpath")))
-			.setConfig(bolt.`type`, "BOLT")
-			.setConfig(bolt.enabled, "true")
-			.setConfig(bolt.address, "localhost:7687")
-			.newGraphDatabase();
-		sys.addShutdownHook {
-			graphDb.shutdown();
-		}
+        if (embeddedMode) {
+        	println("Starting Embedded Neo4j...");
+	        val bolt = GraphDatabaseSettings.boltConnector("0");
+			graphDb = new GraphDatabaseFactory()
+				.newEmbeddedDatabaseBuilder(new File(AppConf.getConfig("graph.service.embedded.dbpath")))
+				.setConfig(bolt.`type`, "BOLT")
+				.setConfig(bolt.enabled, "true")
+				.setConfig(bolt.address, "localhost:7687")
+				.newGraphDatabase();
+			sys.addShutdownHook {
+				graphDb.shutdown();
+			}	
+        }
     }
 	
     override def afterAll() {
         super.afterAll();
-        println("Stop Embedded Neo4j...");
-        if (null != graphDb) graphDb.shutdown();
+        if(embeddedMode) {
+        	println("Stop Embedded Neo4j...");
+        	if (null != graphDb) graphDb.shutdown();
+        }
+    }
+    
+    private def embeddedMode() : Boolean = {
+    	val isEmbedded = AppConf.getConfig("graph.service.embedded.enable");
+    	StringUtils.isNotBlank(isEmbedded) && StringUtils.equalsIgnoreCase("true", isEmbedded);
     }
   
 }
