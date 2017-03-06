17246a9db4201e5fa44c88a8a5a8c3ed11743aa6
amit behera <amitb@ilimi.in>
2017-03-06 20:01:10 +0530

    Issue #689 feat: debugging

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/vidyavaani/job/ContentAssetRelationModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/vidyavaani/job/ContentAssetRelationModel.scala
@@ -77,9 +77,14 @@ object ContentAssetRelationModel extends optional.Application with IJob {
             assestIds;
         } else {
             val mediaList = JSONUtils.deserialize[Map[String, Map[String, AnyRef]]](body).get("theme").get.get("manifest").get.asInstanceOf[Map[String, AnyRef]].get("media").get.asInstanceOf[List[Map[String, AnyRef]]];
-            println("mediaList: ",JSONUtils.serialize(mediaList))
+            //println("mediaList: ",JSONUtils.serialize(mediaList))
             mediaList.map { x =>
                 //println(JSONUtils.serialize(x))
+                if(!x.isInstanceOf[String]){
+                    println(JSONUtils.serialize(x))
+                }else{
+                    println("=== AssetId as String ===")
+                }
                 x.getOrElse("assetId", "").asInstanceOf[String]
             }.filter { x => !"".equals(x) }.toArray
         }
