1bdf00defd253e4db75e34a709e1f98cb20f1ea2
yuva <yuva.phani@gmail.com>
2017-03-06 15:21:06 +0530

    Issue #706 feat: Added Consumption Metrics (Content metrics)

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/updater/ConsumptionMetricsUpdater.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/updater/ConsumptionMetricsUpdater.scala
@@ -20,6 +20,7 @@ import scala.concurrent.duration._
 import org.ekstep.analytics.updater.GenieUsageSummaryFact
 import org.ekstep.analytics.util.Constants
 import org.joda.time.DateTime
+import org.ekstep.analytics.util.ContentUsageSummaryFact
 
 /**
  * @author mahesh
@@ -51,32 +52,38 @@ object ConsumptionMetricsUpdater extends Application with IJob {
 		}
 	}
 
-	private def execute(config: JobConfig)(implicit sc: SparkContext) = {
-		val modelParams = config.modelParams.get;
-		val periodType = modelParams.getOrElse("periodType", WEEK.toString()).asInstanceOf[String];
-		val periodUpTo = modelParams.getOrElse("periodUpTo", 100).asInstanceOf[Int];
-		val metrics = if ("ALL".equals(periodType)) {
-			ALL_PERIOD_TYPES.map { period => getGenieMetrics(period, periodUpTo) }.reduce((a,b) => a ++ b);
-		} else {
-			getGenieMetrics(periodType, periodUpTo);
-		}
-		import com.pygmalios.reactiveinflux.spark._
-		implicit val params = ReactiveInfluxDbName(AppConf.getConfig("reactiveinflux.database"))
+    private def execute(config: JobConfig)(implicit sc: SparkContext) = {
+        val modelParams = config.modelParams.get;
+        val periodType = modelParams.getOrElse("periodType", WEEK.toString()).asInstanceOf[String];
+        val periodUpTo = modelParams.getOrElse("periodUpTo", 100).asInstanceOf[Int];
+        val metrics = if ("ALL".equals(periodType)) {
+            ALL_PERIOD_TYPES.map { period => getMetrics(period, periodUpTo) }.reduce((a,b) => a ++ b);
+        } else {
+            getMetrics(periodType, periodUpTo);
+        }
+        import com.pygmalios.reactiveinflux.spark._
+        implicit val params = ReactiveInfluxDbName(AppConf.getConfig("reactiveinflux.database"))
         implicit val awaitAtMost = Integer.parseInt(AppConf.getConfig("reactiveinflux.awaitatmost")).second
         metrics.saveToInflux()
         JobLogger.end("ConsumptionMetricsUpdater Job Completed.", "SUCCESS", Option(Map("date" -> "", "inputEvents" -> metrics.count(), "outputEvents" -> metrics.count(), "timeTaken" -> 0)));
-	}
+    }
 
-	private def getGenieMetrics(periodType: String, periodUpTo: Int)(implicit sc: SparkContext): RDD[Point] = {
-		val periods = CommonUtil.getPeriods(periodType, periodUpTo).toList;
-		val genieRdd = sc.cassandraTable[GenieUsageSummaryFact](Constants.CONTENT_KEY_SPACE_NAME, Constants.GENIE_LAUNCH_SUMMARY_FACT).where("d_period IN?", periods).map { x => x };
-		val metrics = genieRdd.map { x => Point(time = getDateTime(periodType, x.d_period.toString), measurement = GENIE_METRICS, tags = Map("env" -> AppConf.getConfig("application.env"), "period" -> periodType.toLowerCase(), "tag" -> x.d_tag), fields = Map("sessions" -> x.m_total_sessions.toDouble, "timespent" -> x.m_total_ts)) };
-		metrics
-	}
+    private def getMetrics(periodType: String, periodUpTo: Int)(implicit sc: SparkContext): RDD[Point] = {
+        val periods = CommonUtil.getPeriods(periodType, periodUpTo).toList;
+        val genieMetrics = getGenieMetrics(periodType, periods)
+        val contentMetrics = getContentMetrics(periodType, periods)
+        genieMetrics ++ contentMetrics
+    }
 
-	private def getContentMetrics(periods: List[Int])(implicit sc: SparkContext): RDD[Point] = {
-		sc.emptyRDD[Point];
-	}
+    private def getGenieMetrics(periodType: String, periods: List[Int])(implicit sc: SparkContext): RDD[Point] = {
+        val genieRdd = sc.cassandraTable[GenieUsageSummaryFact](Constants.CONTENT_KEY_SPACE_NAME, Constants.GENIE_LAUNCH_SUMMARY_FACT).where("d_period IN?", periods).map { x => x };
+        genieRdd.map { x => Point(time = getDateTime(periodType, x.d_period.toString), measurement = GENIE_METRICS, tags = Map("env" -> AppConf.getConfig("application.env"), "period" -> periodType.toLowerCase(), "tag" -> x.d_tag), fields = Map("sessions" -> x.m_total_sessions.toDouble, "timespent" -> x.m_total_ts)) };
+    }
+
+    private def getContentMetrics(periodType: String, periods: List[Int])(implicit sc: SparkContext): RDD[Point] = {
+        val contentRdd = sc.cassandraTable[ContentUsageSummaryFact](Constants.CONTENT_KEY_SPACE_NAME, Constants.CONTENT_USAGE_SUMMARY_FACT).where("d_period IN?", periods).map { x => x };
+        contentRdd.map { x => Point(time = getDateTime(periodType, x.d_period.toString), measurement = CONTENT_METRICS, tags = Map("env" -> AppConf.getConfig("application.env"), "period" -> periodType.toLowerCase(), "tag" -> x.d_tag, "content" -> x.d_content_id), fields = Map("sessions" -> x.m_total_sessions.toDouble, "timespent" -> x.m_total_ts)) };
+    }
 
 	private def getDateTime(periodType: String, period: String): DateTime = {
 		periodType match {
