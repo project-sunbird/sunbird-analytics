fe38e94ad550690f4f7e1b116ee255dc7c0a6f22
Mahesh <mahesh@canopusconsulting.com>
2017-03-08 11:02:50 +0530

    Issue ekstep/Learning-Platform-Analytics#706 fix: update test cases.

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/updater/TestConsumptionMetricsUpdater.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/updater/TestConsumptionMetricsUpdater.scala
@@ -67,7 +67,7 @@ class TestConsumptionMetricsUpdater extends SparkSpec(null) {
     it should "validate timespent for period in content_metrics " in {
         implicit val awaitAtMost = 10.seconds
         syncInfluxDb(new URI(AppConf.getConfig("reactiveinflux.url")), AppConf.getConfig("reactiveinflux.database")) { db =>
-            val queryResult = db.query("SELECT timespent FROM content_metrics where period = 'day' ")
+            val queryResult = db.query("SELECT timespent FROM content_metrics where period = 'week' ")
             queryResult.rows.map { x => x.mkString.split(",")(1).trim() } should be(List("20"))
         }
     }
