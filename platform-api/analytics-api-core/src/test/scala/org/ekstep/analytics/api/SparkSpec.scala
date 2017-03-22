d61a115ac0d268aa9d068d072a311087d8bb2582
Jitendra Singh Sankhwar <jitendras@ilimi.in>
2017-03-22 13:16:47 +0530

    Issue #721 feat: Remove unused dependancy and refactor code.

--- platform-api/analytics-api-core/src/test/scala/org/ekstep/analytics/api/SparkSpec.scala
+++ platform-api/analytics-api-core/src/test/scala/org/ekstep/analytics/api/SparkSpec.scala
@@ -2,15 +2,10 @@ package org.ekstep.analytics.api
 
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.cassandraunit.CQLDataLoader
-import org.cassandraunit.dataset.cql.FileCQLDataSet
-import org.cassandraunit.utils.EmbeddedCassandraServerHelper
 import org.ekstep.analytics.api.util.CommonUtil
 import org.ekstep.analytics.api.util.JSONUtils
 import org.scalatest.BeforeAndAfterAll
 
-import com.datastax.spark.connector.cql.CassandraConnector
-import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 
 
@@ -30,8 +25,6 @@ class SparkSpec extends BaseSpec with BeforeAndAfterAll {
 
   override def afterAll() {
     CommonUtil.closeSparkContext();
-    EmbeddedCassandraServerHelper.cleanEmbeddedCassandra();
-    EmbeddedCassandraServerHelper.stopEmbeddedCassandra();
   }
 
   def loadFile[T](file: String)(implicit mf: Manifest[T]): RDD[T] = {
