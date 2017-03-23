7550647acaf5b4f995c89dadfa5c4d26f66e94a7
Mahesh <mahesh@canopusconsulting.com>
2017-03-23 13:24:23 +0530

    Issue #725 fix: API Mock implementation

--- platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/recommend/CreationRecommendations.scala
+++ platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/recommend/CreationRecommendations.scala
@@ -52,8 +52,8 @@ object CreationRecommendations extends IRecommendations {
         }
     }
     
-    private def getMockResponse(limit: Int) : List[Map[String, String]] = {
-    	val languages = List("Kannada","Punjabi","Odia","Hindi","Assamese","Tamil","Gujarati","English","Telugu","Bengali","Marathi");
+    private def getMockResponse(limit: Int) : List[Map[String, AnyRef]] = {
+    	val languages = List(Map("code" -> "as","name" -> "Assamese"),Map("code" -> "bn","name" -> "Bengali"),Map("code" -> "en","name" -> "English"),Map("code" -> "gu","name" -> "Gujarati"),Map("code" -> "hi","name" -> "Hindi"),Map("code" -> "ka","name" -> "Kannada"),Map("code" -> "mr","name" -> "Marathi"),Map("code" -> "or","name" -> "Odia"),Map("code" -> "ta","name" -> "Tamil"),Map("code" -> "te","name" -> "Telugu"));
         val concepts = List("Num:C2:SC1","Num:C4:SC6","Num:C1:SC2:MC12","Num:C1:SC3:MC5","Num:C2:SC1:MC6","Num:C3:SC6","Num:C1:SC2:MC3","Num:C1:SC2:MC20","Num:C1:SC3:MC13","Num:C2:SC1:MC14","Num:C1:SC3","Num:C1:SC2:MC11","Num:C4:SC7","Num:C2:SC1:MC5","Num:C1:SC3:MC6","Num:C3:SC5","Num:C1:SC2:MC19","Num:C1:SC2:MC4","Num:C2:SC1:MC13","Num:C1:SC3:MC14","Num:C4:SC4","Num:C1:SC2:MC10","Num:C2:SC3","Num:C1:SC3:MC7");
         val contentTypes = List("Story", "WorkSheet", "Game");
         val gradeLevels = List("Grade 1", "Grade 2", "Grade 3", "Grade 4", "Grade 5");
@@ -61,9 +61,9 @@ object CreationRecommendations extends IRecommendations {
         val mockResponse = for (i <- 1 to limit) yield {
         	Map("type" -> "Content", 
         		"language" -> languages(random.nextInt(languages.size)), 
-        		"concept" -> concepts(random.nextInt(concepts.size)), 
+        		"concepts" -> List(concepts(random.nextInt(concepts.size))),
         		"contentType" -> contentTypes(random.nextInt(contentTypes.size)), 
-        		"gradeLevel" -> gradeLevels(random.nextInt(gradeLevels.size)));
+        		"gradeLevel" -> List(gradeLevels(random.nextInt(gradeLevels.size))));
         };
         mockResponse.toList
     }
