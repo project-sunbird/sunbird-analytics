8d3804916fee6ad2b8861f567269ea83766fb5b1
sowmya-dixit <sowmyadixit7@gmail.com>
2017-03-27 11:32:03 +0530

    Issue #753 refactor: [VidyaVaani] Enhancements in ContentLanguageRelationModel.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/vidyavaani/job/ContentLanguageRelationModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/vidyavaani/job/ContentLanguageRelationModel.scala
@@ -21,8 +21,9 @@ object ContentLanguageRelationModel extends optional.Application with IJob {
     val NODE_NAME = "Language";
     val RELATION = "expressedIn";
     implicit val className = "org.ekstep.analytics.vidyavaani.job.ContentLanguageRelationModel"
-//    val findQuery =  "MATCH(n:domain{IL_FUNC_OBJECT_TYPE:'Content'}) WHERE n.contentType IN ['Story', 'Game', 'Collection', 'Worksheet'] RETURN n"
     val findQuery =  "MATCH (n:domain) where n.IL_FUNC_OBJECT_TYPE = 'Content' AND n.contentType IN ['Story', 'Game', 'Collection', 'Worksheet'] return n.language, n.IL_UNIQUE_ID" 
+    val realtionQuery = "MATCH (n:domain{IL_FUNC_OBJECT_TYPE:'Content'}), (l:Language{}) WHERE n.contentType IN ['Story', 'Game', 'Collection', 'Worksheet'] AND l.IL_UNIQUE_ID IN extract(language IN n.language | lower(language)) CREATE (n)-[r:expressedIn]->(l) RETURN r"
+    val deleteQuery = "MATCH (l:Language{}) DETACH DELETE l"
     
     def main(config: String)(implicit sc: Option[SparkContext] = None) {
 
@@ -50,44 +51,30 @@ object ContentLanguageRelationModel extends optional.Application with IJob {
     private def execute()(implicit sc: SparkContext) {
 
         val time = CommonUtil.time({
-            cleanUp();
-            algorithm();
+            val graphDBConfig = Map("url" -> AppConf.getConfig("neo4j.bolt.url"), "user" -> AppConf.getConfig("neo4j.bolt.user"), "password" -> AppConf.getConfig("neo4j.bolt.password"));
+            cleanUp(graphDBConfig);
+            algorithm(graphDBConfig);
         })
-
         JobLogger.end("ContentLanguageRelationModel Completed", "SUCCESS", Option(Map("date" -> "", "inputEvents" -> 0, "outputEvents" -> 0, "timeTaken" -> time._1)));
     }
 
-    private def algorithm()(implicit sc: SparkContext) = {
-        val limit = if (StringUtils.isNotBlank(AppConf.getConfig("graph.content.limit")))
-            Option(Integer.parseInt(AppConf.getConfig("graph.content.limit"))) else None
+    private def algorithm(graphDBConfig: Map[String, String])(implicit sc: SparkContext) = {
 
-        val graphDBConfig = Map("url" -> AppConf.getConfig("neo4j.bolt.url"), "user" -> AppConf.getConfig("neo4j.bolt.user"), "password" -> AppConf.getConfig("neo4j.bolt.password"));
         val contentNodes = GraphQueryDispatcher.dispatch(graphDBConfig, findQuery);
         val res = contentNodes.list().map { x => (x.get("n.language").asList(), x.get("n.IL_UNIQUE_ID").asString()) }
           .map(f => for (i <- f._1) yield (i.toString().toLowerCase(), f._2)).flatMap(f => f)
           .filter(f => StringUtils.isNoneBlank(f._1) && StringUtils.isNoneBlank(f._2))
         val contentLanguage = sc.parallelize(res)
-//        val contentNodes = GraphDBUtil.executeQuery(findQuery, "n") //GraphDBUtil.findNodes(Map("IL_FUNC_OBJECT_TYPE" -> "Content"), Option(List("domain")), limit, Option("WHERE ee.contentType IN ['Story', 'Game', 'Collection', 'Worksheet']"));
-//        val contentLanguage = contentNodes.map { x => x.metadata.getOrElse(Map()) }
-//            .map(f => (f.getOrElse("language", new java.util.ArrayList()).asInstanceOf[java.util.List[String]], f.getOrElse("IL_UNIQUE_ID", "").asInstanceOf[String]))
-//            .map(f => for (i <- f._1) yield (i.toLowerCase(), f._2)).flatMap(f => f)
-//            .filter(f => StringUtils.isNoneBlank(f._1) && StringUtils.isNoneBlank(f._2))
         
         val languages = contentLanguage.groupByKey().map(f => (f._1, f._2.size))
             .map { f =>
                 DataNode(f._1.toLowerCase(), Option(Map("name" -> f._1, "contentCount" -> f._2.asInstanceOf[AnyRef])), Option(List(NODE_NAME)));
             }
         GraphDBUtil.createNodes(languages);
-            
-        val languageContentRels = contentLanguage.map { f =>
-                val startNode = DataNode(f._1.toLowerCase(), None, Option(List(NODE_NAME)));
-                val endNode = DataNode(f._2, None, Option(List("domain")));
-                Relation(startNode, endNode, RELATION, RelationshipDirection.INCOMING.toString);
-            };
-        GraphDBUtil.addRelations(languageContentRels);
+        GraphQueryDispatcher.dispatch(graphDBConfig, realtionQuery);
     }
     
-    private def cleanUp()(implicit sc: SparkContext) {
-        GraphDBUtil.deleteNodes(None, Option(List(NODE_NAME)))
+    private def cleanUp(graphDBConfig: Map[String, String])(implicit sc: SparkContext) {
+        GraphQueryDispatcher.dispatch(graphDBConfig, deleteQuery);
     }
 }
