1c4362eba2732e6231aec0e9a00ad142865ab9ca
sowmya-dixit <sowmyadixit7@gmail.com>
2017-04-05 18:34:04 +0530

    Issue #765 feat: CreationRecommendationModel implementation.

---
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/vidyavaani/job/CreationRecommendationModel.scala
@@ -0,0 +1,52 @@
+package org.ekstep.analytics.vidyavaani.job
+
+import org.ekstep.analytics.job.IGraphExecutionModel
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext
+import com.datastax.spark.connector._
+import org.ekstep.analytics.framework.Job_Config
+import org.ekstep.analytics.util.Constants
+import org.ekstep.analytics.framework.dispatcher.GraphQueryDispatcher
+
+case class Request(grade_level: List[String], concepts: List[String], content_type: String, language: Map[String, String], `type`: String)
+case class RequestRecos(uid: String, requests: List[Request])
+
+object CreationRecommendationModel extends IGraphExecutionModel with Serializable {
+  
+    override def name(): String = "CreationRecommendationModel";
+    override implicit val className = "org.ekstep.analytics.vidyavaani.job.CreationRecommendationModel";
+    
+    val getConceptsQuery = "MATCH path = (usr:User{type: 'author'})-[r:uses]-(cnc:domain{IL_FUNC_OBJECT_TYPE:'Concept'}) where cnc.contentCount > 0 AND r.lift > 1 return usr.IL_UNIQUE_ID, cnc.identifier, cnc.liveContentCount, r.confidence as conf , r.lift as lift ORDER BY lift DESC, conf DESC, cnc.liveContentCount ASC"
+    val getLangsQuery = "MATCH (usr:User{type:'author'})-[r:createdIn]->(lan:Language) where lan.contentCount > 0 AND r.lift > 1 return usr.IL_UNIQUE_ID, lan.IL_UNIQUE_ID, lan.liveContentCount, r.confidence as conf , r.lift as lift ORDER BY lift DESC, conf DESC , lan.liveContentCount ASC"
+    val getContentTypeQuery = "MATCH (usr:User{type:'author'})-[r:uses]->(cntt:ContentType) where cntt.contentCount > 0 AND r.lift > 1 return usr.IL_UNIQUE_ID, cntt.IL_UNIQUE_ID, cntt.liveContentCount, r.confidence as conf , r.lift as lift ORDER BY lift DESC, conf DESC , cntt.liveContentCount ASC"
+    
+    override def preProcess(input: RDD[String], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[String] = {
+        sc.parallelize(Seq(""));
+    }
+
+    override def algorithm(ppQueries: RDD[String], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[String] = {
+        
+        val concepts = GraphQueryDispatcher.dispatch(graphDBConfig, getConceptsQuery).list().toArray();
+        val langs = GraphQueryDispatcher.dispatch(graphDBConfig, getLangsQuery).list().toArray();
+        val contentTypes = GraphQueryDispatcher.dispatch(graphDBConfig, getContentTypeQuery).list().toArray();
+        
+        val authorConcepts = concepts.map(x => x.asInstanceOf[org.neo4j.driver.v1.Record]).map{x => (x.get("usr.IL_UNIQUE_ID").asString(), (x.get("cnc.identifier").asString(), x.get("cnc.liveContentCount").asLong(), x.get("conf").asDouble(), x.get("lift").asDouble()))}
+        val authorConceptsRDD = sc.parallelize(authorConcepts).groupByKey().map(f => (f._1, f._2.toList.take(5).map(x => (x._1, x._4))))
+        
+        val authorLang = langs.map(x => x.asInstanceOf[org.neo4j.driver.v1.Record]).map{x => (x.get("usr.IL_UNIQUE_ID").asString(), (x.get("lan.IL_UNIQUE_ID").asString(), x.get("lan.liveContentCount").asLong(), x.get("conf").asDouble(), x.get("lift").asDouble()))}
+        val authorLangRDD = sc.parallelize(authorLang).groupByKey().map(f => (f._1, f._2.toList.take(5).map(x => (x._1, x._4))))
+        
+        val authorContentType = contentTypes.map(x => x.asInstanceOf[org.neo4j.driver.v1.Record]).map{x => (x.get("usr.IL_UNIQUE_ID").asString(), (x.get("cntt.IL_UNIQUE_ID").asString(), x.get("cntt.liveContentCount").asLong(), x.get("conf").asDouble(), x.get("lift").asDouble()))}
+        val authorContentTypeRDD = sc.parallelize(authorContentType).groupByKey().map(f => (f._1, f._2.toList.take(5).map(x => (x._1, x._4))))
+        
+        val finalResult = authorConceptsRDD.join(authorLangRDD).join(authorContentTypeRDD).mapValues{ f =>
+            
+            val combinations = for(x <- f._1._1; y <- f._1._2; z <- f._2) yield (x, y, z)
+            val combinedLift = combinations.map(x => (x._1._1, x._2._1, x._3._1, (x._1._2 + x._2._2 + x._3._2))).sortBy(f => f._4).reverse
+            // TO-DO: Add code to Language and grade level
+            combinedLift.map(x => Request(List(""), List(x._1), x._3, Map("" -> x._2), "content")) 
+        }.map(f => RequestRecos(f._1, f._2))
+        finalResult.saveToCassandra(Constants.PLATFORM_KEY_SPACE_NAME, Constants.REQUEST_RECOS);
+        ppQueries;
+    }
+}
---
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/vidyavaani/job/TestCreationRecommendationModel.scala
@@ -0,0 +1,31 @@
+package org.ekstep.analytics.vidyavaani.job
+
+import org.ekstep.analytics.model.SparkGraphSpec
+import org.ekstep.analytics.framework.conf.AppConf
+import org.ekstep.analytics.framework.dispatcher.GraphQueryDispatcher
+
+class TestCreationRecommendationModel extends SparkGraphSpec(null) {
+  
+    it should "create recommendations for authors and populate the same into cassandra" in {
+
+        val graphConfig = Map("url" -> AppConf.getConfig("neo4j.bolt.url"),
+            "user" -> AppConf.getConfig("neo4j.bolt.user"),
+            "password" -> AppConf.getConfig("neo4j.bolt.password"));
+        
+        // Running all VV jobs
+        ContentLanguageRelationModel.main("{}")(Option(sc));
+        ConceptLanguageRelationModel.main("{}")(Option(sc));
+        ContentAssetRelationModel.main("{}")(Option(sc));
+        AuthorRelationsModel.main("{}")(Option(sc));
+        
+        val query1 = "MATCH (cnt:domain {IL_FUNC_OBJECT_TYPE:'Content'}) where cnt.contentType in ['Game', 'Worksheet', 'Story', 'Collection'] and cnt.status in ['Live', 'Draft'] WITH count(cnt) as ncount MATCH (usr:User{type: 'author'})-[r:uses]-(cnc:domain{IL_FUNC_OBJECT_TYPE:'Concept'}) WHERE cnc.contentCount > 0 WITH r, ncount, toFloat(r.contentCount)/(toFloat(usr.contentCount)) as confidence, (toFloat(r.contentCount)*toFloat(ncount))/(toFloat(usr.contentCount)*toFloat(cnc.contentCount)) as lift SET r.confidence = confidence, r.lift = lift"
+        val query2 = "MATCH (cnt:domain {IL_FUNC_OBJECT_TYPE:'Content'}) where cnt.contentType in ['Game', 'Worksheet', 'Story', 'Collection'] and cnt.status in ['Live', 'Draft'] WITH count(cnt) as ncount MATCH (usr:User{type:'author'})-[r:createdIn]->(lan:Language) WHERE lan.contentCount > 0 WITH r, ncount, toFloat(r.contentCount)/(toFloat(usr.contentCount)) as confidence, (toFloat(r.contentCount)*toFloat(ncount))/(toFloat(usr.contentCount)*toFloat(lan.contentCount)) as lift SET r.confidence = confidence, r.lift = lift"
+        val query3 = "MATCH (cnt:domain {IL_FUNC_OBJECT_TYPE:'Content'}) where cnt.contentType in ['Game', 'Worksheet', 'Story', 'Collection'] and cnt.status in ['Live', 'Draft'] WITH count(cnt) as ncount MATCH (usr:User{type:'author'})-[r:uses]->(cntt:ContentType) WHERE cntt.contentCount > 0 WITH r, ncount, toFloat(r.contentCount)/(toFloat(usr.contentCount)) as confidence, (toFloat(r.contentCount)*toFloat(ncount))/(toFloat(usr.contentCount)*toFloat(cntt.contentCount)) as lift SET r.confidence = confidence, r.lift = lift"
+        
+        GraphQueryDispatcher.dispatch(graphConfig, query1);
+        GraphQueryDispatcher.dispatch(graphConfig, query2);
+        GraphQueryDispatcher.dispatch(graphConfig, query3);
+        
+        CreationRecommendationModel.main("{}")(Option(sc));
+    }
+}
