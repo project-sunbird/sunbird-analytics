10b0f9247ef2c8bbc7b463e97683adae248bea0f
Mahesh <mahesh@canopusconsulting.com>
2017-04-06 17:45:04 +0530

    Issue #765 feat: update to add grade level.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/vidyavaani/job/CreationRecommendationModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/vidyavaani/job/CreationRecommendationModel.scala
@@ -17,7 +17,7 @@ object CreationRecommendationModel extends IGraphExecutionModel with Serializabl
     override def name(): String = "CreationRecommendationModel";
     override implicit val className = "org.ekstep.analytics.vidyavaani.job.CreationRecommendationModel";
     
-    val getConceptsQuery = "MATCH path = (usr:User{type: 'author'})-[r:uses]-(cnc:domain{IL_FUNC_OBJECT_TYPE:'Concept'}) where cnc.contentCount > 0 AND r.lift > 1 return usr.IL_UNIQUE_ID, cnc.IL_UNIQUE_ID, cnc.liveContentCount, r.confidence as conf , r.lift as lift ORDER BY lift DESC, conf DESC, cnc.liveContentCount ASC"
+    val getConceptsQuery = "MATCH path = (usr:User{type: 'author'})-[r:uses]-(cnc:domain{IL_FUNC_OBJECT_TYPE:'Concept'}) where cnc.contentCount > 0 AND r.lift > 1 return usr.IL_UNIQUE_ID, cnc.IL_UNIQUE_ID, cnc.gradeLevel, cnc.liveContentCount, r.confidence as conf , r.lift as lift ORDER BY lift DESC, conf DESC, cnc.liveContentCount ASC"
     val getLangsQuery = "MATCH (usr:User{type:'author'})-[r:createdIn]->(lan:domain{IL_FUNC_OBJECT_TYPE:'Language'}) where lan.contentCount > 0 AND r.lift > 1 return usr.IL_UNIQUE_ID, lan, r.confidence as conf , r.lift as lift ORDER BY lift DESC, conf DESC , lan.liveContentCount ASC"
     val getContentTypeQuery = "MATCH (usr:User{type:'author'})-[r:uses]->(cntt:ContentType) where cntt.contentCount > 0 AND r.lift > 1 return usr.IL_UNIQUE_ID, cntt.name, cntt.liveContentCount, r.confidence as conf , r.lift as lift ORDER BY lift DESC, conf DESC , cntt.liveContentCount ASC"
     
@@ -33,8 +33,8 @@ object CreationRecommendationModel extends IGraphExecutionModel with Serializabl
         val langs = GraphQueryDispatcher.dispatch(graphDBConfig, getLangsQuery).list().toArray();
         val contentTypes = GraphQueryDispatcher.dispatch(graphDBConfig, getContentTypeQuery).list().toArray();
         
-        val authorConcepts = concepts.map(x => x.asInstanceOf[org.neo4j.driver.v1.Record]).map{x => (x.get("usr.IL_UNIQUE_ID").asString(), (x.get("cnc.IL_UNIQUE_ID").asString(), x.get("cnc.liveContentCount").asLong(), x.get("conf").asDouble(), x.get("lift").asDouble()))}
-        val authorConceptsRDD = sc.parallelize(authorConcepts).groupByKey().map(f => (f._1, f._2.toList.take(author_reco_limit).map(x => (x._1, x._4))))
+        val authorConcepts = concepts.map(x => x.asInstanceOf[org.neo4j.driver.v1.Record]).map{x => (x.get("usr.IL_UNIQUE_ID").asString(), (x.get("cnc.IL_UNIQUE_ID").asString(), x.get("cnc.gradeLevel", List()).asInstanceOf[List[String]], x.get("cnc.liveContentCount").asLong(), x.get("conf").asDouble(), x.get("lift").asDouble()))}
+        val authorConceptsRDD = sc.parallelize(authorConcepts).groupByKey().map(f => (f._1, f._2.toList.take(author_reco_limit).map(x => (x._1, x._2, x._5))))
         
         val authorLang = langs.map(x => x.asInstanceOf[org.neo4j.driver.v1.Record]).map{x => (x.get("usr.IL_UNIQUE_ID").asString(), (x.get("lan").asMap().toMap.asInstanceOf[Map[String, String]], x.get("conf").asDouble(), x.get("lift").asDouble()))}
         val authorLangRDD = sc.parallelize(authorLang).groupByKey().map(f => (f._1, f._2.toList.take(author_reco_limit).map(x => (x._1, x._3))))
@@ -45,9 +45,8 @@ object CreationRecommendationModel extends IGraphExecutionModel with Serializabl
         val finalResult = authorConceptsRDD.join(authorLangRDD).join(authorContentTypeRDD).mapValues{ f =>
             
             val combinations = for(x <- f._1._1; y <- f._1._2; z <- f._2) yield (x, y, z)
-            val combinedLift = combinations.map(x => (x._1._1, x._2._1, x._3._1, (x._1._2 + x._2._2 + x._3._2))).sortBy(f => f._4).reverse
-            // TO-DO: Add code to Language and grade level
-            combinedLift.map(x => Request(List(""), List(x._1), x._3, x._2, "content")) 
+            val combinedLift = combinations.map(x => (x._1._2, x._1._1, x._2._1, x._3._1, (x._1._3 + x._2._2 + x._3._2))).sortBy(f => f._4).reverse
+            combinedLift.map(x => Request(x._1, List(x._2), x._4, x._3, "Content"));
         }.map(f => RequestRecos(f._1, f._2))
         finalResult.saveToCassandra(Constants.PLATFORM_KEY_SPACE_NAME, Constants.REQUEST_RECOS);
         ppQueries;
