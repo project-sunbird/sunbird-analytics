dfc7e1321c6a09f6621062a478973977e590bd93
sowmya-dixit <sowmyadixit7@gmail.com>
2017-04-06 12:59:36 +0530

    Issue #738 refactor: Refactor ContentLanguageRelationModel and all other test cases.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/vidyavaani/job/ContentLanguageRelationModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/vidyavaani/job/ContentLanguageRelationModel.scala
@@ -26,30 +26,14 @@ object ContentLanguageRelationModel extends IGraphExecutionModel with Serializab
     override def name(): String = "ContentLanguageRelationModel";
     override implicit val className = "org.ekstep.analytics.vidyavaani.job.ContentLanguageRelationModel";
 
-    val NODE_NAME = "Language";
-    val RELATION = "expressedIn";
-    var algorithmQueries: List[String] = List();
-
-    val findQuery = "MATCH (cnt:domain{IL_FUNC_OBJECT_TYPE: 'Content'}) WHERE lower(cnt.contentType)IN ['story', 'game', 'collection', 'worksheet'] AND cnt.status IN ['Draft', 'Review', 'Live'] RETURN cnt.language"
-
     override def preProcess(input: RDD[String], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[String] = {
         val job_config = sc.cassandraTable[Job_Config](Constants.PLATFORM_KEY_SPACE_NAME, Constants.JOB_CONFIG).where("category='vv' AND config_key=?", "content-lang-rel").first
-        val cleanupQueries = job_config.config_value.get("cleanupQueries").get
-        algorithmQueries = job_config.config_value.get("algorithmQueries").get
-        sc.parallelize(cleanupQueries, JobContext.parallelization);
+        val queries = job_config.config_value.get("cleanupQueries").get ++ job_config.config_value.get("algorithmQueries").get
+        sc.parallelize(queries, JobContext.parallelization);
     }
 
     override def algorithm(ppQueries: RDD[String], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[String] = {
-
-        val contentNodes = GraphQueryDispatcher.dispatch(graphDBConfig, findQuery);
-        val res = contentNodes.list().map { x => (x.get("cnt.language", new java.util.ArrayList()).asInstanceOf[java.util.List[String]]) }
-            .flatMap(f => f).filter(f => StringUtils.isNoneBlank(f))
-        val contentLanguage = sc.parallelize(res).map { x => x.toLowerCase() }.distinct()
-
-        val languages = contentLanguage.map { langName =>
-            DataNode(langName, Option(Map("name" -> langName)), Option(List(NODE_NAME)));
-        }
-        ppQueries.union(sc.parallelize(Seq(GraphDBUtil.createNodesQuery(languages)) ++ algorithmQueries, JobContext.parallelization));
+        ppQueries;
     }
 
 }
--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/vidyavaani/job/TestContentLanguageRelationModel.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/vidyavaani/job/TestContentLanguageRelationModel.scala
@@ -15,18 +15,15 @@ class TestContentLanguageRelationModel extends SparkGraphSpec(null) {
             "user" -> AppConf.getConfig("neo4j.bolt.user"),
             "password" -> AppConf.getConfig("neo4j.bolt.password"));
 
-        val deleteQuery = "MATCH (l:Language{}) DETACH DELETE l"
         val findContentNodesQuery =  "MATCH (n:domain) where n.IL_FUNC_OBJECT_TYPE = 'Content' AND n.contentType IN ['Story', 'Game', 'Collection', 'Worksheet'] return n.language, n.IL_UNIQUE_ID"
-        val findLanguageNodesQuery = "MATCH (l:Language) RETURN l"
-        val contentLangRelationsQuery = "MATCH (n: domain) -[r:expressedIn]-> (l: Language) RETURN r"
+        val findLanguageNodesQuery = "MATCH (lan:domain{IL_FUNC_OBJECT_TYPE:'Language'}) RETURN lan"
+        val contentLangRelationsQuery = "MATCH (n: domain) -[r:expressedIn]-> (l: domain{IL_FUNC_OBJECT_TYPE:'Language'}) RETURN r"
         
         val contentNodes = GraphQueryDispatcher.dispatch(graphConfig, findContentNodesQuery).list();
         contentNodes.size() should be(3)
         
-        GraphQueryDispatcher.dispatch(graphConfig, deleteQuery);
-        
         val langNodesBefore = GraphQueryDispatcher.dispatch(graphConfig, findLanguageNodesQuery).list();
-        langNodesBefore.size() should be(0)
+        langNodesBefore.size() should be(3)
 
         val contentLangRelBefore = GraphQueryDispatcher.dispatch(graphConfig, contentLangRelationsQuery).list;
         contentLangRelBefore.size should be(0)
@@ -38,12 +35,12 @@ class TestContentLanguageRelationModel extends SparkGraphSpec(null) {
         langNodesAfter.size() should be(3)
 
         // Check for contentCount and liveContentCount
-        val queryForEng = "MATCH (l: Language { IL_UNIQUE_ID:'english' }) RETURN l"
+        val queryForEng = "MATCH (l: domain{IL_FUNC_OBJECT_TYPE:'Language', IL_UNIQUE_ID:'lang_en' }) RETURN l"
         val eng = GraphQueryDispatcher.dispatch(graphConfig, queryForEng).list();
         eng.get(0).get("l").asMap().get("contentCount") should be(1)
         eng.get(0).get("l").asMap().get("liveContentCount") should be(1)
         
-        val queryForOther = "MATCH (l: Language { IL_UNIQUE_ID:'other' }) RETURN l"
+        val queryForOther = "MATCH (l: domain{IL_FUNC_OBJECT_TYPE:'Language', IL_UNIQUE_ID:'lang_oth' }) RETURN l"
         val other = GraphQueryDispatcher.dispatch(graphConfig, queryForOther).list();
         other.get(0).get("l").asMap().get("contentCount") should be(1)
         other.get(0).get("l").asMap().get("liveContentCount") should be(0)
@@ -52,26 +49,22 @@ class TestContentLanguageRelationModel extends SparkGraphSpec(null) {
         contentLangRelAfter.size should be(3)
         
         // check for relation between specific content & language
-        val query1 = "MATCH (n: domain{ IL_UNIQUE_ID:'org.ekstep.ra_ms_52d058e969702d5fe1ae0f00' }) -[r]-> (l: Language { IL_UNIQUE_ID:'other' }) RETURN type(r)"
+        val query1 = "MATCH (n: domain{ IL_UNIQUE_ID:'org.ekstep.ra_ms_52d058e969702d5fe1ae0f00' }) -[r]-> (l: domain{IL_FUNC_OBJECT_TYPE:'Language', IL_UNIQUE_ID:'lang_oth' }) RETURN type(r)"
         val rel1 = GraphQueryDispatcher.dispatch(graphConfig, query1).list;
         rel1.size() should be(1)
         rel1.get(0).asMap().get("type(r)") should be("expressedIn")
         
-        val query2 = "MATCH (n: domain{ IL_UNIQUE_ID:'page_1_image_0' }) -[r]-> (l: Language { IL_UNIQUE_ID:'marathi' }) RETURN type(r)"
+        val query2 = "MATCH (n: domain{ IL_UNIQUE_ID:'page_1_image_0' }) -[r]-> (l: domain{IL_FUNC_OBJECT_TYPE:'Language', IL_UNIQUE_ID:'lang_mr' }) RETURN type(r)"
         val rel2 = GraphQueryDispatcher.dispatch(graphConfig, query2).list;
         rel2.size() should be(0)
 
-        val languageContentRelQuery = "MATCH (l: Language) -[r:expressedIn]-> (n: domain) RETURN r"
+        val languageContentRelQuery = "MATCH (l: domain{IL_FUNC_OBJECT_TYPE:'Language'}) -[r:expressedIn]-> (n: domain) RETURN r"
         val languageContentRels = GraphQueryDispatcher.dispatch(graphConfig, languageContentRelQuery).list;
         languageContentRels.size should be(0)
 
-        val languageLanguageRelQuery = "MATCH (l1: Language) -[r:expressedIn]-> (l2: Language) RETURN r"
+        val languageLanguageRelQuery = "MATCH (l1: domain{IL_FUNC_OBJECT_TYPE:'Language'}) -[r:expressedIn]-> (l2: domain{IL_FUNC_OBJECT_TYPE:'Language'}) RETURN r"
         val languageLanguageRels = GraphQueryDispatcher.dispatch(graphConfig, languageLanguageRelQuery).list;
         languageLanguageRels.size should be(0)
-
-        val contentContentRelQuery = "MATCH (n1: domain) -[r:expressedIn]-> (n2: domain) RETURN r"
-        val contentContentRels = GraphQueryDispatcher.dispatch(graphConfig, contentContentRelQuery).list;
-        contentContentRels.size should be(0)
     }
     
 }
