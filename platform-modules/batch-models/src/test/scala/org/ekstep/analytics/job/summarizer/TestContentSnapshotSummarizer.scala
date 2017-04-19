6f8b1df551f08ea2b4c1d3ea220513a7389690df
sowmya-dixit <sowmyadixit7@gmail.com>
2017-04-19 15:27:44 +0530

    Issue #784 feat: ContentSnapshotSummarizer implementation.

---
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/summarizer/TestContentSnapshotSummarizer.scala
@@ -0,0 +1,24 @@
+package org.ekstep.analytics.job.summarizer
+
+import org.ekstep.analytics.framework.JobConfig
+import org.ekstep.analytics.framework.Fetcher
+import org.ekstep.analytics.framework.Query
+import org.ekstep.analytics.framework.Dispatcher
+import org.ekstep.analytics.framework.util.JSONUtils
+import org.ekstep.analytics.vidyavaani.job._
+import org.ekstep.analytics.model.SparkGraphSpec
+
+class TestContentSnapshotSummarizer extends SparkGraphSpec(null) {
+  
+    it should "execute ContentSnapshotSummarizer job and won't throw any Exception" in {
+        
+        // Running all dependent VV jobs
+        ContentLanguageRelationModel.main("{}")(Option(sc));
+        ConceptLanguageRelationModel.main("{}")(Option(sc));
+        ContentAssetRelationModel.main("{}")(Option(sc));
+        AuthorRelationsModel.main("{}")(Option(sc));
+        
+        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/sample_telemetry.log"))))), null, None, "org.ekstep.analytics.model.ContentSnapshotSummary", Option(Map("active_user_days_limit" -> 30.asInstanceOf[AnyRef])), Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestContentSnapshotSummarizer"))
+        ContentSnapshotSummarizer.main(JSONUtils.serialize(config))(Option(sc));
+    }
+}
