6a42caee24ed13b5679a28c3a3f10827cfb04531
amit behera <amitb@ilimi.in>
2017-04-21 15:16:53 +0530

    Issue #769 feat: Asset Snapshot Summarizer Test cases

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestAssetSnapshotSummaryModel.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestAssetSnapshotSummaryModel.scala
@@ -9,8 +9,34 @@ import org.ekstep.analytics.framework.dispatcher.GraphQueryDispatcher
 
 class TestAssetSnapshotSummaryModel extends SparkGraphSpec(null) {
 
-    "AssetSnapshotSummaryModel" should "generate Asset snapshot" in {
-        val event = AssetSnapshotSummaryModel.execute(sc.makeRDD(List(Empty())), None)
-        println(JSONUtils.serialize(event.take(1)))
+    "AssetSnapshotSummaryModel" should "generate Asset snapshot and compare between the total values and used value" in {
+
+        ContentAssetRelationModel.main("{}")(Option(sc));
+        
+        val event = AssetSnapshotSummaryModel.execute(sc.makeRDD(List(Empty())), None).collect.last
+
+        println(JSONUtils.serialize(event))
+
+        val eksMap = event.edata.eks.asInstanceOf[Map[String, AnyRef]]
+        
+        val totalImageCount = eksMap.get("total_images_count").get.asInstanceOf[Long]
+        val usedImageCount = eksMap.get("used_images_count").get.asInstanceOf[Long]
+        val totalAudioCount = eksMap.get("total_audio_count").get.asInstanceOf[Long]
+        val usedAudioCount = eksMap.get("used_audio_count").get.asInstanceOf[Long]
+        val totalQCount = eksMap.get("total_questions_count").get.asInstanceOf[Long]
+        val usedQCount = eksMap.get("used_questions_count").get.asInstanceOf[Long]
+        val totalActCount = eksMap.get("total_activities_count").get.asInstanceOf[Long]
+        val usedActCount = eksMap.get("used_activities_count").get.asInstanceOf[Long]
+        val totalTempCount = eksMap.get("total_templates_count").get.asInstanceOf[Long]
+        val usedTempCount = eksMap.get("used_templates_count").get.asInstanceOf[Long]
+        
+        totalImageCount should be >= usedImageCount
+        totalAudioCount should be >= usedAudioCount
+        totalQCount should be >= usedQCount
+        totalActCount should be >= usedActCount
+        totalTempCount should be >= usedTempCount
+        
+        totalImageCount should be (4L)
+        usedImageCount should be (4L)
     }
 }
