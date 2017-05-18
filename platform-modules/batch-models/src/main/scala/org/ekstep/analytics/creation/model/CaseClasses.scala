d34054ed98281774f1752d1ff09f27a937818caa
Jitendra Singh Sankhwar <jitendras@ilimi.in>
2017-05-18 13:29:07 +0530

    Issue #830 feat: Updated Creation event Structure.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/creation/model/CaseClasses.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/creation/model/CaseClasses.scala
@@ -3,17 +3,29 @@ package org.ekstep.analytics.creation.model
 import org.ekstep.analytics.framework.AlgoInput
 import org.ekstep.analytics.framework.Input
 
+
+/**
+ * @author Jitendra Singh Sankhwar
+ */
 class CaseClasses extends Serializable {}
 
 
+/**
+ * Models to support Creation event model. 
+ */
 @scala.beans.BeanInfo
-class PData(val id: String, val pid: String, val ver: String) extends Serializable;
+class CreationEks(val defaultPlugins: List[String], val loadtimes: Map[String, Long], val client: Map[String, String], val path: String, val method: String, val request: String, val response: String, val responseTime: Long, val status: Int, val uip: String, val `type`: String, val pluginid: String, val pluginver: String, val objectid: String, val stage: String, val containerid: String, val containerplugin: String, val target: String, val action: String, val err: String, val data: Map[String, AnyRef], val severity: String, val duration: Long, val uaspec: Map[String, String], val env: String, val id: String, val name: String, val url: String, val context: String, val targetid: String, val subtype: String) extends Serializable {}
 @scala.beans.BeanInfo
-class CData(val `type`: String, val id: String) extends Serializable;
+class CreationPData(val id: String, val pid: String, val ver: String) extends Serializable;
 @scala.beans.BeanInfo
-class Context(val sid: String, val content_id: String) extends Serializable;
+class CreationCData(val `type`: String, val id: String) extends Serializable;
 @scala.beans.BeanInfo
-class EData(val eks: Map[String, AnyRef]) extends Serializable;
+class CreationContext(val sid: String, val content_id: String) extends Serializable;
+@scala.beans.BeanInfo
+class CreationEData(val eks: CreationEks) extends Serializable;
 
+/**
+ * Creation event model 
+ */
 @scala.beans.BeanInfo
-case class CreationEvent(val eid: String, val ets: Long, val ver: String, val mid: String, val pdata: Option[PData] = None, val cdata: Option[List[CData]] = None, val uid: String, val context: Option[Context] = None, val rid: String, val edata: EData, val tags: List[AnyRef]) extends Input with AlgoInput;
+case class CreationEvent(val eid: String, val ets: Long, val ver: String, val mid: String, val pdata: Option[CreationPData] = None, val cdata: Option[List[CreationCData]] = None, val uid: String, val context: Option[CreationContext] = None, val rid: String, val edata: CreationEData, val tags: List[AnyRef]) extends Input with AlgoInput;
