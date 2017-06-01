f02cb0b332855f4cce333a3d494ed7f494e14600
sowmya-dixit <sowmyadixit7@gmail.com>
2017-06-01 14:19:00 +0530

    Issue #855 refactor: Content snapshot summarizer enhancements to add avg_creation_ts.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ContentSnapshotSummaryModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ContentSnapshotSummaryModel.scala
@@ -11,8 +11,11 @@ import org.joda.time.DateTime
 import org.ekstep.analytics.util.CypherQueries
 import scala.collection.JavaConversions._
 import org.apache.commons.lang3.StringUtils
+import com.datastax.spark.connector._
+import org.ekstep.analytics.util.Constants
+import org.ekstep.analytics.updater.CEUsageSummaryFact
 
-case class ContentSnapshotAlgoOutput(total_content_count: Long, live_content_count: Long, review_content_count: Long, total_user_count: Long = 0L, active_user_count: Long = 0L, author_id: String = "all", partner_id: String = "all") extends AlgoOutput
+case class ContentSnapshotAlgoOutput(total_content_count: Long, live_content_count: Long, review_content_count: Long, creation_ts: Double, avg_creation_ts: Double, total_user_count: Long = 0L, active_user_count: Long = 0L, author_id: String = "all", partner_id: String = "all") extends AlgoOutput
 
 object ContentSnapshotSummaryModel extends IBatchModelTemplate[DerivedEvent, DerivedEvent, ContentSnapshotAlgoOutput, MeasuredEvent] with Serializable {
 
@@ -27,6 +30,10 @@ object ContentSnapshotSummaryModel extends IBatchModelTemplate[DerivedEvent, Der
 
         val active_user_days_limit = config.getOrElse("active_user_days_limit", 30).asInstanceOf[Int];
         val days_limit_timestamp = new DateTime().minusDays(active_user_days_limit).getMillis
+
+        // Fetch content time spent from CE Usage Summary Fact table
+        val contentTimespent = sc.cassandraTable[CEUsageSummaryFact](Constants.CREATION_METRICS_KEY_SPACE_NAME, Constants.CE_USAGE_SUMMARY).where("d_period=?", 0).map(x => (x.d_content_id, x.total_ts)).filter(f => ((!"all".equals(f._1)) && f._2 > 0))
+        val contentTimespentMap = contentTimespent.collect().toMap
 
         // For author_id = all
         val total_user_count = GraphQueryDispatcher.dispatch(CypherQueries.CONTENT_SNAPSHOT_TOTAL_USER_COUNT).list().get(0).get("count(usr)").asLong();
@@ -43,19 +50,37 @@ object ContentSnapshotSummaryModel extends IBatchModelTemplate[DerivedEvent, Der
         val totalContentCount = contentCountByStatus.values.reduce((a, b) => a + b);
         val liveContentCount = contentCountByStatus.getOrElse("live", 0).asInstanceOf[Number].longValue();
         val reviewContentCount = contentCountByStatus.getOrElse("review", 0).asInstanceOf[Number].longValue();
-        val rdd1 = sc.makeRDD(List(ContentSnapshotAlgoOutput(totalContentCount, liveContentCount, reviewContentCount, total_user_count, active_user_count)))
+        val creationTs = contentTimespent.map(f => f._2).sum
+        val avgCreationTs = if (contentTimespent.count() > 0) creationTs / contentTimespent.count() else 0.0
+        val rdd1 = sc.makeRDD(List(ContentSnapshotAlgoOutput(totalContentCount, liveContentCount, reviewContentCount, creationTs, avgCreationTs, total_user_count, active_user_count)))
 
         // For specific author_id
         val contentCountPerAuthorByStatus = GraphQueryDispatcher.dispatch(CypherQueries.CONTENT_COUNT_PER_AUTHOR_BY_STATUS).list()
             .toArray().map { x => x.asInstanceOf[org.neo4j.driver.v1.Record] }
             .map { x => (x.get("identifier").asString(), x.get("status").asString(), x.get("count").asLong()) }.groupBy(f => f._1);
 
-        val rdd2 = sc.makeRDD(contentCountPerAuthorByStatus.map { f =>
+        val authorContentTSRDD = sc.makeRDD(GraphQueryDispatcher.dispatch(CypherQueries.AUTHOR_CONTENT_LIST).list()
+            .toArray().map { x => x.asInstanceOf[org.neo4j.driver.v1.Record] }
+            .map { x => (x.get("author").asString(), x.get("contentList").asList().toList.map { x => x.toString() }) }
+            .map { x =>
+                val contentTsList = for (f <- x._2) yield (f, contentTimespentMap.get(f).getOrElse(0.0))
+                val filteredContentTsList = contentTsList.filter(f => f._2 > 0.0)
+                val ts = filteredContentTsList.map(f => f._2).sum
+                val avg_ts = if (filteredContentTsList.size > 0) ts / filteredContentTsList.size else 0.0
+                (x._1, (ts, avg_ts))
+            })
+
+        val authorContentCount = sc.makeRDD(contentCountPerAuthorByStatus.map { f =>
             val total = f._2.map(f => f._3).reduce((a, b) => a + b);
             val live = f._2.filter(f => StringUtils.equals("live", f._2)).headOption.getOrElse((f._1, "live", 0L))._3;
             val review = f._2.filter(f => StringUtils.equals("review", f._2)).headOption.getOrElse((f._1, "review", 0L))._3;
-            ContentSnapshotAlgoOutput(total, live, review, 0, 0, f._1);
+            (f._1, (total, live, review))
         }.toList)
+
+        val rdd2 = authorContentCount.leftOuterJoin(authorContentTSRDD).map { f =>
+            val ts = f._2._2.getOrElse(0.0, 0.0)
+            ContentSnapshotAlgoOutput(f._2._1._1, f._2._1._2, f._2._1._3, ts._1, ts._2, 0, 0, f._1);
+        }
 
         // For specific partner_id
         val partner_user = GraphQueryDispatcher.dispatch(CypherQueries.CONTENT_SNAPSHOT_PARTNER_USER_COUNT).list().toArray().map { x => x.asInstanceOf[org.neo4j.driver.v1.Record] }.map { x => (x.get("usr.IL_UNIQUE_ID").asString(), x.get("cnt.createdFor").asList().toList, x.get("cnt.createdOn").asString()) }
@@ -67,17 +92,29 @@ object ContentSnapshotSummaryModel extends IBatchModelTemplate[DerivedEvent, Der
         val partnerActiveUserCountRDD = sc.parallelize(partner_active_users).filter(f => f._3 >= days_limit_timestamp).groupBy(f => f._1).map(x => (x._1, x._2.size.toLong))
         val partnerUserCountRDD = sc.parallelize(partner_user.map(x => (x._1, x._2))).map(f => (f._1, f._2.map { x => x.toString() }.filterNot { x => StringUtils.isBlank(x) })).map(f => for (i <- f._2) yield (f._1, i)).flatMap(f => f).groupBy(f => f._2).map(x => (x._1, x._2.map(x => x._2).toList.distinct.size.toLong))
 
+        val partnerContentTSRDD = sc.makeRDD(GraphQueryDispatcher.dispatch(CypherQueries.PARTNER_CONTENT_LIST).list()
+            .toArray().map { x => x.asInstanceOf[org.neo4j.driver.v1.Record] }
+            .map { x => (x.get("partner").asString(), x.get("contentList").asList().toList.map { x => x.toString() }) }
+            .map { x =>
+                val contentTsList = for (f <- x._2) yield (f, contentTimespentMap.get(f).getOrElse(0.0))
+                val filteredContentTsList = contentTsList.filter(f => f._2 > 0.0)
+                val ts = filteredContentTsList.map(f => f._2).sum
+                val avg_ts = if (filteredContentTsList.size > 0) ts / filteredContentTsList.size else 0.0
+                (x._1, (ts, avg_ts))
+            })
+
         val contentCountPerPartnerByStatus = GraphQueryDispatcher.dispatch(CypherQueries.CONTENT_COUNT_PER_PARTNER_BY_STATUS).list()
             .toArray().map { x => x.asInstanceOf[org.neo4j.driver.v1.Record] }
             .map { x => (x.get("identifier").asList().toList.map { x => x.toString() }.filterNot { x => StringUtils.isBlank(x) }, x.get("status").asString(), x.get("count").asLong()) }
             .map(f => for (i <- f._1) yield (i, f._2, f._3)).flatMap(f => f).groupBy(f => f._1)
 
-        val rdd3 = partnerUserCountRDD.leftOuterJoin(partnerActiveUserCountRDD).map { f =>
+        val rdd3 = partnerUserCountRDD.leftOuterJoin(partnerActiveUserCountRDD).leftOuterJoin(partnerContentTSRDD).map { f =>
             val contentCount = contentCountPerPartnerByStatus.get(f._1).get
             val total = contentCount.map(f => f._3).reduce((a, b) => a + b);
             val live = contentCount.filter(f => StringUtils.equals("live", f._2)).headOption.getOrElse((f._1, "live", 0L))._3;
             val review = contentCount.filter(f => StringUtils.equals("review", f._2)).headOption.getOrElse((f._1, "review", 0L))._3;
-            ContentSnapshotAlgoOutput(total, live, review, f._2._1, f._2._2.getOrElse(0),"all", f._1);
+            val ts = f._2._2.getOrElse(0.0, 0.0)
+            ContentSnapshotAlgoOutput(total, live, review, ts._1, ts._2, f._2._1._1, f._2._1._2.getOrElse(0), "all", f._1);
         }
 
         // For partner_id and author_id combinations
@@ -86,12 +123,28 @@ object ContentSnapshotSummaryModel extends IBatchModelTemplate[DerivedEvent, Der
             .map { x => (x.get("author").asString(), x.get("partner").asList().toList.map { x => x.toString() }.filterNot { x => StringUtils.isBlank(x) }, x.get("status").asString(), x.get("count").asLong()) }
             .map(f => for (i <- f._2) yield ((f._1, i), f._3, f._4)).flatMap(f => f).groupBy(f => f._1)
 
-        val rdd4 = sc.makeRDD(contentCountPerPartnerPerAuthorByStatus.map { f =>
+        val authorPartnerContentTSRDD = sc.makeRDD(GraphQueryDispatcher.dispatch(CypherQueries.AUTHOR_PARTNER_CONTENT_LIST).list()
+            .toArray().map { x => x.asInstanceOf[org.neo4j.driver.v1.Record] }
+            .map { x => (x.get("author").asString(), x.get("partner").asString(), x.get("contentList").asList().toList.map { x => x.toString() }) }
+            .map { x =>
+                val contentTsList = for (f <- x._3) yield (f, contentTimespentMap.get(f).getOrElse(0.0))
+                val filteredContentTsList = contentTsList.filter(f => f._2 > 0.0)
+                val ts = filteredContentTsList.map(f => f._2).sum
+                val avg_ts = if (filteredContentTsList.size > 0) ts / filteredContentTsList.size else 0.0
+                ((x._1, x._2), (ts, avg_ts))
+            })
+
+        val authorPartnerContentCountRDD = sc.makeRDD(contentCountPerPartnerPerAuthorByStatus.map { f =>
             val total = f._2.map(f => f._3).reduce((a, b) => a + b);
             val live = f._2.filter(f => StringUtils.equals("live", f._2)).headOption.getOrElse((f._1, "live", 0L))._3;
             val review = f._2.filter(f => StringUtils.equals("review", f._2)).headOption.getOrElse((f._1, "review", 0L))._3;
-            ContentSnapshotAlgoOutput(total, live, review, 0, 0, f._1._1, f._1._2);
+            ((f._1._1, f._1._2), (total, live, review));
         }.toList)
+
+        val rdd4 = authorPartnerContentCountRDD.leftOuterJoin(authorPartnerContentTSRDD).map { f =>
+            val ts = f._2._2.getOrElse(0.0, 0.0)
+            ContentSnapshotAlgoOutput(f._2._1._1, f._2._1._2, f._2._1._3, ts._1, ts._2, 0, 0, f._1._1, f._1._2);
+        }
 
         rdd1 ++ (rdd2) ++ (rdd3) ++ (rdd4);
     }
@@ -105,7 +158,9 @@ object ContentSnapshotSummaryModel extends IBatchModelTemplate[DerivedEvent, Der
                 "active_user_count" -> x.active_user_count,
                 "total_content_count" -> x.total_content_count,
                 "live_content_count" -> x.live_content_count,
-                "review_content_count" -> x.review_content_count);
+                "review_content_count" -> x.review_content_count,
+                "creation_ts" -> x.creation_ts,
+                "avg_creation_ts" -> x.avg_creation_ts);
             MeasuredEvent("ME_CONTENT_SNAPSHOT_SUMMARY", System.currentTimeMillis(), System.currentTimeMillis(), "1.0", mid, "", None, None,
                 Context(PData(config.getOrElse("producerId", "AnalyticsDataPipeline").asInstanceOf[String], config.getOrElse("modelId", "ContentSnapshotSummarizer").asInstanceOf[String], config.getOrElse("modelVersion", "1.0").asInstanceOf[String]), None, config.getOrElse("granularity", "SNAPSHOT").asInstanceOf[String], DtRange(System.currentTimeMillis(), System.currentTimeMillis())),
                 Dimensions(None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, None, Option(x.author_id), Option(x.partner_id)),
--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentSnapshotSummaryModel.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestContentSnapshotSummaryModel.scala
@@ -7,9 +7,11 @@ import org.ekstep.analytics.framework.conf.AppConf
 import org.joda.time.DateTime
 import org.ekstep.analytics.framework.dispatcher.GraphQueryDispatcher
 import org.apache.commons.lang3.StringUtils
+import com.datastax.spark.connector.cql.CassandraConnector
+import com.datastax.spark.connector._
 
 class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
-  
+
     "ContentSnapshotSummaryModel" should "generate content snapshot summary events" in {
 
         // Running all VV jobs
@@ -17,14 +19,23 @@ class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
         ConceptLanguageRelationModel.main("{}")(Option(sc));
         ContentAssetRelationModel.main("{}")(Option(sc));
         AuthorRelationsModel.main("{}")(Option(sc));
-        
+
+        // Populate ce usage summary fact table
+        CassandraConnector(sc.getConf).withSessionDo { session =>
+            session.execute("TRUNCATE creation_metrics_db.ce_usage_summary_fact");
+            session.execute("INSERT INTO creation_metrics_db.ce_usage_summary_fact(d_period, d_content_id, users_count, total_sessions, total_ts, avg_ts_session, updated_date) VALUES (0, 'org.ekstep.ra_ms_52d058e969702d5fe1ae0f00', 0, 0, 20.0, 20.0, 1475731808000);");
+            session.execute("INSERT INTO creation_metrics_db.ce_usage_summary_fact(d_period, d_content_id, users_count, total_sessions, total_ts, avg_ts_session, updated_date) VALUES (0, 'org.ekstep.ra_ms_52d02eae69702d0905cf0800', 0, 0, 0.0, 20.0, 1475731808000);");
+            session.execute("INSERT INTO creation_metrics_db.ce_usage_summary_fact(d_period, d_content_id, users_count, total_sessions, total_ts, avg_ts_session, updated_date) VALUES (0, 'org.ekstep.ra_ms_5391b1d669702d107e030000', 0, 0, 20.0, 20.0, 1475731808000);");
+            session.execute("INSERT INTO creation_metrics_db.ce_usage_summary_fact(d_period, d_content_id, users_count, total_sessions, total_ts, avg_ts_session, updated_date) VALUES (0, 'test_content', 0, 0, 20.0, 20.0, 1475731808000);");
+        }
+
         val rdd = ContentSnapshotSummaryModel.execute(sc.makeRDD(List()), None);
         val events = rdd.collect
 
         events.length should be(9)
-        
-        val event1 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "all") &&  StringUtils.equals(x.dimensions.partner_id.get, "all")}.first();
-        
+
+        val event1 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "all") && StringUtils.equals(x.dimensions.partner_id.get, "all") }.first();
+
         // Check for author_id = all
         event1.context.pdata.model should be("ContentSnapshotSummarizer");
         event1.context.pdata.ver should be("1.0");
@@ -39,9 +50,11 @@ class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
         eks1.get("review_content_count").get should be(0)
         eks1.get("total_user_count").get should be(2)
         eks1.get("active_user_count").get should be(0)
-        
-        val event2 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "290") &&  StringUtils.equals(x.dimensions.partner_id.get, "all")}.first();
-        
+        eks1.get("creation_ts").get should be(60.0)
+        eks1.get("avg_creation_ts").get should be(20.0)
+
+        val event2 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "290") && StringUtils.equals(x.dimensions.partner_id.get, "all") }.first();
+
         // Check for specific author_id
         event2.context.pdata.model should be("ContentSnapshotSummarizer");
         event2.context.pdata.ver should be("1.0");
@@ -56,9 +69,11 @@ class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
         eks2.get("review_content_count").get should be(0)
         eks2.get("total_user_count").get should be(0)
         eks2.get("active_user_count").get should be(0)
-        
-        val event3 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "291") &&  StringUtils.equals(x.dimensions.partner_id.get, "all")}.first();
-        
+        eks2.get("creation_ts").get should be(40.0)
+        eks2.get("avg_creation_ts").get should be(20.0)
+
+        val event3 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "291") && StringUtils.equals(x.dimensions.partner_id.get, "all") }.first();
+
         event3.context.pdata.model should be("ContentSnapshotSummarizer");
         event3.context.pdata.ver should be("1.0");
         event3.context.granularity should be("SNAPSHOT");
@@ -72,10 +87,12 @@ class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
         eks3.get("review_content_count").get should be(0)
         eks3.get("total_user_count").get should be(0)
         eks3.get("active_user_count").get should be(0)
-        
+        eks3.get("creation_ts").get should be(20.0)
+        eks3.get("avg_creation_ts").get should be(20.0)
+
         // check for specific partner_id
-        val event4 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "all") &&  StringUtils.equals(x.dimensions.partner_id.get, "org.ekstep.partner.pratham")}.first();
-        
+        val event4 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "all") && StringUtils.equals(x.dimensions.partner_id.get, "org.ekstep.partner.pratham") }.first();
+
         event4.context.pdata.model should be("ContentSnapshotSummarizer");
         event4.context.pdata.ver should be("1.0");
         event4.context.granularity should be("SNAPSHOT");
@@ -89,10 +106,12 @@ class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
         eks4.get("review_content_count").get should be(0)
         eks4.get("total_user_count").get should be(1)
         eks4.get("active_user_count").get should be(0)
-        
+        eks4.get("creation_ts").get should be(40.0)
+        eks4.get("avg_creation_ts").get should be(20.0)
+
         // check for partner_id & author_id combination
-        val event5 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "290") &&  StringUtils.equals(x.dimensions.partner_id.get, "org.ekstep.partner.pratham")}.first();
-        
+        val event5 = rdd.filter { x => StringUtils.equals(x.dimensions.author_id.get, "290") && StringUtils.equals(x.dimensions.partner_id.get, "org.ekstep.partner.pratham") }.first();
+
         event5.context.pdata.model should be("ContentSnapshotSummarizer");
         event5.context.pdata.ver should be("1.0");
         event5.context.granularity should be("SNAPSHOT");
@@ -106,8 +125,11 @@ class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
         eks5.get("review_content_count").get should be(0)
         eks5.get("total_user_count").get should be(0)
         eks5.get("active_user_count").get should be(0)
+        eks5.get("creation_ts").get should be(40.0)
+        eks5.get("avg_creation_ts").get should be(20.0)
+
     }
-    
+
     it should "generate content snapshot summary event for active user count > 0" in {
 
         // Running all VV jobs
@@ -115,19 +137,23 @@ class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
         ConceptLanguageRelationModel.main("{}")(Option(sc));
         ContentAssetRelationModel.main("{}")(Option(sc));
         AuthorRelationsModel.main("{}")(Option(sc));
-        
+
+        CassandraConnector(sc.getConf).withSessionDo { session =>
+            session.execute("TRUNCATE creation_metrics_db.ce_usage_summary_fact");
+        }
+
         val createdOn = new DateTime().toString()
         val query1 = s"CREATE (usr:User {type:'author', IL_UNIQUE_ID:'test_author', contentCount:0, liveContentCount:0})<-[r:createdBy]-(cnt: domain{IL_FUNC_OBJECT_TYPE:'Content', IL_UNIQUE_ID:'test_content', contentType:'story', createdFor:['org.ekstep.partner1'], createdOn:'$createdOn'}) RETURN usr.IL_UNIQUE_ID, cnt.createdOn"
         GraphQueryDispatcher.dispatch(query1)
-        
+
         val rdd = ContentSnapshotSummaryModel.execute(sc.makeRDD(List()), None);
         val events = rdd.collect
 
         events.length should be(11)
-        
+
         // Check for author_id = all
         val event1 = events(0);
-        
+
         event1.context.pdata.model should be("ContentSnapshotSummarizer");
         event1.context.pdata.ver should be("1.0");
         event1.context.granularity should be("SNAPSHOT");
@@ -141,10 +167,12 @@ class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
         eks1.get("review_content_count").get should be(0)
         eks1.get("total_user_count").get should be(3)
         eks1.get("active_user_count").get should be(1)
-        
+        eks1.get("creation_ts").get should be(0.0)
+        eks1.get("avg_creation_ts").get should be(0.0)
+
         // Check for specific partner_id
         val event2 = events(5);
-        
+
         event2.context.pdata.model should be("ContentSnapshotSummarizer");
         event2.context.pdata.ver should be("1.0");
         event2.context.granularity should be("SNAPSHOT");
@@ -158,28 +186,30 @@ class TestContentSnapshotSummaryModel extends SparkGraphSpec(null) {
         eks2.get("review_content_count").get should be(0)
         eks2.get("total_user_count").get should be(1)
         eks2.get("active_user_count").get should be(1)
-        
+        eks2.get("creation_ts").get should be(0.0)
+        eks2.get("avg_creation_ts").get should be(0.0)
+
     }
-    
+
     it should "check content snapshot summary event for empty partner_id" in {
-        
+
         // Running all VV jobs
         ContentLanguageRelationModel.main("{}")(Option(sc));
         ConceptLanguageRelationModel.main("{}")(Option(sc));
         ContentAssetRelationModel.main("{}")(Option(sc));
         AuthorRelationsModel.main("{}")(Option(sc));
-        
+
         val createdOn = new DateTime().toString()
         val query1 = s"CREATE (usr:User {type:'author', IL_UNIQUE_ID:'test_author_1', contentCount:0, liveContentCount:0})<-[r:createdBy]-(cnt: domain{IL_FUNC_OBJECT_TYPE:'Content', IL_UNIQUE_ID:'test_content_1', contentType:'story', createdFor:[''], createdOn:'$createdOn'}) RETURN usr.IL_UNIQUE_ID, cnt.createdOn, cnt.createdFor"
         val res = GraphQueryDispatcher.dispatch(query1)
-        
+
         val rdd = ContentSnapshotSummaryModel.execute(sc.makeRDD(List()), None);
         val events = rdd.collect
 
         events.length should be(10)
-        
+
         val out = rdd.filter { x => StringUtils.isBlank(x.dimensions.partner_id.get) }
         out.count() should be(0)
-        
+
     }
 }
