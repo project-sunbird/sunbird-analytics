d56da9b49a0a7a0d8afef306ac39d4ff11858b6d
yuva <yuva.phani@gmail.com>
2017-06-05 15:58:28 +0530

    Issue #859 refactor:[Data Product] remove println statements.

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestAuthorUsageSummaryModel.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestAuthorUsageSummaryModel.scala
@@ -16,9 +16,6 @@ class TestAuthorUsageSummaryModel extends SparkSpec(null) {
     "AuthorUsageSummaryModel" should "generate metrics per author" in {
         val rdd3 = loadData()
         val events = rdd3.filter { x => (x.uid == "316") }.collect()
-        for(e<-events){
-            println(JSONUtils.serialize(e))
-        }
         val event3 = events(1).edata.eks.asInstanceOf[Map[String, AnyRef]]
         event3.get("total_session").get.asInstanceOf[Number].longValue() should be(3)
         event3.get("total_ts").get.asInstanceOf[Number].doubleValue() should be(874.8)
