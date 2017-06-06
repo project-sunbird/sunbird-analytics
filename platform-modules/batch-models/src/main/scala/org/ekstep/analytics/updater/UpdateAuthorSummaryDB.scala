481aa91512d696de47f559cbadc643a9d2c4b155
amit behera <amitb@ilimi.in>
2017-06-06 11:00:31 +0530

    Issue #861 refactor: refactor Author Usage Updater

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdateAuthorSummaryDB.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdateAuthorSummaryDB.scala
@@ -16,8 +16,8 @@ import org.ekstep.analytics.framework.AlgoInput
 import org.ekstep.analytics.connector.InfluxDB._
 import org.ekstep.analytics.framework.dispatcher.InfluxDBDispatcher.InfluxRecord
 
-case class AuthorMetricsFact_T(d_period: Int, d_author_id: String, total_sessions: Long, total_ts: Double, total_ce_ts: Double, total_ce_visit: Long, percent_ce_sessions: Double, avg_ts_session: Double, percent_ce_ts: Double, updated_date: Long, last_gen_date: Long)
-case class AuthorMetricsFact(d_period: Int, d_author_id: String, total_sessions: Long, total_ts: Double, total_ce_ts: Double, total_ce_visit: Long, percent_ce_sessions: Double, avg_ts_session: Double, percent_ce_ts: Double, updated_date: Long) extends AlgoOutput with Output
+case class AuthorMetricsFact_T(d_period: Int, d_author_id: String, total_sessions: Long, total_ts: Double, total_ce_ts: Double, total_ce_visit: Long, ce_visits_count: Long, percent_ce_sessions: Double, avg_ts_session: Double, percent_ce_ts: Double, updated_date: Long, last_gen_date: Long)
+case class AuthorMetricsFact(d_period: Int, d_author_id: String, total_sessions: Long, total_ts: Double, total_ce_ts: Double, total_ce_visit: Long, ce_visits_count: Long, percent_ce_sessions: Double, avg_ts_session: Double, percent_ce_ts: Double, updated_date: Long) extends AlgoOutput with Output
 case class AuthorMetricsIndex(d_period: Int, d_author_id: String)
 
 object UpdateAuthorSummaryDB extends IBatchModelTemplate[DerivedEvent, DerivedEvent, AuthorMetricsFact, AuthorMetricsFact] with IInfluxDBUpdater with Serializable {
@@ -25,7 +25,7 @@ object UpdateAuthorSummaryDB extends IBatchModelTemplate[DerivedEvent, DerivedEv
     override def name(): String = "UpdateAuthorSummaryDB";
     implicit val className = "org.ekstep.analytics.updater.UpdateAuthorSummaryDB";
     val AUTHOR_USAGE_METRICS = "author_usage_metrics";
-    
+
     override def preProcess(data: RDD[DerivedEvent], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[DerivedEvent] = {
         data;
     }
@@ -38,11 +38,11 @@ object UpdateAuthorSummaryDB extends IBatchModelTemplate[DerivedEvent, DerivedEv
             val totalTS = CommonUtil.roundDouble(eksMap.getOrElse("total_ts", 0.0).asInstanceOf[Double], 2)
             val totalCETS = CommonUtil.roundDouble(eksMap.getOrElse("ce_total_ts", 0.0).asInstanceOf[Double], 2)
             val totalCEVisits = eksMap.getOrElse("ce_total_visits", 0l).asInstanceOf[Number].longValue()
-            val ce_visits_occ = eksMap.getOrElse("ce_visits_occ", 0l).asInstanceOf[Number].longValue()
-            val percentCEsessions = (if (0 != totalSessions) (ce_visits_occ * 1.0 / totalSessions) else 0.0) * 100
+            val ce_visits_count = eksMap.getOrElse("ce_visits_count", 0l).asInstanceOf[Number].longValue()
+            val percentCEsessions = (if (0 != totalSessions) (ce_visits_count * 1.0 / totalSessions) else 0.0) * 100
             val avgSessionTS = CommonUtil.roundDouble(if (0 != totalSessions) (totalTS / totalSessions) else 0.0, 2)
             val percentCEts = CommonUtil.roundDouble((if (0 != totalTS) (totalCETS / totalTS) else 0.0) * 100, 2)
-            AuthorMetricsFact_T(period, author, totalSessions, totalTS, totalCETS, totalCEVisits, percentCEsessions, avgSessionTS, percentCEts, System.currentTimeMillis(), x.syncts)
+            AuthorMetricsFact_T(period, author, totalSessions, totalTS, totalCETS, totalCEVisits, ce_visits_count, percentCEsessions, avgSessionTS, percentCEts, System.currentTimeMillis(), x.syncts)
         }.cache
         rollup(authorMetrics, DAY).union(rollup(authorMetrics, WEEK)).union(rollup(authorMetrics, MONTH)).union(rollup(authorMetrics, CUMULATIVE)).cache();
     }
@@ -59,10 +59,10 @@ object UpdateAuthorSummaryDB extends IBatchModelTemplate[DerivedEvent, DerivedEv
             val time = getDateTime(x.d_period);
             InfluxRecord(Map("period" -> time._2, "author_id" -> x.d_author_id), fields, time._1);
         };
-        val authors  = getDenormalizedData("User", data.map { x => x.d_author_id })
+        val authors = getDenormalizedData("User", data.map { x => x.d_author_id })
         metrics.denormalize("author_id", "author_name", authors).saveToInflux(AUTHOR_USAGE_METRICS);
     }
-    
+
     private def rollup(data: RDD[AuthorMetricsFact_T], period: Period): RDD[AuthorMetricsFact] = {
 
         val currentData = data.map { x =>
@@ -74,7 +74,7 @@ object UpdateAuthorSummaryDB extends IBatchModelTemplate[DerivedEvent, DerivedEv
         val rollupSummaries = joinedData.map { x =>
             val index = x._1
             val newSumm = x._2._1
-            val prvSumm = x._2._2.getOrElse(AuthorMetricsFact(index.d_period, index.d_author_id, 0l, 0.0, 0.0, 0l, 0.0, 0.0, 0.0, System.currentTimeMillis()))
+            val prvSumm = x._2._2.getOrElse(AuthorMetricsFact(index.d_period, index.d_author_id, 0l, 0.0, 0.0, 0l, 0l, 0.0, 0.0, 0.0, System.currentTimeMillis()))
             reduce(prvSumm, newSumm, period);
         }
         rollupSummaries;
@@ -85,9 +85,10 @@ object UpdateAuthorSummaryDB extends IBatchModelTemplate[DerivedEvent, DerivedEv
         val totalTS = CommonUtil.roundDouble(fact1.total_ts + fact2.total_ts, 2)
         val totalCETS = CommonUtil.roundDouble(fact1.total_ce_ts + fact2.total_ce_ts, 2)
         val totalCEVisits = fact1.total_ce_visit + fact2.total_ce_visit
-        val percentCEsessions = CommonUtil.roundDouble((if (0 != totalSessions) (totalCEVisits * 1.0 / totalSessions) else 0.0) * 100, 2)
+        val ceVisitCount = fact1.ce_visits_count + fact2.ce_visits_count
+        val percentCEsessions = CommonUtil.roundDouble((if (0 != totalSessions) (ceVisitCount * 1.0 / totalSessions) else 0.0) * 100, 2)
         val avgSessionTS = CommonUtil.roundDouble(if (0 != totalSessions) (totalTS / totalSessions) else 0.0, 2)
         val percentCEts = CommonUtil.roundDouble((if (0 != totalTS) (totalCETS / totalTS) else 0.0) * 100, 2)
-        AuthorMetricsFact(fact1.d_period, fact1.d_author_id, totalSessions, totalTS, totalCETS, totalCEVisits, percentCEsessions, avgSessionTS, percentCEts, System.currentTimeMillis())
+        AuthorMetricsFact(fact1.d_period, fact1.d_author_id, totalSessions, totalTS, totalCETS, totalCEVisits, ceVisitCount, percentCEsessions, avgSessionTS, percentCEts, System.currentTimeMillis())
     }
 }
