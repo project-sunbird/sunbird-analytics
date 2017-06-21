efb0431f998b59519abe150fd04cfc83c1886c7d
Mahesh <mahesh@canopusconsulting.com>
2017-06-21 11:22:38 +0530

    Issue #000 fix: reset metrics-play integration

--- platform-api/analytics-api/app/Filters.scala
+++
@@ -1,9 +0,0 @@
-
-import javax.inject.Inject
-
-import com.kenshoo.play.metrics.MetricsFilter
-import play.api.http.HttpFilters
-
-class Filters @Inject() (metricsFilter: MetricsFilter) extends HttpFilters {
-        val filters = Seq(metricsFilter)
-}
