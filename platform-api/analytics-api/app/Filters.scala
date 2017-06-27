a6ab0203ba9d035f4d46cc6e75de4627580e9255
Mahesh <mahesh@canopusconsulting.com>
2017-06-27 22:36:00 +0530

    Issue #941 feat: metrics-play integration.

---
+++ platform-api/analytics-api/app/Filters.scala
@@ -0,0 +1,9 @@
+
+import javax.inject.Inject
+
+import com.kenshoo.play.metrics.MetricsFilter
+import play.api.http.HttpFilters
+
+class Filters @Inject() (metricsFilter: MetricsFilter) extends HttpFilters {
+        val filters = Seq(metricsFilter)
+}
