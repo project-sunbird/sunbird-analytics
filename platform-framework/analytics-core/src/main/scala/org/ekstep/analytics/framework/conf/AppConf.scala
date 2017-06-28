22dffb6d1c90cd7fc677ebf69d9235aeb06c5276
Mahesh <mahesh@canopusconsulting.com>
2017-06-28 10:56:04 +0530

    Issue #000 feat: added method to get Typesafe config object.

--- platform-framework/analytics-core/src/main/scala/org/ekstep/analytics/framework/conf/AppConf.scala
+++ platform-framework/analytics-core/src/main/scala/org/ekstep/analytics/framework/conf/AppConf.scala
@@ -1,26 +1,24 @@
 package org.ekstep.analytics.framework.conf
 
-import java.util.Properties
-import java.io.FileInputStream
-import scala.io.Source
-import org.ekstep.analytics.framework.util.JobLogger
 import com.typesafe.config.ConfigFactory
-import com.typesafe.config.ConfigParseOptions
-import com.typesafe.config.ConfigResolveOptions
+import com.typesafe.config.Config
 
 object AppConf {
 
     implicit val className = "org.ekstep.analytics.framework.conf.AppConf";
 
-    lazy val conf = ConfigFactory.load();
-    lazy val env = ConfigFactory.systemEnvironment();
+    lazy val defaultConf = ConfigFactory.load();
+    lazy val envConf = ConfigFactory.systemEnvironment();
+    lazy val conf = defaultConf.withFallback(envConf);
 
     def getConfig(key: String): String = {
-        if (env.hasPath(key))
-            env.getString(key);
-        else if (conf.hasPath(key))
+        if (conf.hasPath(key))
             conf.getString(key);
         else "";
+    }
+    
+    def getConfig(): Config = {
+    	return conf;
     }
 
     def getAwsKey(): String = {
