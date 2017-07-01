c255b1afecd811b97a9e57d3bcd52c6ff646f0f0
tusif86 <tausif.raza86@gmail.com>
2017-07-01 17:12:10 +0530

    Issue #934 refactor: Change UpdatePluginSnapshotMetric to UpdatePluginSnapshotDB

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/updater/PluginSnapshotMetricsUpdater.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/updater/PluginSnapshotMetricsUpdater.scala
@@ -4,13 +4,13 @@ import org.ekstep.analytics.framework.IJob
 import optional.Application
 import org.apache.spark.SparkContext
 import org.ekstep.analytics.framework.JobDriver
-import org.ekstep.analytics.updater.UpdatePluginSnapshotMetrics
+import org.ekstep.analytics.updater.UpdatePluginSnapshotDB
 
 object PluginSnapshotMetricsUpdater extends Application with IJob {
     implicit val className = "org.ekstep.analytics.job.updater.PluginSnapshotMetricsUpdater"
 
     def main(config: String)(implicit sc: Option[SparkContext] = None) {
         implicit val sparkContext: SparkContext = sc.getOrElse(null);
-        JobDriver.run("batch", config, UpdatePluginSnapshotMetrics);
+        JobDriver.run("batch", config, UpdatePluginSnapshotDB);
     }
 }
