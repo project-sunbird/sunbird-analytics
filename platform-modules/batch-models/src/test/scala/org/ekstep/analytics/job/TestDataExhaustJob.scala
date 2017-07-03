42cc9060a49f3ba5a47eeb0b3e80d84b697d57b2
amit behera <amitb@ilimi.in>
2017-07-03 13:13:09 +0530

    Issue #920 debug: Data Exhaust

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/TestDataExhaustJob.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/TestDataExhaustJob.scala
@@ -50,8 +50,10 @@ class TestDataExhaustJob extends SparkSpec(null) {
                 val prefix = fileDetails.get("prefix").get
                 deleteS3File(bucket, prefix, request_ids)
             case "local" =>
-                val path = fileDetails.get("path").get
-                deleteLocalFile(path)
+                for (request_id <- request_ids) {
+                    val path = fileDetails.get("path").get + "/" + request_id + ".zip"
+                    deleteLocalFile(path)
+                }
         }
     }
 
@@ -68,11 +70,8 @@ class TestDataExhaustJob extends SparkSpec(null) {
         val config = """{"search":{"type":"local","queries":[{"file":"src/test/resources/data-exhaust/creation-raw/*"}]},"model":"org.ekstep.analytics.model.DataExhaustJobModel","output":[{"to":"file","params":{"file": "/tmp/dataexhaust"}}],"parallelization":8,"appName":"Data Exhaust","deviceMapping":false,"modelParams":{}, "exhaustConfig":{"eks-consumption-raw":{"events":["DEFAULT"],"eventConfig":{"DEFAULT":{"eventType":"ConsumptionRaw","searchType":"local","saveType":"local","fetchConfig":{"params":{"file":"src/test/resources/data-exhaust/consumption-raw/*"}},"filterMapping":{"tags":{"name":"genieTag","operator":"IN"}},"saveConfig":{"params":{"path":"/tmp/data-exhaust/"}},"localPath":"/tmp/data-exhaust"}}}}}"""
         DataExhaustJob.main(config)(Option(sc));
 
-//        val files1 = new File("/tmp/data-exhaust/1234/OE_INTERACT").listFiles()
-//        files1.length should not be (0)
-//
-//        val files2 = new File("/tmp/data-exhaust/1234/GE_INTERACT").listFiles()
-//        files2.length should not be (0)
+        val files1 = new File("/tmp/data-exhaust/1234.zip").isFile()
+        files1 should be(true)
 
         val fileDetails = Map("fileType" -> "local", "path" -> "/tmp/data-exhaust")
         val request_ids = Array("1234")
@@ -91,33 +90,33 @@ class TestDataExhaustJob extends SparkSpec(null) {
 
         DataExhaustJob.main(config)(Option(sc));
 
-//        val files1 = new File("/tmp/data-exhaust/requestID1/ME_SESSION_SUMMARY").listFiles()
-//        files1.length should be > (0)
+        val files1 = new File("/tmp/data-exhaust/requestID1.zip").isFile()
+        files1 should be(true)
 
         val fileDetails = Map("fileType" -> "local", "path" -> "/tmp/data-exhaust")
         val request_ids = Array("requestID1")
         postProcess(fileDetails, request_ids)
     }
 
-        it should "test for creation raw data" in {
-            preProcess()
-    
-            val requests = Array(
-                JobRequest("client-key2", "requestID2", None, "SUBMITTED", JSONUtils.serialize(RequestConfig(RequestFilter("2017-06-22", "2017-06-22", None, Option(List("CP_IMPRESSION")), None, None), Option("eks-creation-raw"))),
-                    None, None, None, None, None, None, DateTime.now(), None, None, None, None, None, None, None, None, None, None))
-    
-            sc.makeRDD(requests).saveToCassandra(Constants.PLATFORM_KEY_SPACE_NAME, Constants.JOB_REQUEST)
-            val config = """{"search":{"type":"local","queries":[{"file":"src/test/resources/data-exhaust/creation-raw/*"}]},"model":"org.ekstep.analytics.model.DataExhaustJobModel","output":[{"to":"file","params":{"file": "/tmp/dataexhaust"}}],"parallelization":8,"appName":"Data Exhaust","deviceMapping":false, "modelParams":{}, "exhaustConfig":{"eks-creation-raw":{"events":["DEFAULT"],"eventConfig":{"DEFAULT":{"eventType":"CreationRaw","searchType":"local","saveType":"local","fetchConfig":{"params":{"file":"src/test/resources/data-exhaust/creation-raw/*"}},"filterMapping":{"tags":{"name":"genieTag","operator":"IN"}},"saveConfig":{"params":{"path":"/tmp/data-exhaust/"}},"localPath":"/tmp/data-exhaust"}}}}}"""
-    
-            DataExhaustJob.main(config)(Option(sc));
-    
-//            val files1 = new File("/tmp/data-exhaust/requestID2/").listFiles()
-//            files1.length should be > (0)
-        
-            val fileDetails = Map("fileType" -> "local", "path" -> "/tmp/data-exhaust")
-            val request_ids = Array("requestID2")
-            postProcess(fileDetails, request_ids)
-        }
+    it should "test for creation raw data" in {
+        preProcess()
+
+        val requests = Array(
+            JobRequest("client-key2", "requestID2", None, "SUBMITTED", JSONUtils.serialize(RequestConfig(RequestFilter("2017-06-22", "2017-06-22", None, Option(List("CP_IMPRESSION")), None, None), Option("eks-creation-raw"))),
+                None, None, None, None, None, None, DateTime.now(), None, None, None, None, None, None, None, None, None, None))
+
+        sc.makeRDD(requests).saveToCassandra(Constants.PLATFORM_KEY_SPACE_NAME, Constants.JOB_REQUEST)
+        val config = """{"search":{"type":"local","queries":[{"file":"src/test/resources/data-exhaust/creation-raw/*"}]},"model":"org.ekstep.analytics.model.DataExhaustJobModel","output":[{"to":"file","params":{"file": "/tmp/dataexhaust"}}],"parallelization":8,"appName":"Data Exhaust","deviceMapping":false, "modelParams":{}, "exhaustConfig":{"eks-creation-raw":{"events":["DEFAULT"],"eventConfig":{"DEFAULT":{"eventType":"CreationRaw","searchType":"local","saveType":"local","fetchConfig":{"params":{"file":"src/test/resources/data-exhaust/creation-raw/*"}},"filterMapping":{"tags":{"name":"genieTag","operator":"IN"}},"saveConfig":{"params":{"path":"/tmp/data-exhaust/"}},"localPath":"/tmp/data-exhaust"}}}}}"""
+
+        DataExhaustJob.main(config)(Option(sc));
+
+        val files1 = new File("/tmp/data-exhaust/requestID2.zip").isFile()
+        files1 should be(true)
+
+        val fileDetails = Map("fileType" -> "local", "path" -> "/tmp/data-exhaust")
+        val request_ids = Array("requestID2")
+        postProcess(fileDetails, request_ids)
+    }
 
     ignore should "run the data exhaust and save data to S3" in {
 
