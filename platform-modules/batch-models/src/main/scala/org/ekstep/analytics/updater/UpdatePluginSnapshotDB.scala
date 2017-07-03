d4ec91d1ebaf17d8e3466771e981ba5692067fd6
Mahesh <mahesh@canopusconsulting.com>
2017-07-03 06:34:18 +0530

    Issue #000 fix: plugin snapshot metrics fix.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdatePluginSnapshotDB.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdatePluginSnapshotDB.scala
@@ -23,8 +23,8 @@ import org.joda.time.DateTime
 import org.ekstep.analytics.framework.dispatcher.InfluxDBDispatcher
 import org.ekstep.analytics.connector.InfluxDB._
 
-case class PluginMetrics(d_plugin_id: String, plugin_name: String, domain: String, author: String, content_count: Int)
-case class PluginSnapshotMetrics(d_period: Int, d_plugin_id: String, plugin_name: String, domain: String, author: String, content_count: Int, content_count_start: Int, updated_date: Option[DateTime] = Option(DateTime.now())) extends AlgoOutput with Output
+case class PluginMetrics(d_plugin_id: String, plugin_name: String, category: String, author: String, content_count: Int)
+case class PluginSnapshotMetrics(d_period: Int, d_plugin_id: String, plugin_name: String, category: String, author: String, content_count: Int, content_count_start: Int, updated_date: Option[DateTime] = Option(DateTime.now())) extends AlgoOutput with Output
 case class PluginSnapshotIndex(d_period: Int, d_plugin_id: String)
 
 object UpdatePluginSnapshotDB extends IBatchModelTemplate[Empty, Empty, PluginSnapshotMetrics, PluginSnapshotMetrics] with IInfluxDBUpdater with Serializable {
@@ -38,7 +38,7 @@ object UpdatePluginSnapshotDB extends IBatchModelTemplate[Empty, Empty, PluginSn
 
     private def getPluginMetrics(query: String)(implicit sc: SparkContext): List[PluginMetrics] = {
         GraphQueryDispatcher.dispatch(query).list().map { x =>
-            PluginMetrics(x.get("plugin_id").asString(), x.get("name").asString(), x.get("domain").asList().last.asInstanceOf[String], x.get("author").asString(), x.get("contentCount").asInt)
+            PluginMetrics(x.get("plugin_id").asString(), x.get("name").asString(), x.get("category").asList().last.asInstanceOf[String], x.get("author").asString(), x.get("contentCount").asInt)
         }.toList
     }
     override def preProcess(data: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[Empty] = {
@@ -63,12 +63,12 @@ object UpdatePluginSnapshotDB extends IBatchModelTemplate[Empty, Empty, PluginSn
             val pulgin_id = x._1.d_plugin_id
             val plugin_name = x._2._1.plugin_name
             val author = x._2._1.author
-            val domain = x._2._1.domain
+            val category = x._2._1.category
             val content_count = x._2._1.content_count
             if (prevSumm == null) {
-                PluginSnapshotMetrics(period, pulgin_id, plugin_name, domain, author, content_count, content_count)
+                PluginSnapshotMetrics(period, pulgin_id, plugin_name, category, author, content_count, content_count)
             } else {
-                PluginSnapshotMetrics(period, pulgin_id, plugin_name, domain, author, content_count, prevSumm.content_count_start)
+                PluginSnapshotMetrics(period, pulgin_id, plugin_name, category, author, content_count, prevSumm.content_count_start)
             }
         }
     }
@@ -82,7 +82,7 @@ object UpdatePluginSnapshotDB extends IBatchModelTemplate[Empty, Empty, PluginSn
     private def saveToInfluxDB(data: RDD[PluginSnapshotMetrics])(implicit sc: SparkContext) {
         data.map { x =>
             val time = getDateTime(x.d_period);
-            InfluxRecord(Map("period" -> time._2, "plugin_id" -> x.d_plugin_id, "author" -> x.author, "domain" -> x.domain, "plugin_name" -> x.plugin_name), Map("content_count" -> x.content_count.asInstanceOf[AnyRef], "content_count_start" -> x.content_count_start.asInstanceOf[AnyRef]), time._1);
+            InfluxRecord(Map("period" -> time._2, "plugin_id" -> x.d_plugin_id, "author" -> x.author, "category" -> x.category, "plugin_name" -> x.plugin_name), Map("content_count" -> x.content_count.asInstanceOf[AnyRef], "content_count_start" -> x.content_count_start.asInstanceOf[AnyRef]), time._1);
 
         }.saveToInflux(PLUGIN_SNAPSHOT_METRICS);
 
