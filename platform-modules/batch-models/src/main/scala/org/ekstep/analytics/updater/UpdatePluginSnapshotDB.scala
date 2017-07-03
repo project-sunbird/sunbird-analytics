b5b86675a7fa65d33e460b8001e3e5a96f7c121b
Mahesh <mahesh@canopusconsulting.com>
2017-07-03 07:04:22 +0530

    Issue #000 fix: plugin snapshot metrics fix.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdatePluginSnapshotDB.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdatePluginSnapshotDB.scala
@@ -38,7 +38,7 @@ object UpdatePluginSnapshotDB extends IBatchModelTemplate[Empty, Empty, PluginSn
 
     private def getPluginMetrics(query: String)(implicit sc: SparkContext): List[PluginMetrics] = {
         GraphQueryDispatcher.dispatch(query).list().map { x =>
-            PluginMetrics(x.get("plugin_id").asString(), x.get("name").asString(), x.get("category").asList().last.asInstanceOf[String], x.get("author").asString(), x.get("contentCount").asInt)
+            PluginMetrics(x.get("plugin_id").asString(), x.get("name").asString(), x.get("category").asList().mkString(","), x.get("author").asString(), x.get("contentCount").asInt)
         }.toList
     }
     override def preProcess(data: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[Empty] = {
@@ -82,7 +82,7 @@ object UpdatePluginSnapshotDB extends IBatchModelTemplate[Empty, Empty, PluginSn
     private def saveToInfluxDB(data: RDD[PluginSnapshotMetrics])(implicit sc: SparkContext) {
         data.map { x =>
             val time = getDateTime(x.d_period);
-            InfluxRecord(Map("period" -> time._2, "plugin_id" -> x.d_plugin_id, "author" -> x.author, "category" -> x.category, "plugin_name" -> x.plugin_name), Map("content_count" -> x.content_count.asInstanceOf[AnyRef], "content_count_start" -> x.content_count_start.asInstanceOf[AnyRef]), time._1);
+            InfluxRecord(Map("period" -> time._2, "plugin_id" -> x.d_plugin_id), Map("author" -> x.author, "category" -> x.category, "plugin_name" -> x.plugin_name, "content_count" -> x.content_count.asInstanceOf[AnyRef], "content_count_start" -> x.content_count_start.asInstanceOf[AnyRef]), time._1);
 
         }.saveToInflux(PLUGIN_SNAPSHOT_METRICS);
 
