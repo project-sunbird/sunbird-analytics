0712f0c523f65b797d49d863e7396bb5b6f1f7b1
sowmya-dixit <sowmyadixit7@gmail.com>
2017-07-10 13:22:21 +0530

    Issue #947 refactor: channel & pdata changes in MonitorSummaryModel.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/MonitorSummaryModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/MonitorSummaryModel.scala
@@ -94,9 +94,9 @@ object MonitorSummaryModel extends IBatchModelTemplate[DerivedEvent, DerivedEven
                 "total_ts" -> x.total_ts,
                 "jobs_summary" -> x.job_summary);
 
-            MeasuredEvent("ME_MONITOR_SUMMARY", System.currentTimeMillis(), x.syncTs, "1.0", mid, "", None, None,
-                Context(PData(config.getOrElse("id", "AnalyticsDataPipeline").asInstanceOf[String], config.getOrElse("modelId", "MonitorSummarizer").asInstanceOf[String], config.getOrElse("modelVersion", "1.0").asInstanceOf[String]), None, "DAY", x.dtange),
-                Dimensions(None, None, None, None, None, None, None, None, None, None, Option(CommonUtil.getPeriod(x.syncTs, DAY)), None, None, None, None, None, None, None, None, None, None, None, None, None),
+            MeasuredEvent("ME_MONITOR_SUMMARY", System.currentTimeMillis(), x.syncTs, "1.0", mid, "", None, None, None,
+                Context(PData(config.getOrElse("id", "AnalyticsDataPipeline").asInstanceOf[String], config.getOrElse("modelVersion", "1.0").asInstanceOf[String], Option(config.getOrElse("modelId", "MonitorSummarizer").asInstanceOf[String])), None, "DAY", x.dtange),
+                Dimensions(None, None, None, None, None, None, None, None, None, None, None, Option(CommonUtil.getPeriod(x.syncTs, DAY)), None, None, None, None, None, None, None, None, None, None, None, None, None),
                 MEEdata(measures), None);
         }
     }
