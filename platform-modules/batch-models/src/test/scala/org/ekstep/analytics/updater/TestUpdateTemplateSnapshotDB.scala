fd23347ca484e202cc127e1c27f0db64a2d9904f
sowmya-dixit <sowmyadixit7@gmail.com>
2017-09-22 11:33:34 +0530

    Issue #1030 fix: author_id null in TemplateSnapshotUpdater.

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/updater/TestUpdateTemplateSnapshotDB.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/updater/TestUpdateTemplateSnapshotDB.scala
@@ -41,7 +41,7 @@ class TestUpdateTemplateSnapshotDB extends SparkGraphSpec(null) {
         val test_IdData = data.filter { x => "test-template1".equals(x.d_template_id) }.last
         test_IdData.d_template_id should be("test-template1")
         test_IdData.template_name should be("test-temp")
-        test_IdData.author_id should be("test-author1")
+        test_IdData.author_id.get should be("test-author1")
         test_IdData.category.get should be("domain")
         test_IdData.content_count should be(3)
         test_IdData.question_count should be(1)
