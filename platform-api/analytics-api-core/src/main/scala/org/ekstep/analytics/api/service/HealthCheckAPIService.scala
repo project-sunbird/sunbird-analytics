f2b080c48ce04634e822e7e8d064a5174339d1ff
Jitendra Singh Sankhwar <jitendras@ilimi.in>
2017-10-09 17:36:45 +0530

    Issue #1033 refactor: Remove spark dependency from Health API and update Job API test cases

--- platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/HealthCheckAPIService.scala
+++ platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/HealthCheckAPIService.scala
@@ -1,24 +1,18 @@
 package org.ekstep.analytics.api.service
 
 import org.ekstep.analytics.api.util.CommonUtil
-import org.ekstep.analytics.api.Response
-import org.apache.spark.SparkContext
+import org.ekstep.analytics.api.util.DBUtil
 import org.ekstep.analytics.api.util.JSONUtils
-import com.datastax.spark.connector._
-import org.apache.spark.rdd.RDD
-import org.ekstep.analytics.api.ContentUsageSummaryFact
-import org.ekstep.analytics.api.Constants
-import com.datastax.spark.connector.cql.CassandraConnector
-import akka.actor.Props
+
 import akka.actor.Actor
 
 case class ServiceHealthReport(name: String, healthy: Boolean, message: Option[String] = None)
 
 object HealthCheckAPIService {
   
-    case class GetHealthStatus(sc: SparkContext)
+    case class GetHealthStatus()
   
-    def getHealthStatus()(implicit sc: SparkContext): String = {
+    def getHealthStatus(): String = {
 
         val checks = getChecks()
         val healthy = checks.forall { x => x.healthy == true }
@@ -30,12 +24,9 @@ object HealthCheckAPIService {
         JSONUtils.serialize(response);
     }
 
-    private def checkCassandraConnection()(implicit sc: SparkContext): Boolean = {
+    private def checkCassandraConnection(): Boolean = {
         try {
-            val connector = CassandraConnector(sc.getConf);
-            val session = connector.openSession();
-            session.close();
-            true;
+            DBUtil.checkCassandraConnection
         } catch {
             // $COVERAGE-OFF$ Disabling scoverage as the below code cannot be covered
             // TODO: Need to get confirmation from amit.
@@ -45,11 +36,8 @@ object HealthCheckAPIService {
         }
     }
 
-    private def getChecks()(implicit sc: SparkContext): Array[ServiceHealthReport] = {
+    private def getChecks(): Array[ServiceHealthReport] = {
         try {
-            val nums = Array(10, 5, 18, 4, 8, 56)
-            val rdd = sc.parallelize(nums)
-            rdd.sortBy(f => f).collect
             val sparkReport = ServiceHealthReport("Spark Cluster", true);
             val cassReport = ServiceHealthReport("Cassandra Database", checkCassandraConnection());
             Array(sparkReport, cassReport);
@@ -64,16 +52,16 @@ object HealthCheckAPIService {
         }
     }
     
-    def main(args: Array[String]): Unit = {
-        implicit val sc = CommonUtil.getSparkContext(10, "Test");
-        println(getHealthStatus);
-    }
+//    def main(args: Array[String]): Unit = {
+//        implicit val sc = CommonUtil.getSparkContext(10, "Test");
+//        println(getHealthStatus);
+//    }
 }
 
 class HealthCheckAPIService extends Actor {
 	import HealthCheckAPIService._;
 
 	def receive = {
-		case GetHealthStatus(sc: SparkContext) => sender() ! getHealthStatus()(sc);
+		case GetHealthStatus => sender() ! getHealthStatus();
 	}
 }
