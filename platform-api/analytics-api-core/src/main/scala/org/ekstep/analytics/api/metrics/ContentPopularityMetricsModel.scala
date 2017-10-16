14a14a74b43cd0ff229fce95ca58b8ebadf69aae
amit behera <amitb@ilimi.in>
2017-10-16 15:53:23 +0530

    Issue #1034 fix: fixing in precomputed views

--- platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/metrics/ContentPopularityMetricsModel.scala
+++ platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/metrics/ContentPopularityMetricsModel.scala
@@ -6,13 +6,13 @@ import org.apache.spark.SparkContext
 import com.typesafe.config.Config
 import org.apache.spark.rdd.RDD
 import org.ekstep.analytics.api.util.CommonUtil
-import org.ekstep.analytics.api.ContentPopularityViews
+//import org.ekstep.analytics.api.ContentPopularityViews
 import org.ekstep.analytics.api.Rating
 
-object ContentPopularityMetricsModel extends IMetricsModel[ContentPopularityViews, ContentPopularityMetrics]  with Serializable {
+object ContentPopularityMetricsModel extends IMetricsModel[ContentPopularityMetrics, ContentPopularityMetrics]  with Serializable {
 	override def metric : String = "cps";
 	
-	override def getMetrics(records: RDD[ContentPopularityViews], period: String, fields: Array[String] = Array())(implicit sc: SparkContext, config: Config): RDD[ContentPopularityMetrics] = {
+	override def getMetrics(records: RDD[ContentPopularityMetrics], period: String, fields: Array[String] = Array())(implicit sc: SparkContext, config: Config): RDD[ContentPopularityMetrics] = {
 	    val periodEnum = periodMap.get(period).get._1;
 		val periods = _getPeriods(period);
 		val addComments = returnComments(fields);
@@ -28,9 +28,9 @@ object ContentPopularityMetricsModel extends IMetricsModel[ContentPopularityView
 		};
 	}
 
-	private def _merge(obj: ContentPopularityViews, dummy: ContentPopularityMetrics, addComments: Boolean): ContentPopularityMetrics = {
+	private def _merge(obj: ContentPopularityMetrics, dummy: ContentPopularityMetrics, addComments: Boolean): ContentPopularityMetrics = {
         
-	    val m_ratings = obj.m_ratings.getOrElse(List()).map{x=> Rating(x._1, x._2)}
+	    val m_ratings = obj.m_ratings.getOrElse(List()).map{x=> Rating(x.rating, x.timestamp)}
 	    if (addComments)
         	ContentPopularityMetrics(dummy.d_period, dummy.label, obj.m_comments, obj.m_downloads, obj.m_side_loads, Option(m_ratings), obj.m_avg_rating)
         else
