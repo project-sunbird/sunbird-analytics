2c2498bae36b55d69a2bbe4eda56933d30c0359e
Jitendra Singh Sankhwar <jitendras@ilimi.in>
2017-11-02 13:29:29 +0530

    Issue #1042 refactor: DataConvertor changes

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/v3converter/DataConvertor.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/v3converter/DataConvertor.scala
@@ -4,6 +4,7 @@ import org.ekstep.analytics.framework._
 import org.ekstep.analytics.framework.util.JSONUtils
 import org.apache.spark.rdd.RDD
 import org.ekstep.analytics.framework.util.CommonUtil
+import org.ekstep.analytics.creation.model.CreationEvent
 
 case class DerivedEventTest(eid: String, ets: Long, syncts: Long, ver: String, mid: String, uid: String, channel: Option[String], content_id: Option[String] = None, cdata: Option[CData], context: Context, dimensions: Dimensions, edata: MEEdata, tags: Option[AnyRef] = None) extends Input with AlgoInput;
 case class UserProfile(uid: String, gender: String, age: Int);
@@ -26,20 +27,34 @@ case class EventV3(val eid: String, val ets: Long, val `@timestamp`: String, val
 
 class DataConvertor extends SparkSpec(null) {
 
-    val fileNameList = List("test-data1.log", "test-data2.log")
-    val inputPath = "src/test/resources/genie-usage-summary/stage/"
+    //                val `type` = if ("GE_SESSION_START".equals(x.eid) || "GE_SESSION_END".equals(x.eid)) "session"
+//                else if ("GE_GAME_START".equals(x.eid) || "GE_GAME_END".equals(x.eid) || "GE_LAUNCH_GAME".equals(x.eid)) "contentplay" else "app"
+//    val fileNameList = List("test_data_groupInfo.log","test_data_partnerId.log","test_data.log","test_data1.log", "test_data2.log", "test_data3.log", "test_data4.log", "test_data5.log",
+//        "test_data6.log", "test_data7.log", "test_data8.log", "test_data9.log","test_data1.log")
+    val fileNameList = List("test_data_1.log")
+    val inputPath = "src/test/resources/portal-session-summary/"
     val outputPath = "/tmp/data/v3/"
 
-    val genieEventMapping = Map("GE_START" -> "START", "GE_GENIE_START" -> "START", "GE_END" -> "END", "GE_GENIE_END" -> "END", "GE_INTERACT" -> "IMPRESSION", "GE_CREATE_PROFILE" -> "AUDIT", "GE_CREATE_USER" -> "AUDIT", "GE_SESSION_START" -> "START", "GE_SESSION_END" -> "END", "GE_LAUNCH_GAME" -> "START", "GE_GAME_END" -> "END", "GE_ERROR" -> "ERROR", "GE_API_CALL" -> "LOG", "GE_INTERRUPT" -> "INTERRUPT", "GE_GENIE_RESUME" -> "INTERRUPT", "GE_GENIE_UPDATE" -> "LOG", "GE_TRANSFER" -> "SHARE", "GE_FEEDBACK" -> "FEEDBACK", "OE_START" -> "START")
+    val genieEventMapping = Map("GE_START" -> "START", "GE_GENIE_START" -> "START", "GE_END" -> "END", "GE_GENIE_END" -> "END", "GE_INTERACT" -> "IMPRESSION", "GE_CREATE_PROFILE" -> "AUDIT", "GE_CREATE_USER" -> "AUDIT", "GE_SESSION_START" -> "START", "GE_SESSION_END" -> "END", "GE_LAUNCH_GAME" -> "START", "GE_GAME_END" -> "END", "GE_ERROR" -> "ERROR", "GE_API_CALL" -> "LOG", "GE_INTERRUPT" -> "INTERRUPT", "GE_GENIE_RESUME" -> "INTERRUPT", "GE_GENIE_UPDATE" -> "LOG", "GE_TRANSFER" -> "SHARE", "GE_FEEDBACK" -> "FEEDBACK", "OE_START" -> "START",
+        "OE_END" -> "END",
+        "OE_ASSESS" -> "ASSESS",
+        "OE_INTERACT" -> "INTERACT",
+        "OE_INTERRUPT" -> "INTERRUPT",
+        "OE_NAVIGATE" -> "IMPRESSION",
+        "OE_ITEM_RESPONSE" -> "RESPONSE",
+        "OE_LEVEL_SET" -> "RESPONSE")
     val portalEventMapping = Map("CP_SESSION_START" -> "START", "CP_IMPRESSION" -> "IMPRESSION", "CP_INTERACT" -> "INTERACT", "BE_OBJECT_LIFECYCLE" -> "AUDIT", "CP_ERROR" -> "ERROR", "CP_API_CALL" -> "LOG", "CE_START" -> "START", "CE_END" -> "END", "CE_API_CALL" -> "LOG", "CE_ERROR" -> "ERROR", "CE_PLUGIN_LIFECYCLE" -> "AUDIT", "CE_INTERACT" -> "INTERACT")
-    val filterList = List("GE_GENIE_START", "GE_START", "GE_GENIE_END", "GE_END", "GE_INTERACT", "GE_INTERACT", "GE_CREATE_PROFILE", "GE_CREATE_USER", "GE_SESSION_START", "GE_SESSION_END", "GE_LAUNCH_GAME", "GE_GAME_END", "GE_ERROR", "GE_API_CALL", "GE_INTERRUPT", "GE_GENIE_RESUME", "GE_GENIE_UPDATE", "GE_TRANSFER", "GE_FEEDBACK", "OE_START")
+    val filterList = List("GE_GENIE_START", "GE_START", "GE_GENIE_END", "GE_END", "GE_INTERACT", "GE_INTERACT", "GE_CREATE_PROFILE", "GE_CREATE_USER", 
+            "GE_SESSION_START", "GE_SESSION_END", "GE_LAUNCH_GAME", "GE_GAME_END", "GE_ERROR", "GE_API_CALL", "GE_INTERRUPT", "GE_GENIE_RESUME", "GE_GENIE_UPDATE",
+            "GE_TRANSFER", "GE_FEEDBACK", "OE_START", "OE_END", "OE_ASSESS", "OE_INTERACT", "OE_INTERRUPT", "OE_NAVIGATE", "OE_ITEM_RESPONSE", "OE_LEVEL_SET",
+            "CP_SESSION_START", "CP_IMPRESSION", "CP_INTERACT", "BE_OBJECT_LIFECYCLE", "CP_ERROR", "CP_API_CALL", "CE_START", "CE_END", "CE_INTERACT", "CE_ERROR", "CE_API_CALL")
 
     def getEid(key: String, env: String): String = env.toLowerCase() match {
         case "genie"  => genieEventMapping.get(key).get
         case "portal" => portalEventMapping.get(key).get
     }
 
-    def filterEvent(events: RDD[Event], list: List[String]): RDD[Event] = {
+    def filterEvent(events: RDD[CreationEvent], list: List[String]): RDD[CreationEvent] = {
         DataFilter.filter(events, Filter("eid", "IN", Option(list)))
     }
 
@@ -50,7 +65,7 @@ class DataConvertor extends SparkSpec(null) {
     it should " filter v2 events successfully" in {
         fileNameList.map { fileName =>
             println("fileName : " + fileName)
-            val rdd = loadFile[Event](inputPath + fileName);
+            val rdd = loadFile[CreationEvent](inputPath + fileName);
             println("data loaded successfully.")
             println("count : " + rdd.count)
 
@@ -63,22 +78,22 @@ class DataConvertor extends SparkSpec(null) {
 
     it should " convert data to telemetry v3 successfully" in {
         fileNameList.map { fileName =>
-            val rdd = loadFile[Event]("/tmp/data/" + fileName);
+            val rdd = loadFile[CreationEvent]("/tmp/data/" + fileName);
             val data = rdd.collect().map { x =>
 
                 val env = if (x.eid.startsWith("CP")) "portal" else if (x.eid.startsWith("OE")) "player" else if (x.eid.startsWith("GE")) "genie" else if (x.eid.startsWith("CE")) "attool" else ""
-                val pData = V3PData(env, Option(x.ver))
+                val pData = V3PData(env, Option(x.ver), x.pdata.get.pid)
                 val rollUp = RollUp("l1", "l2", "l3", "l4")
-                val context = V3Context(x.channel.getOrElse("in.ekstep"), Option(pData), env, Option(x.sid), Option(x.did), None, Option(rollUp))
-                val `object` = V3Object(x.gdata.id, "user", Option(x.gdata.ver), Option(rollUp))
+                val sid = if(x.context.isDefined) x.context.get.sid else ""
+                val context = V3Context(x.channel.getOrElse("in.ekstep"), Option(pData), env, Option(sid), None, None, Option(rollUp))
+                val `object` = V3Object("", "user", None, Option(rollUp))
 
                 val eks = JSONUtils.deserialize[V3EData](JSONUtils.serialize(x.edata.eks))
-                val `type` = if ("GE_SESSION_START".equals(x.eid) || "GE_SESSION_END".equals(x.eid)) "session"
-                else if ("GE_GAME_START".equals(x.eid) || "GE_GAME_END".equals(x.eid) || "GE_LAUNCH_GAME".equals(x.eid)) "contentplay" else "app"
+                val `type` = x.edata.eks.`type`
                 val eData = EDataV3(eks.dspec, eks.loc, eks.pass, eks.qid, eks.score, eks.res, eks.length, eks.atmpts, eks.failedatmpts, eks.category, eks.current, eks.max, `type`, eks.extype, eks.id, eks.gid, eks.itype, eks.stageid, eks.stageto, eks.resvalues, eks.params, eks.uri, eks.state, eks.subtype, eks.pos, eks.values, eks.tid, eks.direction, eks.datatype, eks.count, eks.contents, eks.comments, eks.rating, eks.qtitle, eks.qdesc, eks.mmc, eks.context, eks.method, eks.request, eks.defaultPlugins, eks.loadtimes, eks.client, eks.path, eks.response, eks.responseTime, eks.status, eks.uip, eks.pluginid, eks.pluginver, eks.objectid, eks.stage, eks.containerid, eks.containerplugin, eks.target, eks.action, eks.err, eks.data, eks.severity, eks.duration, eks.uaspec, eks.env, eks.stageid, eks.name, eks.url, eks.targetid, eks.parentid, eks.parenttype, eks.code, eks.prevstate, eks.email, eks.access, eks.partners, eks.profile, eks.item, eks.visits)
                 val eid = getEid(x.eid, "portal")
-                val ets = if (null != x.ets && x.ets != 0) x.ets else CommonUtil.getTimestamp(x.ts)
-                EventV3(eid, ets, x.`@timestamp`, x.ver, "", Actor(x.uid, "User"), context, Option(`object`), eData, x.tags.asInstanceOf[List[Map[String, String]]])
+                val ets = if (null != x.ets && x.ets != 0) x.ets //else CommonUtil.getTimestamp(x.ts)
+                EventV3(eid, ets.asInstanceOf[Long], x.`@timestamp`, x.ver, "", Actor(x.uid, "User"), context, Option(`object`), eData, x.tags.asInstanceOf[List[Map[String, String]]])
             }
             val events = data.map(f => JSONUtils.serialize(f))
             saveToFile(events, outputPath + fileName)
