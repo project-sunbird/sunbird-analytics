e037d4516509e0e0271c5ee3dacb539decd215ca
sowmya-dixit <sowmyadixit7@gmail.com>
2017-11-30 17:58:24 +0530

    Issue #1057 refactor: App Session Summary update for structure change.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/AppSessionSummaryModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/AppSessionSummaryModel.scala
@@ -55,7 +55,7 @@ object AppSessionSummaryModel extends IBatchModelTemplate[V3Event, PortalSession
 
     override def preProcess(data: RDD[V3Event], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[PortalSessionInput] = {
         JobLogger.log("Filtering Events of BE_OBJECT_LIFECYCLE, CP_SESSION_START, CE_START, CE_END, CP_INTERACT, CP_IMPRESSION")
-        val filteredData = DataFilter.filter(data, Array(Filter("context.pdata", "ISNOTEMPTY", None), Filter("eventId", "IN", Option(List("AUDIT", "START", "INTERACT", "IMPRESSION", "END"))))).filter { x => ((x.context.pdata.get.id.contains(Constants.PORTAL_ENV)) || ((x.context.env.equals(Constants.EDITOR_ENV)) && ("IMPRESSION".equals(x.eid) || "END".equals(x.eid)))) };
+        val filteredData = DataFilter.filter(data, Array(Filter("eventId", "IN", Option(List("AUDIT", "START", "INTERACT", "IMPRESSION", "END"))), Filter("context.pdata", "ISNOTEMPTY", None))).filter { x => ((x.context.pdata.get.id.contains(Constants.PORTAL_ENV)) || ((x.context.env.equals(Constants.EDITOR_ENV)) && ("IMPRESSION".equals(x.eid) || "END".equals(x.eid)))) };
         filteredData.map { event =>
             val channel = CommonUtil.getChannelId(event)
             ((channel, event.context.sid.get), Buffer(event))
