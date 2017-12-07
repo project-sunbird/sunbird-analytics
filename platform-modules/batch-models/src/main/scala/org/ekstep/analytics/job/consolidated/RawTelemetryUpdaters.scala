a6d3373f4d734a595608774fa04b1736656904fb
amit behera <amitb@ilimi.in>
2017-12-07 11:53:53 +0530

    Issue #1059 feat: split LearnerProfileUpdater into two jobs - implemented jobs for two DataProduct and Updated Job Factory

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/consolidated/RawTelemetryUpdaters.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/consolidated/RawTelemetryUpdaters.scala
@@ -3,7 +3,7 @@ package org.ekstep.analytics.job.consolidated
 import org.apache.spark.SparkContext
 import org.ekstep.analytics.model._
 import org.ekstep.analytics.framework.JobDriver
-import org.ekstep.analytics.updater.UpdateLearnerProfileDB
+import org.ekstep.analytics.updater._
 import org.ekstep.analytics.updater.UpdateDeviceSpecificationDB
 import org.ekstep.analytics.framework.IJob
 
@@ -12,7 +12,7 @@ object RawTelemetryUpdaters extends optional.Application with IJob {
     val className = "org.ekstep.analytics.job.RawTelemetryUpdaters"
 
     def main(config: String)(implicit sc: Option[SparkContext] = None) {
-        val models = List(UpdateLearnerProfileDB, UpdateDeviceSpecificationDB);
+        val models = List(UpdateLearnerProfileCreateDB, UpdateLearnerProfileUpdateDB, UpdateDeviceSpecificationDB);
         implicit val sparkContext: SparkContext = sc.getOrElse(null);
         //JobDriver.run("batch", config, models, className);
     }
--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/updater/TestLearnerProfileUpdater.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/updater/TestLearnerProfileUpdater.scala
@@ -7,10 +7,10 @@ import org.ekstep.analytics.framework.Query
 import org.ekstep.analytics.framework.util.JSONUtils
 import org.ekstep.analytics.framework.Dispatcher
 
-class TestLearnerProfileUpdaterJob extends SparkSpec(null) {
+class TestLearnerProfileUpdater extends SparkSpec(null) {
 
     "LearnerProfileUpdater" should "execute the job and shouldn't throw any exception" in {
-        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/learner-profile/2016-04-04-1459753547783.json"))))), None, None, "org.ekstep.analytics.updater.LearnerProfileUpdater", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestLearnerProfileUpdaterJob"), Option(false))
+        val config = JobConfig(Fetcher("local", None, Option(Array(Query(None, None, None, None, None, None, None, None, None, Option("src/test/resources/learner-profile/test-data2.log"))))), None, None, "org.ekstep.analytics.job.updater.LearnerProfileUpdater", None, Option(Array(Dispatcher("console", Map("printEvent" -> false.asInstanceOf[AnyRef])))), Option(10), Option("TestLearnerProfileUpdaterJob"), Option(false))
         LearnerProfileUpdater.main(JSONUtils.serialize(config))(Option(sc));
     }
 }
