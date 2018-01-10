3532e467aae54bf6eaf2e737d299ead34432b4c3
Sowmya <sowmyadixit7@gmail.com>
2018-01-10 15:42:48 +0530

    Issue #1072 feat: Workflow Summary Model implementation.

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestWorkFlowSummaryModel.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestWorkFlowSummaryModel.scala
@@ -3,13 +3,18 @@ package org.ekstep.analytics.model
 import org.ekstep.analytics.framework.V3Event
 import scala.collection.mutable.Buffer
 import org.ekstep.analytics.framework.V3PData
+import org.ekstep.analytics.framework.util.JSONUtils
 
 class TestWorkFlowSummaryModel extends SparkSpec {
     it should "test data" in {
-        val data = loadFile[V3Event]("src/test/resources/workflow-summary/test-data1.log").collect().toBuffer
-        val mappedOut = WorkFlowSummaryModel.arrangeWorkflowData(data)
+        val data = loadFile[V3Event]("src/test/resources/workflow-summary/test-data1.log")
+        val out = WorkFlowSummaryModel.execute(data, None)
+        println("output count: ", out.count)
+        out.foreach(f => println(JSONUtils.serialize(f)))
         
-        println("output map size: " + mappedOut.size)
+        
+//        println("output map size: " + mappedOut.size)
+//        mappedOut.foreach(f => println(" key: ", f._1, " bufferCount: ", f._2.size, " firstEvent: ", f._2.head.eid, " LastEvent: ", f._2.last.eid))
         
         
         
