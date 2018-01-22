20f7baf54d60dc6c220349ce9faf52d349e24cfa
Santhosh <santhosh@ilimi.in>
2018-01-22 21:56:19 +0530

    Issue #1084 fix: gzip issue

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/summarizer/FixSecorValidBackupJob.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/summarizer/FixSecorValidBackupJob.scala
@@ -15,31 +15,36 @@ import org.joda.time.DateTime
 import org.ekstep.analytics.framework.OutputDispatcher
 import org.ekstep.analytics.framework.Dispatcher
 
+case class ValidEvent(`@timestamp`: AnyRef);
 object FixSecorValidBackupJob extends optional.Application with IJob {
-      
+
     implicit val className = "org.ekstep.analytics.job.FixSecorValidBackupJob"
-    
+
     def main(config: String)(implicit sc: Option[SparkContext] = None) {
         implicit val sparkContext: SparkContext = sc.getOrElse(null);
-        
+
         val jobConfig = JSONUtils.deserialize[JobConfig](config);
-        val data = DataFetcher.fetchBatchData[Map[String, AnyRef]](jobConfig.search).cache();
-        val groupedData = data.map { event =>
-            val syncts = event.get("@timestamp").get.asInstanceOf[String]
-            (syncts, Buffer(JSONUtils.serialize(event)))
-        }.partitionBy(new HashPartitioner(JobContext.parallelization)).reduceByKey((a, b) => a ++ b)
-        
+        val data = DataFetcher.fetchBatchData[String](jobConfig.search).cache();
+
         val filterDate = CommonUtil.dateFormat.parseMillis("2017-12-20")
-        val filteredData = groupedData.filter(f => CommonUtil.df5.parseMillis(f._1) >= filterDate)
-        
+        val groupedData = data.map { event =>
+            val syncts = JSONUtils.deserialize[ValidEvent](event).`@timestamp`;
+            if (syncts.isInstanceOf[Double]) {
+                (syncts.asInstanceOf[Double].toLong, event)
+            } else if (syncts.isInstanceOf[Long]) {
+                (syncts.asInstanceOf[Long], event)
+            } else {
+                (CommonUtil.getTimestamp(syncts.asInstanceOf[String]), event)
+            }
+        }.filter(f => f._1 >= filterDate).groupBy(f => CommonUtil.dateFormat.print(f._1));
+
         val filePrefix = "valid-corrected/"
-         val bucket = "ekstep-dev-data-store"
-         val out = filteredData.map { x =>
-             val longValue = CommonUtil.df5.parseMillis(x._1)
-             val dateFormat = new DateTime(longValue).toLocalDate()
-             val fileKey = filePrefix + dateFormat + "-" + longValue + ".json";
-             println(fileKey)
-             OutputDispatcher.dispatch(Dispatcher("s3", Map("bucket" -> bucket, "key" -> fileKey, "file" -> fileKey, "zip" -> true.asInstanceOf[AnyRef])), x._2.toArray)
+        val bucket = "ekstep-dev-data-store"
+        val out = groupedData.map { x =>
+            val fileKey = filePrefix + x._1 + "-" + System.currentTimeMillis() + ".json.gz";
+            val events = x._2.map(f => f._2);
+            println("fileKey", fileKey)
+            OutputDispatcher.dispatch(Dispatcher("s3file", Map("bucket" -> bucket, "key" -> fileKey, "file" -> fileKey, "zip" -> true.asInstanceOf[AnyRef])), events.toArray)
         }
         println("Input events count: ", data.count(), " Output events count: ", out.flatMap(f => f).count())
     }
