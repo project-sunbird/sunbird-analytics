e6f36b4b09bb275ec83093ab5794b91fbe13608b
Sowmya <sowmyadixit7@gmail.com>
2018-01-29 17:05:34 +0530

    Issue #1089 feat: Workflow Usage Summary Model implementation.

---
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/model/TestWorkFlowUsageSummaryModel.scala
@@ -0,0 +1,19 @@
+package org.ekstep.analytics.model
+
+import org.ekstep.analytics.framework.util.CommonUtil
+import org.ekstep.analytics.framework.util.JSONUtils
+import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext
+import org.ekstep.analytics.framework.DerivedEvent
+
+class TestWorkFlowUsageSummaryModel extends SparkSpec(null) {
+  
+    "WorkFlowUsageSummaryModel" should "generate workflow usage summary events" in {
+
+        val rdd1 = loadFile[DerivedEvent]("src/test/resources/workflow-usage-summary/test-data1.log");
+        val rdd2 = WorkFlowUsageSummaryModel.execute(rdd1, None);
+        val me = rdd2.collect();
+        
+        //me.foreach(f => println(JSONUtils.serialize(f)))
+    }
+}
