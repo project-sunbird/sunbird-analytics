6abbef26a84b142075bd6f4c91369c8927f15e20
Sowmya <sowmyadixit7@gmail.com>
2018-01-30 17:40:28 +0530

    Issue #1089 feat: Add Workflow Usage Summarizer and test cases.

---
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/summarizer/WorkFlowUsageSummarizer.scala
@@ -0,0 +1,19 @@
+package org.ekstep.analytics.job.summarizer
+
+import org.ekstep.analytics.framework.JobDriver
+import org.apache.spark.SparkContext
+import org.ekstep.analytics.framework.util.JobLogger
+import org.ekstep.analytics.framework.IJob
+import org.ekstep.analytics.model.WorkFlowUsageSummaryModel
+
+object WorkFlowUsageSummarizer extends optional.Application with IJob {
+  
+    implicit val className = "org.ekstep.analytics.job.WorkFlowUsageSummarizer"
+    
+    def main(config: String)(implicit sc: Option[SparkContext] = None) {
+        implicit val sparkContext: SparkContext = sc.getOrElse(null);
+        JobLogger.log("Started executing Job")
+        JobDriver.run("batch", config, WorkFlowUsageSummaryModel);
+        JobLogger.log("Job Completed.")
+    }
+}
