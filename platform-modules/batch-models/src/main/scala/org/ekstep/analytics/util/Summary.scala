ecf02260635167086abe486bb0774afcef37c21f
amit behera <amitb@ilimi.in>
2018-02-20 14:15:24 +0530

    Issue #1072 feat: refactoring workflow summary

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/util/Summary.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/util/Summary.scala
@@ -12,12 +12,12 @@ class Summary(val summaryKey: String, val event: V3Event) {
     val did: Option[String] = None
     val sid: String = ""
     val uid: String = ""
-    val pdata: PData = PData("", "") 
+    val pdata: PData = PData("", "")
     val channel: String = "in.ekstep"
     val content_id: Option[String] = None
     val session_type: String = ""
     val syncts: Long = 0l
-    val dt_range: DtRange = DtRange(0l, 0l) 
+    val dt_range: DtRange = DtRange(0l, 0l)
     val mode: Option[String] = None
     val item_responses: Option[Buffer[ItemResponse]] = None
     val start_time: Long = 0l
@@ -29,27 +29,48 @@ class Summary(val summaryKey: String, val event: V3Event) {
     val telemetry_version: String = ""
     val env_summary: Option[Iterable[EnvSummary]] = None
     val events_summary: Option[Iterable[EventSummary]] = None
-    
+
     val CHILD: Buffer[Summary] = null
-    val PARENT: Summary = null
-    
+    var PARENT: Summary = null
+
     val events: Buffer[V3Event] = Buffer(event)
     var isClosed: Boolean = false
-    
-    
+
     def add(event: V3Event) {
         events.append(event)
     }
-    
-    def check(eventKey: String) {
-         StringUtils.equals(this.summaryKey, eventKey)   
+
+    def addChild(child: Summary) {
+        CHILD.append(child);
     }
-    
-    def computeMetrics(){
-        
+
+    def setParent(parent: Summary) {
+        this.PARENT = parent;
     }
-    
-    def close(){
+
+    def getParent(): Summary = {
+        return this.PARENT;
+    }
+
+    def checkSimilarity(eventKey: String): Boolean = {
+        StringUtils.equals(this.summaryKey, eventKey)
+    }
+
+    def computeMetrics(currentSumm: Summary) {
+
+    }
+
+    def rollUpSummary(currentSumm: Summary) {
+
+    }
+
+    def close() {
+        for (child <- CHILD) {
+            if (child.isClosed == false) {
+                child.isClosed = true
+            }
+        }
+        rollUpSummary(this)
         this.isClosed = true
     }
 }
