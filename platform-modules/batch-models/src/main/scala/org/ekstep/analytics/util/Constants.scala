bc2674da58aebb2886588f6bd5c9f858c6c12b00
Sowmya <sowmyadixit7@gmail.com>
2018-03-27 13:56:04 +0530

    Issue #1111 refactor: Update portal pdata ids for App Session Summary.

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/util/Constants.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/util/Constants.scala
@@ -67,7 +67,8 @@ object Constants {
     val PLAYER_ENV = "ContentPlayer"
     val PORTAL_ENV = "portal"
     val EDITOR_ENV = "contenteditor"
-    val PORTAL_PDATAIDS = List("in.ekstep.community", "org.ekstep.content.platform", "sunbird_portal", "in.ekstep.dev", "in.ekstep.qa")
+    val PORTAL_PDATAIDS = List("dev.ekstep.portal", "qa.ekstep.portal", "prod.ekstep.portal", "dev.sunbird.portal", "qa.sunbird.portal", "prod.sunbird.portal", "dev.diksha.portal", "qa..diksha.portal", "prod.diksha.portal")
+//    val PORTAL_PDATAIDS = List("in.ekstep.community", "org.ekstep.content.platform", "sunbird_portal", "in.ekstep.dev", "in.ekstep.qa")
     
     def getContentList(): String = {
         s"$LP_URL/v2/analytics/content/list";
