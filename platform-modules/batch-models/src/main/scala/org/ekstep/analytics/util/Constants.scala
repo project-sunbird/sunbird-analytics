476f7c0a941a0c22d9c2d0d09ae14029ee1e45e7
sunil <sunils@ilimi.in>
2018-12-05 20:12:12 +0530

    Issue #SB-7293 refactor: config changes

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/util/Constants.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/util/Constants.scala
@@ -71,7 +71,7 @@ object Constants {
     val EDITOR_ENV = "contenteditor"
     val PORTAL_PDATAIDS = List("dev.ekstep.portal", "qa.ekstep.portal", "prod.ekstep.portal", "dev.sunbird.portal", "qa.sunbird.portal", "prod.sunbird.portal", "dev.diksha.portal", "qa.diksha.portal", "prod.diksha.portal")
 //    val PORTAL_PDATAIDS = List("in.ekstep.community", "org.ekstep.content.platform", "sunbird_portal", "in.ekstep.dev", "in.ekstep.qa")
-    val HIERARCHY_STORE_KEY_SPACE_NAME = AppConf.getConfig("cassandra.content_store_prefix")+"hierarchy_store"
+    val HIERARCHY_STORE_KEY_SPACE_NAME = AppConf.getConfig("cassandra.hierarchy_store_prefix")+"hierarchy_store"
     val CONTENT_HIERARCHY_TABLE = "content_hierarchy"
 
     def getContentList(): String = {
