a4ab707dda81dc9666fd4fabe783e065e277d760
sunil <sunils@ilimi.in>
2018-12-11 11:41:13 +0530

    Issue #SB-7293 fix: ETBCoverageSummaryModel print graph event for debugging

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ETBCoverageSummaryModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/ETBCoverageSummaryModel.scala
@@ -132,7 +132,9 @@ object ETBCoverageSummaryModel extends IBatchModelTemplate[Empty, ContentHierarc
             GraphUpdateEvent(DateTime.now().getMillis, metric.contentId,
                 Map("properties" -> finalContentMap), metric.objectType)
         }
-        JobLogger.log("ETB coverage Graph Event :" + output.foreach(x => println(JSONUtils.serialize(x))))
+        output.foreach { event =>
+            JobLogger.log("ETB coverage Graph Event :" + JSONUtils.serialize(event), logLevel= Level.INFO)
+        }
         println("ETB coverage Graph Event :" + output.foreach(x => println(JSONUtils.serialize(x))))
         output
     }
