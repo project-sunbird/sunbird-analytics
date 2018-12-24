5597425e84cf0356284fb49c0971c13cbadf383e
Anand <anandp@ilimi.in>
2018-12-24 17:20:54 +0530

    Issue SC-283 fix: Fix case class names and variable names

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdatePortalMetrics.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/updater/UpdatePortalMetrics.scala
@@ -20,35 +20,36 @@ import org.ekstep.analytics.util.Constants
 import scala.util.Try
 
 
-case class WorkFlowUsageMetrics(noOfUniqueDevices: Long, totalContentPlaySessions: Long, totalTimeSpent: Double, totalContentPublished: contentPublishedList) extends AlgoOutput with Output with AlgoInput
+case class WorkFlowUsageMetrics(noOfUniqueDevices: Long, totalContentPlaySessions: Long, totalTimeSpent: Double, totalContentPublished: ContentPublishedList) extends AlgoOutput with Output with AlgoInput
 
 case class PortalMetrics(eid: String, ets: Long, syncts: Long, metrics_summary: Option[WorkFlowUsageMetrics]) extends AlgoOutput with Output
 
-case class contentPublishedList(count: Int, languages: List[publisherByLanguage])
+case class ContentPublishedList(count: Int, languages: List[PublisherByLanguage])
 
-case class publisherByLanguage(language: String, publishers: List[Publisher])
+case class PublisherByLanguage(language: String, publishers: List[Publisher])
 
 case class Publisher(id: String, count: Double)
 
 case class ESResponse(aggregations: Aggregations)
 
-case class Aggregations(language_agg: Language_agg)
+case class Aggregations(language_agg: AggregationResult)
 
-case class Language_agg(buckets: List[Buckets])
+case class AggregationResult(buckets: List[Buckets])
 
-case class Buckets(key: String, doc_count: Double, publisher_agg: Language_agg)
+case class Buckets(key: String, doc_count: Double, publisher_agg: AggregationResult)
 
 case class OrgResponse(id: String, ver: String, ts: String, params: Params, responseCode: String, result: OrgResult)
 
 case class OrgResult(response: ContentList)
 
-case class ContentList(count: Long, content: List[orgProps])
+case class ContentList(count: Long, content: List[OrgProps])
 
-case class orgProps(orgName: String, hashTagId: String, rootOrgId: String)
+case class OrgProps(orgName: String, hashTagId: String, rootOrgId: String)
 
 case class DeviceProfile(device_id: String)
 
-object UpdatePortalMetrics extends IBatchModelTemplate[DerivedEvent, DerivedEvent, WorkFlowUsageMetrics, PortalMetrics] with Serializable {
+object UpdatePortalMetrics extends IBatchModelTemplate[DerivedEvent, DerivedEvent, WorkFlowUsageMetrics, PortalMetrics]
+  with Serializable {
 
   val className = "org.ekstep.analytics.updater.UpdatePortalMetrics"
 
@@ -75,17 +76,24 @@ object UpdatePortalMetrics extends IBatchModelTemplate[DerivedEvent, DerivedEven
     * @param sc     - Spark context
     * @return - DashBoardSummary ->(uniqueDevices, totalContentPlayTime, totalTimeSpent,)
     */
-  override def algorithm(data: RDD[DerivedEvent], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[WorkFlowUsageMetrics] = {
-    val languageList = getLanguageAndPublisherList()
-    println("event", JSONUtils.serialize(languageList))
+  override def algorithm(data: RDD[DerivedEvent], config: Map[String, AnyRef])
+                        (implicit sc: SparkContext): RDD[WorkFlowUsageMetrics] = {
+    val publisherByLanguageList = getLanguageAndPublisherList()
+    println("event", JSONUtils.serialize(publisherByLanguageList))
     val totalContentPublished = Try(ContentAdapter.getPublishedContentList().count).getOrElse(0)
-    val noOfUniqueDevices = sc.cassandraTable[DeviceProfile](Constants.DEVICE_KEY_SPACE_NAME, Constants.DEVICE_PROFILE_TABLE).map(_.device_id).distinct().count()
-    val metrics = sc.cassandraTable[WorkFlowUsageMetricsAlgoOutput](Constants.PLATFORM_KEY_SPACE_NAME, Constants.WORKFLOW_USAGE_SUMMARY).map(event => {
-      (event.total_timespent, event.total_content_play_sessions)
-    })
+    val noOfUniqueDevices =
+      sc.cassandraTable[DeviceProfile](Constants.DEVICE_KEY_SPACE_NAME, Constants.DEVICE_PROFILE_TABLE)
+        .map(_.device_id).distinct().count()
+    val metrics =
+      sc.cassandraTable[WorkFlowUsageMetricsAlgoOutput](Constants.PLATFORM_KEY_SPACE_NAME, Constants.WORKFLOW_USAGE_SUMMARY)
+        .map(event => {
+          (event.total_timespent, event.total_content_play_sessions)
+        })
     val totalTimeSpent = metrics.map(_._1).sum()
     val totalContentPlaySessions = metrics.map(_._2).sum().toLong
-    sc.parallelize(Array(WorkFlowUsageMetrics(noOfUniqueDevices, totalContentPlaySessions, CommonUtil.roundDouble(totalTimeSpent / 3600, 2), contentPublishedList(totalContentPublished, languageList))))
+    sc.parallelize(Array(WorkFlowUsageMetrics(noOfUniqueDevices, totalContentPlaySessions,
+      CommonUtil.roundDouble(totalTimeSpent / 3600, 2),
+      ContentPublishedList(totalContentPublished, publisherByLanguageList))))
   }
 
   /**
@@ -105,7 +113,7 @@ object UpdatePortalMetrics extends IBatchModelTemplate[DerivedEvent, DerivedEven
     sc.parallelize(Array(metrics))
   }
 
-  private def getLanguageAndPublisherList(): List[publisherByLanguage] = {
+  private def getLanguageAndPublisherList(): List[PublisherByLanguage] = {
     val apiURL = Constants.ELASTIC_SEARCH_SERVICE_ENDPOINT + "/" + Constants.ELASTIC_SEARCH_INDEX_COMPOSITESEARCH_NAME + "/_search"
     val request =
       s"""
@@ -199,7 +207,7 @@ object UpdatePortalMetrics extends IBatchModelTemplate[DerivedEvent, DerivedEven
           }
         })
       }).flatMap(f => f).filter(_.id.nonEmpty)
-      publisherByLanguage(languageBucket.key, publishers)
+      PublisherByLanguage(languageBucket.key, publishers)
     }).filter(_.publishers.nonEmpty)
   }
 
