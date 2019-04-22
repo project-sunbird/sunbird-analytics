b69ffc041fa7bec9d1163c9fac6eb33dac9b7024
Sunil pandith <sunil.slvphsn@gmail.com>
2019-04-22 18:51:09 +0530

    Issue #SB-11295 fix: Date format for generatedOn field

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/CourseMetricsJob.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/CourseMetricsJob.scala
@@ -172,7 +172,7 @@ object CourseMetricsJob extends optional.Application with IJob with ReportGenera
         when(expr("progress/leafnodescount * 100").isNull, 100.0)
           .otherwise(expr("progress/leafnodescount * 100")), 2)
         .cast("double"))
-      .withColumn("generatedOn", date_format(from_utc_timestamp(current_timestamp.cast(DataTypes.TimestampType), "Asia/Kolkata"), "yyyy-MM-dd'T'HH:mm:ssXXX'Z'"))
+      .withColumn("generatedOn", date_format(from_utc_timestamp(current_timestamp.cast(DataTypes.TimestampType), "Asia/Kolkata"), "yyyy-MM-dd'T'HH:mm:ss'Z'"))
   }
 
 
