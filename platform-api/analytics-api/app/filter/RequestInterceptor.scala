89ddee6ba55a89f5fdcc2706935fe7548e60aaf1
Manjunath Davanam <manjunathd@ilimi.in>
2019-04-25 18:35:00 +0530

    Issue #SB-11825 fix: api_access issue fix

--- platform-api/analytics-api/app/filter/RequestInterceptor.scala
+++ platform-api/analytics-api/app/filter/RequestInterceptor.scala
@@ -25,7 +25,7 @@ object RequestInterceptor extends Filter {
             val requestTime = endTime - startTime
             val key = play.api.routing.Router.Tags.RouteActionMethod
             val apiName = if (request.tags.isDefinedAt(key)) request.tags(play.api.routing.Router.Tags.RouteActionMethod) else ""
-            APILogger.log("ekstep.analytics-api", Option(Map("rid" -> apiName, "uip" -> request.remoteAddress, "type" -> "api-access", "title" -> apiName, "category" -> "", "size" -> "", "duration" -> requestTime, "status" -> result.header.status, "protocol" -> "", "method" -> request.method, "action" -> "", "value" -> 0, "params" -> List(request.queryString.map { case (k, v) => k -> v.mkString }))),apiName)
+            APILogger.log("ekstep.analytics-api", Option(Map("rid" -> apiName, "uip" -> request.remoteAddress, "type" -> "api_access", "title" -> apiName, "category" -> "", "size" -> "", "duration" -> requestTime, "status" -> result.header.status, "protocol" -> "", "method" -> request.method, "action" -> "", "value" -> 0, "params" -> List(request.queryString.map { case (k, v) => k -> v.mkString }))),apiName)
             result.withHeaders("Request-Time" -> requestTime.toString)
         }
     }
