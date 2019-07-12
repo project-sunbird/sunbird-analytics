99550f3f6ca6a3ce8624f0ca600e6cc3eabe9501
Manjunath Davanam <manjunathd@ilimi.in>
2019-07-12 19:15:06 +0530

    Issue #SB-13080 feat: Adhoc script to delete,create and copy the index data

---
+++ platform-modules/adhoc-scripts/src/main/es-script/delete_index.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+#This script is to delete the index
+#Before running this script please make sure existing index is already copied or not.  
+
+#Configurations
+index_name="" # Please mention the index name here : ex:cbatchstats
+host="localhost" #Change this to ES IP
+
+echo "Delete of $index_name index is started.."
+
+curl -X DELETE "$host:9200/$index_name"
+
+echo "Delete of $index_name index is success"
+
