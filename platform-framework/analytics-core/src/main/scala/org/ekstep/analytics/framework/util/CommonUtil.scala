b56419b33a5f38d787bf4026f04d62683f761f63
Manjunath Davanam <manjunathd@ilimi.in>
2019-08-09 16:24:51 +0530

    Issue #SB-13221 fix: Data product OOM Issue fix

--- platform-framework/analytics-core/src/main/scala/org/ekstep/analytics/framework/util/CommonUtil.scala
+++ platform-framework/analytics-core/src/main/scala/org/ekstep/analytics/framework/util/CommonUtil.scala
@@ -45,7 +45,11 @@ object CommonUtil {
 
     def getSparkContext(parallelization: Int, appName: String, sparkCassandraConnectionHost: Option[AnyRef] = None, sparkElasticsearchConnectionHost: Option[AnyRef] = None): SparkContext = {
         JobLogger.log("Initializing Spark Context")
-        val conf = new SparkConf().setAppName(appName);
+        val conf = new SparkConf().setAppName(appName).set("spark.default.parallelism", parallelization.toString)
+          .set("spark.driver.memory", AppConf.getConfig("spark.driver.memory"))
+          .set("spark.memory.fraction", AppConf.getConfig("spark.memory.fraction"))
+          .set("spark.memory.storageFraction", AppConf.getConfig("spark.memory.storageFraction"))
+
         val master = conf.getOption("spark.master");
         // $COVERAGE-OFF$ Disabling scoverage as the below code cannot be covered as they depend on environment variables
         if (master.isEmpty) {
