b6a0719401c7cde54aeb2329bf1e0ab00a4cb274
Manjunath Davanam <manjunathd@ilimi.in>
2019-10-10 17:18:08 +0530

    Issue #SB-14499 fix: Elastic search read issue fix

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/AssessmentMetricsJob.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/AssessmentMetricsJob.scala
@@ -178,10 +178,8 @@ object AssessmentMetricsJob extends optional.Application with IJob {
       .withColumn("agg_max_score", sum("total_max_score") over assessmentAggDf)
       // To avoid converting numeric field to date format in the spread sheet, We enclosing score with double quotes.
       // Example: 2/3 => '2/3'. Since google spread sheet will consider 2/3 as date format column.
-      .withColumn("total_sum_score", concat(col("agg_score"), lit("/"), col("agg_max_score")))
-
-    val quoteStr = udf((scoreStr: String) => "\"" + scoreStr + "\"")
-    val aggregatedDF = resDF.withColumn("total_sum_score", quoteStr(col("total_sum_score"))).withColumn("grand_score", quoteStr(col("grand_total")))
+      .withColumn("total_sum_score", concat(lit("'"), col("agg_score"), lit("/"), col("agg_max_score"), lit("'")))
+    val aggregatedDF = resDF.withColumn("grand_score", concat(lit("'"), col("grand_total"), lit("'")))
     /**
       * Filter only valid enrolled userid for the specific courseid
       */
