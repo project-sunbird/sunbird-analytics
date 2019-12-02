539dd36e24cf3b4c45bc88865fc71b796cc68a1b
Utkarsha Kapoor <utkit14@gmail.com>
2019-12-02 18:27:24 +0530

    #SC-1465 Refactor: Refactored the MetricsAuditModel

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/MetricsAuditModel.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/model/MetricsAuditModel.scala
@@ -1,18 +1,21 @@
 package org.ekstep.analytics.model
 
+import java.util.Date
+
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
+import org.ekstep.analytics.framework._
 import org.ekstep.analytics.framework.conf.AppConf
-import org.ekstep.analytics.framework.util.{CommonUtil, RestUtil}
-import org.ekstep.analytics.framework.{Empty, IBatchModelTemplate, V3DerivedEvent, V3MetricEdata}
-import org.ekstep.analytics.util.SecorMetricUtil
-import org.joda.time.DateTime
+import org.ekstep.analytics.framework.util.{CommonUtil, JSONUtils}
+
+case class DruidMetrics(total_count: Long, date: String)
 
 object MetricsAuditModel extends IBatchModelTemplate[Empty, Empty, V3DerivedEvent, V3DerivedEvent] with Serializable {
 
   val className = "org.ekstep.analytics.model.MetricsAuditJob"
-
   override def name: String = "MetricsAuditJob"
+
+  implicit val fc = new FrameworkContext()
 
   override def preProcess(events: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[Empty] = {
     events;
@@ -20,43 +23,65 @@ object MetricsAuditModel extends IBatchModelTemplate[Empty, Empty, V3DerivedEven
 
   override def algorithm(events: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[V3DerivedEvent] = {
 
-    val auditConfig = config.get("auditConfig")
-    val models = auditConfig.get.asInstanceOf[Map[String, AnyRef]]
+    val auditConfig = config("auditConfig").asInstanceOf[List[Map[String, AnyRef]]]
+    val metrics = auditConfig.map{f =>
+      var metricsEvent: V3DerivedEvent = null
+      val queryConfig = JSONUtils.deserialize[JobConfig](JSONUtils.serialize(f))
+      val queryType = queryConfig.search.`type`
+      val name = queryConfig.name.get
 
-    val metrics = models.flatMap{f =>
-      val queryType = f._1
       queryType match {
+        case "azure" =>
+          val data = DataFetcher.fetchBatchData[V3Event](queryConfig.search)
+          metricsEvent = getSecorMetrics(queryConfig, data, name)
         case "druid" =>
-          computeDruidMetrics(models.get("druid").get.asInstanceOf[Map[String, AnyRef]])
-        case "secor" =>
-          SecorMetricUtil.getSecorMetrics(models.get("secor").get.asInstanceOf[Map[String,AnyRef]])
+          val data = DataFetcher.fetchBatchData[DruidMetrics](queryConfig.search).first()
+          val metrics = List(V3MetricEdata(name, Some(data.total_count)),V3MetricEdata("date", Some(data.date)))
+          metricsEvent = CommonUtil.getMetricEvent(Map("system" -> "DruidCount", "subsystem" -> name, "metrics" -> metrics), AppConf.getConfig("metric.producer.id"), AppConf.getConfig("metric.producer.pid"))
       }
+      metricsEvent
     }
-    sc.parallelize(metrics.toList)
+    sc.parallelize(metrics)
   }
 
   override def postProcess(events: RDD[V3DerivedEvent], config: Map[String, AnyRef])(implicit sc: SparkContext): RDD[V3DerivedEvent] = {
     events;
   }
 
-  def computeDruidMetrics(druidConfig: Map[String, AnyRef]) (implicit sc: SparkContext): List[V3DerivedEvent] = {
-    val druidModel = druidConfig("models").asInstanceOf[Map[String, AnyRef]]
 
-    val apiURL = AppConf.getConfig("druid.sql.host")
-    val startDate = new DateTime().minusDays(1).toString("yyyy-MM-dd HH:mm:ss")
-    val endDate = new DateTime().toString("yyyy-MM-dd HH:mm:ss")
-
-    val metrics = druidModel.map(model => {
-      computeDruidSourceCount(apiURL, model._2.asInstanceOf[Map[String, String]]("query"), startDate, endDate, model._1)
-    })
-    (metrics.toList)
+  def getSecorMetrics(queryConfig: JobConfig,rdd: RDD[V3Event], name: String)(implicit sc: SparkContext): V3DerivedEvent = {
+    var metricEvent: List[V3MetricEdata] = null
+    name match {
+      case "denorm" =>
+        metricEvent = getDenormSecorAudit(rdd, queryConfig.filters.get)
+      case "failed" =>
+        metricEvent = getFailedSecorAudit(rdd)
+      case _ =>
+        metricEvent = getTotalSecorCountAudit(name, rdd)
+    }
+    val v3metric = CommonUtil.getMetricEvent(Map("system" -> "SecorAuditBackup", "subsystem" -> name,
+      "metrics" -> metricEvent), AppConf.getConfig("metric.producer.id"), AppConf.getConfig("metric.producer.pid"))
+    v3metric
   }
 
-  def computeDruidSourceCount(apiURL: String, query: String, startDate: String, endDate: String, systemName: String): V3DerivedEvent = {
-    val requestQuery = AppConf.getConfig(query).format(startDate, endDate)
-    val response = RestUtil.post[List[Map[String, String]]](apiURL, requestQuery).headOption
-    val responseMetrics = response.map(f => V3MetricEdata("telemetryCount", f.get("total").get.asInstanceOf[String])).toList
-    val metricsEvent = CommonUtil.getMetricEvent(Map("system" -> "DruidCount", "subsystem" -> systemName, "metrics" -> responseMetrics), AppConf.getConfig("metric.producer.id"), AppConf.getConfig("metric.producer.pid"))
-    metricsEvent
+  def getTotalSecorCountAudit(secorName: String, rdd: RDD[V3Event]): List[V3MetricEdata] = {
+
+    val totalCount = rdd.count()
+    val metricEvent = List(V3MetricEdata("date", Option(new Date())), V3MetricEdata("inputEvents", Option(totalCount)))
+    metricEvent
+  }
+
+  def getFailedSecorAudit(rdd: RDD[V3Event]): List[V3MetricEdata] = {
+    val failedCountByPID = rdd.filter(f => null != f.context && null != f.context.pdata).groupBy(f => f.context.pdata.get.id)
+    val metrics = failedCountByPID.map(f => V3MetricEdata(f._1, Some(f._2.size)))
+    metrics.collect().toList
+  }
+
+  def getDenormSecorAudit(rdd: RDD[V3Event], filters: Array[Filter])(implicit sc: SparkContext): List[V3MetricEdata] = {
+    val count = rdd.count()
+    val metricData = filters.map{f =>
+      V3MetricEdata(f.name, Option(DataFilter.filter(rdd, f).count()))
+    }.toList
+    metricData
   }
 }
