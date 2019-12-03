6b03d4f51b7d4e18fae3e9e9826163b57507404d
Anand <anandp@ilimi.in>
2019-12-03 12:21:27 +0530

    Issue #SC-1529 feat: Fix unit tests for experiment service

--- platform-api/analytics-api-core/src/test/scala/org/ekstep/analytics/api/util/TestDBUtil.scala
+++ platform-api/analytics-api-core/src/test/scala/org/ekstep/analytics/api/util/TestDBUtil.scala
@@ -9,7 +9,7 @@ class TestDBUtil extends BaseSpec {
     
     it should "fetch list of jobs in a descending order" in {
                 
-        DBUtil.session.execute("DELETE FROM " + AppConf.getConfig("application.env") +"_platform_db.job_request WHERE client_key='partner1'");
+        DBUtil.session.execute("DELETE FROM " + AppConf.getConfig("application.env") +"_platform_db.job_request WHERE client_key='partner1'")
         
         val request_data1 = """{"filter":{"start_date":"2016-11-19","end_date":"2016-11-20","tags":["becb887fe82f24c644482eb30041da6d88bd8150"]}}"""
         val request_data2 = """{"filter":{"start_date":"2016-11-19","end_date":"2016-11-20","tags":["test-tag"],"events":["OE_ASSESS"]}}"""
@@ -18,7 +18,7 @@ class TestDBUtil extends BaseSpec {
             JobRequest(Option("partner1"), Option("1234"), None, Option("SUBMITTED"), Option(request_data1),
                 Option(1), Option(DateTime.now()), None, None, None, None, None, None, None, None, None, None, None, None, None, None, None),
             JobRequest(Option("partner1"), Option("273645"), Option("test-job-id"), Option("COMPLETED"), Option(request_data2),
-                Option(1), Option(DateTime.parse("2017-01-08", CommonUtil.dateFormat)), Option("https://test-location"), Option(DateTime.parse("2017-01-08", CommonUtil.dateFormat)), None, None, None, None, None, Option(123234), Option(532), Option(12343453L), None, None, None, None, None));
+                Option(1), Option(DateTime.parse("2017-01-08", CommonUtil.dateFormat)), Option("https://test-location"), Option(DateTime.parse("2017-01-08", CommonUtil.dateFormat)), None, None, None, None, None, Option(123234), Option(532), Option(12343453L), None, None, None, None, None))
         DBUtil.saveJobRequest(requests)
         
         val jobs = DBUtil.getJobRequestList("partner1")
