4072271288c7ffbecc52240404a66705b553a6aa
Manjunath Davanam <manjunathd@ilimi.in>
2019-12-05 13:07:53 +0530

    Issue SC-1482 fix: Removed unused methods from the base adapter and code format

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/adapter/TestBaseAdapter.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/adapter/TestBaseAdapter.scala
@@ -1,45 +1,17 @@
 package org.ekstep.analytics.adapter
 
-import org.ekstep.analytics.framework.{Params, Response}
-import org.ekstep.analytics.framework.exception.DataAdapterException
 import org.ekstep.analytics.framework.util.JSONUtils
+import org.ekstep.analytics.framework.{Params, Response}
 import org.ekstep.analytics.model.SparkSpec
-import org.junit.Assert
 import org.sunbird.cloud.storage.conf.AppConf
 
 class TestBaseAdapter extends SparkSpec {
 
-    "BaseAdapter" should "pass all test cases" in {
-        val respStr = "{\"id\":\"analytics.device-profile\",\"ver\":\"1.0\",\"ts\":\"2019-11-12T07:28:10.555+00:00\",\"params\":{\"resmsgid\":\"e7f845ae-88f9-40b5-9cf3-1b2efc722879\",\"status\":\"successful\",\"client_key\":null},\"responseCode\":\"OK\",\"result\":{\"userDeclaredLocation\":{\"state\":\"Karnataka\",\"district\":\"KOPPAL\"},\"ipLocation\":{\"state\":\"Karnataka\",\"district\":\"BENGALURU URBAN SOUTH\"}}}"
-        val response = JSONUtils.deserialize[Response](respStr)
-        ContentAdapter.checkResponse(response)
-        ContentResponse("", "", "", Params(None, None, None, None, None), "", ContentResult(0, Option(Array[Map[String, AnyRef]]())))
-        Console.println("reports_azure_storage_key", AppConf.getConfig("reports_azure_storage_key"));
-    }
-
-    it should "throw DataAdapterException" in {
-        val respStr = "{\"id\":\"analytics.device-profile\",\"ver\":\"1.0\",\"ts\":\"2019-11-12T07:28:10.555+00:00\",\"params\":{\"resmsgid\":\"e7f845ae-88f9-40b5-9cf3-1b2efc722879\",\"status\":\"Failed\",\"client_key\":null},\"responseCode\":\"400 Not Found\",\"result\":{\"userDeclaredLocation\":{\"state\":\"Karnataka\",\"district\":\"KOPPAL\"},\"ipLocation\":{\"state\":\"Karnataka\",\"district\":\"BENGALURU URBAN SOUTH\"}}}"
-        val response = JSONUtils.deserialize[Response](respStr)
-
-        the[DataAdapterException] thrownBy {
-            ContentAdapter.checkResponse(response)
-        }
-        
-        noException should be thrownBy {
-          ContentAdapter.getTextbookContents("2019-11-01", "2019-11-02");
-        }
-
-    }
-
-    it should "Should able to search" in {
-        val anyRefVariable: AnyRef = new java.util.Date
-        val arr = Array(Map("date" -> anyRefVariable))
-
-        def testFun(a: Int, b: Int): ContentResult = {
-            ContentResult(20, Option(arr))
-        }
-
-        Assert.assertNotNull(ContentAdapter.search(10, 2, arr, testFun))
-    }
-
+  "BaseAdapter" should "pass all test cases" in {
+    val respStr = "{\"id\":\"analytics.device-profile\",\"ver\":\"1.0\",\"ts\":\"2019-11-12T07:28:10.555+00:00\",\"params\":{\"resmsgid\":\"e7f845ae-88f9-40b5-9cf3-1b2efc722879\",\"status\":\"successful\",\"client_key\":null},\"responseCode\":\"OK\",\"result\":{\"userDeclaredLocation\":{\"state\":\"Karnataka\",\"district\":\"KOPPAL\"},\"ipLocation\":{\"state\":\"Karnataka\",\"district\":\"BENGALURU URBAN SOUTH\"}}}"
+    val response = JSONUtils.deserialize[Response](respStr)
+    ContentAdapter.checkResponse(response)
+    ContentResponse("", "", "", Params(None, None, None, None, None), "", ContentResult(0, Option(Array[Map[String, AnyRef]]())))
+    Console.println("reports_azure_storage_key", AppConf.getConfig("reports_azure_storage_key"));
+  }
 }
