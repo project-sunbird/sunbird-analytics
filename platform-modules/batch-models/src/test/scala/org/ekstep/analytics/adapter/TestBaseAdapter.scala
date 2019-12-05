f1c70e8360759ee47fa5a11b68f6accf1553f2b5
Manjunath Davanam <manjunathd@ilimi.in>
2019-12-05 13:23:30 +0530

    Issue SC-1482 fix: Base adapter test case improvement

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/adapter/TestBaseAdapter.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/adapter/TestBaseAdapter.scala
@@ -1,5 +1,6 @@
 package org.ekstep.analytics.adapter
 
+import org.ekstep.analytics.framework.exception.DataAdapterException
 import org.ekstep.analytics.framework.util.JSONUtils
 import org.ekstep.analytics.framework.{Params, Response}
 import org.ekstep.analytics.model.SparkSpec
@@ -13,5 +14,15 @@ class TestBaseAdapter extends SparkSpec {
     ContentAdapter.checkResponse(response)
     ContentResponse("", "", "", Params(None, None, None, None, None), "", ContentResult(0, Option(Array[Map[String, AnyRef]]())))
     Console.println("reports_azure_storage_key", AppConf.getConfig("reports_azure_storage_key"));
+
+  }
+
+
+  it should "Throw exception" in {
+    val badReqRes = "{\"id\":\"analytics.device-profile\",\"ver\":\"1.0\",\"ts\":\"2019-11-12T07:28:10.555+00:00\",\"params\":{\"resmsgid\":\"e7f845ae-88f9-40b5-9cf3-1b2efc722879\",\"status\":\"successful\",\"client_key\":null},\"responseCode\":\"BAD REQUEST\",\"result\":null}"
+    the[DataAdapterException] thrownBy {
+      ContentAdapter.checkResponse(JSONUtils.deserialize[Response](badReqRes))
+      ContentResponse("", "", "", Params(None, None, None, None, None), "", ContentResult(0, Option(Array[Map[String, AnyRef]]())))
+    } should have message "BAD REQUEST"
   }
 }
