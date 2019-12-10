5e67f49950f93a78478dcd37c0c16b2d670de208
Harikumar Palemkota <harip@ilimi.in>
2019-12-10 14:58:17 +0530

    Issue #SC-1570 checking user and geo reports exists in test cases

--- platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/report/TestStateAdminReportJob.scala
+++ platform-modules/batch-models/src/test/scala/org/ekstep/analytics/job/report/TestStateAdminReportJob.scala
@@ -1,10 +1,13 @@
 package org.ekstep.analytics.job
 
-import org.apache.spark.sql.functions.{col}
+import java.io.File
+
+import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{DataFrame, Encoders, SparkSession}
 import org.ekstep.analytics.job.report.{BaseReportSpec, BaseReportsJob, ShadowUserData, StateAdminReportJob}
-import org.ekstep.analytics.util.{EmbeddedCassandra}
+import org.ekstep.analytics.util.EmbeddedCassandra
 import org.scalamock.scalatest.MockFactory
+import org.sunbird.cloud.storage.conf.AppConf
 
 class TestStateAdminReportJob extends BaseReportSpec with MockFactory {
 
@@ -24,6 +27,7 @@ class TestStateAdminReportJob extends BaseReportSpec with MockFactory {
 
 
   "StateAdminReportJob" should "generate reports" in {
+    val tempDir = AppConf.getConfig("admin.metrics.temp.dir")
     val reportDF = StateAdminReportJob.generateReport()(spark)
     assert(reportDF.columns.contains("index") === true)
     assert(reportDF.columns.contains("registered") === true)
@@ -34,6 +38,17 @@ class TestStateAdminReportJob extends BaseReportSpec with MockFactory {
     val apslug = reportDF.where(col("slug") === "ApSlug")
     val districtName = apslug.select("districtName").collect().map(_ (0)).toList
     assert(districtName(0) === "GULBARGA")
+    //checking reports were created under slug folder
+    val slugName = apslug.select("slug").collect().map(_ (0)).toList
+    val apslugDirPath = tempDir+"/renamed/"+slugName(0)+"/"
+    val userDetail = new File(apslugDirPath+"user-detail.csv")
+    val userSummary = new File(apslugDirPath+"user-summary.json")
+    val validateUserDetail = new File(apslugDirPath+"validated-user-detail.csv")
+    val validateUserSummary = new File(apslugDirPath+"validated-user-summary.json")
+    assert(userDetail.exists() === true)
+    assert(userSummary.exists() === true)
+    assert(validateUserDetail.exists() === true)
+    assert(validateUserSummary.exists() === true)
   }
 
 }
