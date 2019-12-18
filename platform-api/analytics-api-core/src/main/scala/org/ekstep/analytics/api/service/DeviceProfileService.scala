4ff3fdc2504f98cf70a62781ead13dfbbb46bc84
Anand <anandp@ilimi.in>
2019-12-18 17:43:18 +0530

    Issue #SB-16362 feat: Redis conn fix for device profile service

--- platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/DeviceProfileService.scala
+++ platform-api/analytics-api-core/src/main/scala/org/ekstep/analytics/api/service/DeviceProfileService.scala
@@ -1,39 +1,35 @@
 package org.ekstep.analytics.api.service
 
-import akka.actor.{Actor, ActorRef}
-import akka.pattern.pipe
+import akka.actor.{Actor, Props}
 import com.google.common.net.InetAddresses
 import com.google.common.primitives.UnsignedInts
 import com.typesafe.config.Config
-import javax.inject.{Inject, Named}
+import javax.inject.Inject
 import org.ekstep.analytics.api.util._
 import redis.clients.jedis.Jedis
 import redis.clients.jedis.exceptions.JedisConnectionException
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.collection.JavaConverters._
 
 case class DeviceProfileRequest(did: String, headerIP: String)
 
 class DeviceProfileService @Inject()(
                                       config: Config,
                                       redisUtil: RedisUtil,
-                                      H2DB : H2DBUtil
+                                      H2DB: H2DBUtil
                                     ) extends Actor {
 
-  implicit val ec: ExecutionContext = context.system.dispatchers.lookup("device-register-actor")
   implicit val className: String ="DeviceProfileService"
   val deviceDatabaseIndex: Int = config.getInt("redis.deviceIndex")
   val geoLocationCityTableName: String = config.getString("postgres.table.geo_location_city.name")
   val geoLocationCityIpv4TableName: String = config.getString("postgres.table.geo_location_city_ipv4.name")
-  implicit val jedisConnection: Jedis = redisUtil.getConnection(deviceDatabaseIndex)
-  private val enableDebugLogging = config.getBoolean("device.api.enable.debug.log")
 
   override def preStart { println("starting DeviceProfileService") }
 
   override def postStop { println("Stopping DeviceProfileService") }
 
   override def preRestart(reason: Throwable, message: Option[Any]) {
-    println(s"restarting DeviceProfileActor: $message")
+    println(s"Restarting DeviceProfileActor: $message")
     reason.printStackTrace()
     super.preRestart(reason, message)
   }
@@ -41,9 +37,8 @@ class DeviceProfileService @Inject()(
   def receive = {
     case deviceProfile: DeviceProfileRequest =>
       try {
-        val senderActor = sender()
         val result = getDeviceProfile(deviceProfile)
-        result.pipeTo(senderActor)
+        sender() ! result
       } catch {
         case ex: JedisConnectionException =>
           ex.printStackTrace()
@@ -62,7 +57,7 @@ class DeviceProfileService @Inject()(
       }
   }
 
-  def getDeviceProfile(deviceProfileRequest: DeviceProfileRequest): Future[Option[DeviceProfile]] = {
+  def getDeviceProfile(deviceProfileRequest: DeviceProfileRequest): Option[DeviceProfile] = {
 
     if (deviceProfileRequest.headerIP.nonEmpty) {
       val ipLocationFromH2 = resolveLocationFromH2(deviceProfileRequest.headerIP)
@@ -70,29 +65,33 @@ class DeviceProfileService @Inject()(
 
       // logging resolved location details
       if (ipLocationFromH2.state.nonEmpty) {
-        if (enableDebugLogging) {
-          println(s"For IP: ${deviceProfileRequest.headerIP}, Location resolved for $did to state: ${ipLocationFromH2.state}, district: ${ipLocationFromH2.districtCustom}")
-        }
-        APILogger.log("", Option(Map("comments" -> s"Location resolved for $did to state: ${ipLocationFromH2.state}, district: ${ipLocationFromH2.districtCustom}")), "getDeviceProfile")
+        APILogger.log("", Option(Map("comments" -> s"IP Location resolved for $did to state: ${ipLocationFromH2.state}, district: ${ipLocationFromH2.districtCustom}")), "getDeviceProfile")
       } else {
-        if (enableDebugLogging) {
-          println(s"For IP: ${deviceProfileRequest.headerIP}, Location is not resolved for $did")
-        }
-        APILogger.log("", Option(Map("comments" -> s"Location is not resolved for $did")), "getDeviceProfile")
+        APILogger.log("", Option(Map("comments" -> s"IP Location is not resolved for $did")), "getDeviceProfile")
       }
 
-      val deviceLocation = redisUtil.getAllByKey(did)
+      val jedisConnection: Jedis = redisUtil.getConnection(deviceDatabaseIndex)
+      val deviceLocation = try {
+        Option(jedisConnection.hgetAll(did).asScala.toMap)
+      } catch {
+        case ex: Exception =>
+          APILogger.log("", Option(Map("comments" -> s"Redis exception during did lookup: ${ex.getMessage}")), "DeviceProfileService")
+          None
+      } finally {
+        jedisConnection.close()
+      }
+
       val userDeclaredLoc = if (deviceLocation.nonEmpty && deviceLocation.get.getOrElse("user_declared_state", "").nonEmpty) {
         Option(Location(deviceLocation.get("user_declared_state"), deviceLocation.get("user_declared_district")))
       } else None
 
-      if (enableDebugLogging) {
-        userDeclaredLoc.foreach { declaredLocation => println(s"[did: $did, user_declared_state: ${declaredLocation.state}, user_declared_district: ${declaredLocation.district}") }
+      userDeclaredLoc.foreach { declaredLocation =>
+        APILogger.log("", Option(Map("comments" -> s"[did: $did, user_declared_state: ${declaredLocation.state}, user_declared_district: ${declaredLocation.district}")), "DeviceProfileService")
       }
 
-      Future(Some(DeviceProfile(userDeclaredLoc, Option(Location(ipLocationFromH2.state, ipLocationFromH2.districtCustom)))))
+      Some(DeviceProfile(userDeclaredLoc, Option(Location(ipLocationFromH2.state, ipLocationFromH2.districtCustom))))
     } else {
-      Future(None)
+      None
     }
   }
 
@@ -115,3 +114,9 @@ class DeviceProfileService @Inject()(
   }
 
 }
+
+/*
+object DeviceProfileService {
+  def props = Props[DeviceProfileService].withDispatcher("device-profile-actor")
+}
+*/
