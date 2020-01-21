e04b963af8514f0da4769baf556235e6db450820
Isha Wakankar <iwakankar@hotmail.com>
2020-01-21 17:39:21 +0530

    Issue #SB-16574: Joining dataframes for common preprocess

--- platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/report/BaseCourseMetricsJob.scala
+++ platform-modules/batch-models/src/main/scala/org/ekstep/analytics/job/report/BaseCourseMetricsJob.scala
@@ -2,18 +2,40 @@ package org.ekstep.analytics.job.report
 
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.ekstep.analytics.framework.{Empty, FrameworkContext, IBatchModelTemplate}
-import org.ekstep.analytics.util.CourseUtils.{getTenantInfo, getCourseBatchDetails, getLiveCourses}
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.ekstep.analytics.framework._
+import org.ekstep.analytics.util.CourseUtils.{getCourseBatchDetails, getLiveCourses, getTenantInfo, loadData}
+import org.sunbird.cloud.storage.conf.AppConf
 
-trait BaseCourseMetricsJob extends IBatchModelTemplate[Empty,Empty,Empty,Empty] {
+import scala.collection.Map
+
+trait BaseCourseMetricsJob[T <: AnyRef, A <: AlgoInput, B <: AlgoOutput, R <: Output] extends IBatchModelTemplate[Empty,Empty,Empty,Empty] {
 
   def preProcess(events: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[Empty] = {
-    val liveCourses = getLiveCourses(config)
+    val data = getCourseMetrics(config)
     events
   }
 
   def algorithm(events: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[Empty]
 
   def postProcess(events: RDD[Empty], config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): RDD[Empty]
+
+  def getCourseMetrics(config: Map[String, AnyRef])(implicit sc: SparkContext, fc: FrameworkContext): DataFrame = {
+    val readConsistencyLevel: String = AppConf.getConfig("assessment.metrics.cassandra.input.consistency")
+    val sparkConf = sc.getConf
+      .set("spark.cassandra.input.consistency.level", readConsistencyLevel).set("spark.cassandra.connection.host", "localhost")
+      .set("spark.cassandra.connection.port","9042")
+    implicit val spark: SparkSession = SparkSession.builder.config(sparkConf).getOrCreate()
+
+    val liveCourses = getLiveCourses(config)
+    val courseBatch = getCourseBatchDetails(spark, loadData)
+    val tenantInfo = getTenantInfo(spark, loadData)
+
+    val courses = liveCourses.withColumnRenamed("name","courseName")
+    val joinCourses = courses.join(courseBatch, courses.col("identifier") === courseBatch.col("courseid"), "inner")
+    val joinWithTenant = joinCourses.join(tenantInfo, joinCourses.col("channel") === tenantInfo.col("id"), "inner")
+
+    joinWithTenant.select("courseName","name","status","slug", "courseid", "batchid")
+  }
 
 }
